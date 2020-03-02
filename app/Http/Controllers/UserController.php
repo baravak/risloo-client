68f794be628ad0bc0fc39e0d4172323c27e0ab74
Ahmad Karimi <ahmadkarimi1991@gmail.com>
2020-03-02 20:38:37 +0330

    Fetch users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,8 +5,9 @@ use Illuminate\Http\Request;
 use App\User;
 class UserController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        $users = User::apiIndex();
         return view('dashboard.users.index', $this->data);
     }
 }
