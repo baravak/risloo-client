f5e3a515f4241ebe0a450b25423beccfe19e3b6c
Ahmad Karimi <ahmadkarimi1991@gmail.com>
2020-03-05 14:38:57 +0330

    Update Sarkoot and fix

--- resources/views/dashboard/users/create.blade.php
+++ resources/views/dashboard/users/create.blade.php
@@ -33,73 +33,78 @@
         <input type="password" class="form-control form-control-m text-left direction-ltr" id="password" name="password"placeholder="&nbsp;" autocomplete="new-password">
         <label for="password">{{__('password')}}</label>
     </div>
-    <div class="form-group d-flex flex-wrap">
+
+    <div class="form-group mb-0">
         <label>{{__('status')}}</label>
-        <div class="richak richak-sm richak-secondary">
-            <input type="radio" name="status" id="status-active" value="active" @radioSelected($user->status, 'active')>
-            <label for="status-active">
-                <span class="far fa-lightbulb-on richak-icon"></span>
-                {{__('status.active')}}
-            </label>
-        </div>
-        <div class="richak richak-sm richak-secondary">
-            <input type="radio" name="status" id="status-awaiting" value="awaiting" @radioSelected($user->status, 'active')>
-            <label for="status-awaiting">
-                <i class="fal fa-user-clock richak-icon"></i>
-                {{__('status.awaiting')}}
-            </label>
-        </div>
-        <div class="richak richak-sm richak-secondary">
-            <input type="radio" name="status" id="status-blocked" value="blocked" @radioSelected($user->status, 'active')>
-            <label for="status-blocked">
-                <i class="fal fa-user-lock richak-icon"></i>
-                {{__('status.blocked')}}
-            </label>
-        </div>
-    </div>
-
-    <div class="form-group d-flex flex-wrap">
-        <label>{{__('type')}}</label>
-        <div class="richak richak-sm richak-secondary">
-            <input type="radio" name="type" id="type-admin" value="admin" @radioSelected($user->type, 'admin')>
-            <label for="type-admin">
-                <span class="far fa-users-crown richak-icon"></span>
-                {{__('type.admin')}}
-            </label>
-        </div>
-        <div class="richak richak-sm richak-secondary">
-            <input type="radio" name="type" id="type-manager" value="manager" @radioSelected($user->type, 'manager')>
-            <label for="type-manager">
-                <i class="fal fa-users-cog richak-icon"></i>
-                {{__('type.manager')}}
-            </label>
-        </div>
-        <div class="richak richak-sm richak-secondary">
-            <input type="radio" name="type" id="type-operator" value="operator" @radioSelected($user->type, 'operator')>
-            <label for="type-operator">
-                <i class="fal fa-user-headset richak-icon"></i>
-                {{__('type.operator')}}
-            </label>
-        </div>
-        <div class="richak richak-sm richak-secondary">
-            <input type="radio" name="type" id="type-psychologist" value="psychologist" @radioSelected($user->type, 'psychologist')>
-            <label for="type-psychologist">
-                <i class="fal fa-user-tie richak-icon"></i>
-                {{__('type.psychologist')}}
-            </label>
-        </div>
-        <div class="richak richak-sm richak-secondary">
-            <input type="radio" name="type" id="type-client" value="client" @radioSelected($user->type, 'client')>
-            <label for="type-client">
-                <i class="far fa-user-shield richak-icon"></i>
-                {{__('type.client')}}
-            </label>
-        </div>
-    </div>
-
-    <div class="form-group">
         <div class="d-flex flex-wrap">
-            <label>{{__('gender')}}</label>
+            <div class="richak richak-sm richak-secondary">
+                <input type="radio" name="status" id="status-active" value="active" @radioSelected($user->status, 'active')>
+                <label for="status-active">
+                    <span class="far fa-lightbulb-on richak-icon"></span>
+                    {{__('status.active')}}
+                </label>
+            </div>
+            <div class="richak richak-sm richak-secondary">
+                <input type="radio" name="status" id="status-awaiting" value="awaiting" @radioSelected($user->status, 'active')>
+                <label for="status-awaiting">
+                    <i class="fal fa-user-clock richak-icon"></i>
+                    {{__('status.awaiting')}}
+                </label>
+            </div>
+            <div class="richak richak-sm richak-secondary">
+                <input type="radio" name="status" id="status-blocked" value="blocked" @radioSelected($user->status, 'active')>
+                <label for="status-blocked">
+                    <i class="fal fa-user-lock richak-icon"></i>
+                    {{__('status.blocked')}}
+                </label>
+            </div>
+        </div>
+    </div>
+
+    <div class="form-group mb-0">
+        <label>{{__('type')}}</label>
+        <div class="d-flex flex-wrap">
+            <div class="richak richak-sm richak-secondary">
+                <input type="radio" name="type" id="type-admin" value="admin" @radioSelected($user->type, 'admin')>
+                <label for="type-admin">
+                    <span class="far fa-users-crown richak-icon"></span>
+                    {{__('type.admin')}}
+                </label>
+            </div>
+            <div class="richak richak-sm richak-secondary">
+                <input type="radio" name="type" id="type-manager" value="manager" @radioSelected($user->type, 'manager')>
+                <label for="type-manager">
+                    <i class="fal fa-users-cog richak-icon"></i>
+                    {{__('type.manager')}}
+                </label>
+            </div>
+            <div class="richak richak-sm richak-secondary">
+                <input type="radio" name="type" id="type-operator" value="operator" @radioSelected($user->type, 'operator')>
+                <label for="type-operator">
+                    <i class="fal fa-user-headset richak-icon"></i>
+                    {{__('type.operator')}}
+                </label>
+            </div>
+            <div class="richak richak-sm richak-secondary">
+                <input type="radio" name="type" id="type-psychologist" value="psychologist" @radioSelected($user->type, 'psychologist')>
+                <label for="type-psychologist">
+                    <i class="fal fa-user-tie richak-icon"></i>
+                    {{__('type.psychologist')}}
+                </label>
+            </div>
+            <div class="richak richak-sm richak-secondary">
+                <input type="radio" name="type" id="type-client" value="client" @radioSelected($user->type, 'client')>
+                <label for="type-client">
+                    <i class="far fa-user-shield richak-icon"></i>
+                    {{__('type.client')}}
+                </label>
+            </div>
+        </div>
+    </div>
+
+    <div class="form-group mb-0">
+        <label>{{__('gender')}}</label>
+        <div class="d-flex flex-wrap">
             <div class="richak richak-sm richak-secondary">
                 <input type="radio" name="gender" id="gender-male" value="male" @radioSelected($user->gender, 'male')>
                 <label for="gender-male">
