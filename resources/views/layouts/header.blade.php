14dce4c8da4839e14f15628058a779d0b9f07afa
Ahmad Karimi <ahmadkarimi1991@gmail.com>
2020-03-05 19:57:02 +0330

    Fix

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -6,8 +6,10 @@
     </div>
     <div class="d-flex align-items-center">
         <div class="profile-div {{app('request')->user()->response('current') ? 'profile-div-danger' : ''}}">
-            <img src="dist/images/avatar/avatar.jpg" alt="Avatar" class="profile rounded-circle" width="32" height="32">
-            <!-- <span class="profile profile-placeholder d-flex justify-content-center align-items-center">S</span> -->
+            <div class="pulse">
+                <img src="dist/images/avatar/avatar.jpg" alt="Avatar" class="profile rounded-circle" width="32" height="32">
+                <!-- <span class="profile profile-placeholder d-flex justify-content-center align-items-center">S</span> -->
+            </div>
             <div class="profile-dropdown shadow">
                 <div class="profile-dropdown-header p-3 d-flex flex-column align-items-center">
                     <a href="#" class="text-white text-decoration-none fs-14">{{app('request')->user()->name ?: app('request')->user()->id}}</a>
