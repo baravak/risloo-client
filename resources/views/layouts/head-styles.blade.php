3ceb228ba2989957300db596bc039dbaa03ed1ce
Ahmad Karimi <ahmadkarimi1991@gmail.com>
2020-03-12 18:02:44 +0330

    Add iziToast and persian date picker

--- resources/views/layouts/head-styles.blade.php
+++ resources/views/layouts/head-styles.blade.php
@@ -3,6 +3,8 @@
     <link rel="stylesheet" href="/dist/vendors/bootstrap-4.4.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="/dist/vendors/bootstrap4-rtl/bootstrap4-rtl.min.css">
     <link rel="stylesheet" href="/dist/vendors/select2-4.0.13/dist/css/select2.min.css">
+    <link rel="stylesheet" href="/dist/vendors/iziToast/css/iziToast.min.css">
+    <link rel="stylesheet" href="/dist/vendors/persian-datepicker/css/persian-datepicker.min.css">
     <link rel="stylesheet" href="@staticVersion('/dist/css/theme.css')">
     <link rel="stylesheet" href="@staticVersion('/dist/css/sarkoot.css')">
 @endsection
--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -3,6 +3,9 @@
     <script src="/dist/vendors/popper.min.js"></script>
     <script src="/dist/vendors/bootstrap-4.4.1/js/bootstrap.min.js"></script>
     <script src="/dist/vendors/select2-4.0.13/dist/js/select2.min.js"></script>
+    <script src="/dist/vendors/iziToast/js/iziToast.min.js"></script>
+    <script src="/dist/vendors/persian-date/persian-date.min.js"></script>
+    <script src="/dist/vendors/persian-datepicker/js/persian-datepicker.min.js"></script>
 
     <script src="/dist/vendors/amcharts4/core.js"></script>
     <script src="/dist/vendors/amcharts4/charts.js"></script>
