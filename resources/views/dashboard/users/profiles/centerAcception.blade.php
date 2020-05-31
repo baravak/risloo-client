037d7a5dbdf836782cb64cf48186c632feda69e2
Ahmad Karimi <ahmadkarimi1991@gmail.com>
2020-05-31 12:16:49 +0430

    edit centers profile design

--- resources/views/dashboard/users/profiles/centerAcception.blade.php
+++ resources/views/dashboard/users/profiles/centerAcception.blade.php
@@ -18,15 +18,8 @@
             </span>
             @elseif($center->acception && $center->acception->accepted_at)
             <span class="d-flex align-items-baseline">
-                <i class="far fa-badge-check text-primary fs-12 pt-1"></i>
-                <span class="fs-10">{{__("You are is :position of this cenetr", ['position' => __(ucfirst($center->acception->position))])}}</span>
+                <span class="fs-12">{{__("You are is :position of this cenetr", ['position' => __(ucfirst($center->acception->position))])}}</span>
             </span>
         @endif
-    @endcan
-    @can('details', $center)
-        <a href="{{route('dashboard.relationship.users.index', $center->id)}}" class="btn btn-light fs-10">
-            <i class="far fa-address-book"></i>
-            {{__('Center members')}}
-        </a>
     @endcan
 </div>
--- resources/views/dashboard/users/profiles/counseling_center.blade.php
+++ resources/views/dashboard/users/profiles/counseling_center.blade.php
@@ -1,136 +1,86 @@
 @extends($layouts->dashboard)
 @section('content')
-<div class="row">
-    <div class="col-lg-12">
-        <div class="card mb-3">
-            <div class="card-header">
-                {{$user->id == auth()->id() ? __('My :type profile', ['type' => __(ucfirst($user->type))]) : $user->name}}
-                @if ($user->can('edit'))
-                    <a class="badge badge-primary fs-10" href="{{auth()->id() == $user->id ? route('dashboard.users.me.edit') : $user->route('edit')}}">
-                        {{__('Edit')}}
-                    </a>
-                @endif
-                @if (auth()->user()->type == 'admin' && auth()->user()->id != $user->id)
-                    <a href="{{route('auth.as', ['user' => $user->id])}}" class="badge badge-secondary fs-10" data-lijax="click" data-method="POST">
-                        <i class="fal fa-user-secret"></i> {{__('Login to this...')}}
-                    </a>
-                @endif
-                @include('dashboard.users.profiles.centerAcception', ['center' => $user->center])
-            </div>
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-sm-6 col-lg-6 col-xl-3 profile-separator">
-                        <div class="d-flex align-items-center mb-3">
-                            <div href="#" class="media media-primary media-xl rounded-circle" title="{{ $user->name ?: __('?') }}">
-                                @if (isset($user->name))
-                                    <span>{{ Illuminate\Support\Str::substr($user->name, 0, 1) }}</span>
-                                @else
-                                    <span>?</span>
+    <div class="row">
+        <div class="col-12 col-sm-12 col-xl-6">
+            <div class="card mb-3">
+                <div class="card-header d-flex flex-column flex-sm-row justify-content-sm-between align-items-sm-center">
+                    <div class="d-flex align-items-center">
+                        <a href="#" class="media media-lg media-primary rounded-circle">
+                            @if (isset($user->name))
+                                <span>{{ Illuminate\Support\Str::substr($user->name, 0, 1) }}</span>
+                            @else
+                                <span>?</span>
+                            @endif
+                        </a>
+                        <div class="mr-3">
+                            <div class="mb-1">
+                                {{$user->name ?: __('Anonymouse')}}
+                                @if ($user->can('edit'))
+                                    <a href="{{ auth()->id() == $user->id ? route('dashboard.users.me.edit') : $user->route('edit') }}" class="badge badge-primary">{{__('Edit')}}</a>
+                                @endif
+                                @if (auth()->user()->type == 'admin' && auth()->user()->id != $user->id)
+                                    <a href="{{ route('auth.as', ['user' => $user->id]) }}" class="badge badge-secondary" data-lijax="click" data-method="POST">
+                                        <i class="fal fa-user-secret"></i> {{__('Login to this...')}}
+                                    </a>
                                 @endif
                             </div>
-                            <div class="px-3">
-                                <div class="fs-14 font-weight-bold">{{$user->name ?: __('Anonymouse')}}</div>
-                                <div class="fs-12 direction-ltr text-left d-inline-block text-color-1">@username($user->username)</div>
+                            <div class="fs-14 font-weight-bold mb-1">
+                                #1310
                             </div>
+                            @include('dashboard.users.profiles.centerAcception', ['center' => $user->center])
                         </div>
                     </div>
-                    <div class="col-sm-6 col-md-6 col-lg-6 col-xl-3 profile-separator">
-                        <div class="row">
-                            <div class="col-6">
-                                <i class="far fa-mobile text-success"></i>
-                            </div>
-                            <div class="col-6">
-                                @if ($mobile = App\Mobile::parse($user->mobile))
-                                <a href="tel:+{{$mobile[2]}}{{$mobile[0]}}" class="fs-12 direction-ltr d-inline-block font-weight-bold text-decoration-none text-success" target="_blank">+{{$mobile[2]}} {{$mobile[0]}}</a>
-                                @endif
-                            </div>
-                            <div class="col-6">
-                                <i class="far fa-envelope text-primary"></i>
-                            </div>
-                            <div class="col-6">
-                                <a href="mailto:{{$user->email}}" class="fs-12 font-weight-bold text-decoration-none">{{$user->email}}</a>
-                            </div>
-                        </div>
+                    <div data-xhr="center-acception">
+                        @can('details', $user->center)
+                            <a href="{{ route('dashboard.relationship.users.index', $user->center->id) }}" class="btn btn-sm btn-outline-dark mt-3 m-sm-0">
+                                <i class="far fa-address-book align-middle ml-1"></i>
+                                {{__('Center members')}}
+                            </a>
+                        @endcan
                     </div>
-                    <div class="col-sm-6 col-lg-6 col-xl-3 profile-separator">
-                        <div class="row">
-                            <div class="col-6">
-                                <span class="fs-12">تحصیلات</span>
-                            </div>
-                            <div class="col-6">
-                                <span class="fs-12 font-weight-bold">کارشناسی ارشد</span>
-                            </div>
+                </div>
 
-                            <div class="col-6">
-                                <span class="fs-12">تاریخ تولد</span>
-                            </div>
-                            <div class="col-6">
-                                <span class="fs-12 font-weight-bold">۱۳۷۰/۸/۲۷</span>
-                            </div>
-                        </div>
-                    </div>
+                <div class="card-body">Hi</div>
+            </div>
+        </div>
 
-                    <div class="col-sm-6 col-lg-6 col-xl-3">
-                        <div class="row">
-                            <div class="col-6">
-                                <span class="fs-12">جلسات مشاوره</span>
-                            </div>
-                            <div class="col-6">
-                                <span class="fs-12 font-weight-bold">۴۳</span>
-                            </div>
+        <div class="col-xl-3">
+            <div class="card mb-3">
+                <div class="card-header">تخصص&zwnj;های درمانی</div>
+                <div class="card-body">
+                    <ul class="list-group list-group-flush pr-0">
+                        <li class="list-group-item fs-14">تخصص شماره یک</li>
+                        <li class="list-group-item fs-14">تخصص شماره دو</li>
+                        <li class="list-group-item fs-14">تخصص شماره سه</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
 
-                            <div class="col-6">
-                                <span class="fs-12">تست‌های گرفته‌شده</span>
-                            </div>
-                            <div class="col-6">
-                                <span class="fs-12 font-weight-bold">۱۵۰</span>
-                            </div>
-                        </div>
-                    </div>
+        <div class="col-xl-3">
+            <div class="card mb-3">
+                <div class="card-header">روان&zwnj;شناس&zwnj;ها</div>
+                <div class="card-body">
+                    <ul class="list-group list-group-flush pr-0">
+                        <li class="list-group-item fs-14">روان&zwnj;شناس شماره یک</li>
+                        <li class="list-group-item fs-14">روان&zwnj;شناس شماره دو</li>
+                        <li class="list-group-item fs-14">روان&zwnj;شناس شماره سه</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-xl-3">
+            <div class="card mb-3">
+                <div class="card-header">کارگاه&zwnj;های اجرا شده</div>
+                <div class="card-body">
+                    <ul class="list-group list-group-flush pr-0">
+                        <li class="list-group-item fs-14">کارگاه شماره یک</li>
+                        <li class="list-group-item fs-14">کارگاه شماره دو</li>
+                        <li class="list-group-item fs-14">کارگاه شماره سه</li>
+                    </ul>
                 </div>
             </div>
         </div>
     </div>
-
-    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
-        <div class="card">
-            <div class="card-header">اتاق‌های درمان</div>
-            <div class="card-body p-0">
-                <div class="table-responsive">
-                    <table class="table mb-0">
-                        <thead>
-                            <tr>
-                                <th>
-                                    {{__('Title')}}
-                                </th>
-                                <th>
-                                    {{__('Manager')}}
-                                </th>
-                                <th>
-                                </th>
-                            </tr>
-                        </thead>
-                    </table>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
-        <div class="card">
-            <div class="card-header">مدارک و اعتبارنامه‌ها</div>
-            <div class="card-body">
-                <div class="d-flex align-items-center bg-light p-2">
-                    <div>
-                        <i class="fal fa-2x fa-file"></i>
-                    </div>
-                    <div class="px-3">
-                        <div class="fs-12 font-weight-bold">نام فایل</div>
-                        <div class="fs-10">۱۱۰ کیلوبایت</div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 @endsection
