62894d4b3d9cf56728700e0e26d832bc055fc229
Ahmad Karimi <ahmadkarimi1991@gmail.com>
2020-06-01 17:30:26 +0430

    change connection items to cards

--- resources/views/dashboard/relationship-users/index.blade.php
+++ resources/views/dashboard/relationship-users/index.blade.php
@@ -105,4 +105,81 @@
             </div>
         </div>
     </div>
+    <div class="row">
+        @foreach ($users as $user)
+            <div class="col-xl-2">
+                <div class="card">
+                    <div class="card-header">
+                            <a href="#" class="fs-14 font-weight-bold">@displayName($user->user)</a>
+                    </div>
+                    <div class="card-body">
+                        <div class="d-flex justify-content-between align-items-baseline mb-3">
+                            <div class="fs-12">
+                                @if($user->allows('creator'))
+                                    <select name="creator_id" data-lijax="change" data-action="{{ route('dashboard.relationship.users.update', ['relationshipUser'=> $user->id]) }}" data-method="PUT">
+                                        @foreach ($user->allows('creator') as $item)
+                                            <option value="{{$item->id}}" {{$item->id == $user->creator->id ? 'selected' : ''}}>{{$item->name}}</option>
+                                        @endforeach
+                                    </select>
+                                @else
+                                    <a href="{{ $user->creator->route('show') }}">
+                                        @displayName($user->creator)
+                                    </a>
+                                @endif
+                            </div>
+                            <div class="fs-12">
+                                @if($user->allows('position'))
+                                    <select name="position" id="position" data-lijax="change" data-action="{{route('dashboard.relationship.users.update', ['relationshipUser'=> $user->id])}}" data-method="PUT">
+                                        @foreach ($user->allows('position') as $item)
+                                            <option value="{{$item}}" {{$user->position == $item ? 'selected' : ''}}>{{__(ucfirst($item))}}</option>
+                                        @endforeach
+                                    </select>
+                                @else
+                                    <span>
+                                        {{__(ucfirst($user->position))}}
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="fs-12">
+                            {{__(ucfirst($user->accepted_at))}}
+                        </div>
+                    </div>
+                    <div class="card-footer">
+                        <div class="fs-12">
+                            @if($user->allows('status'))
+                                <button class="btn btn-light btn-sm fs-10" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <i class="far fa-cog"></i>
+                                    <span>
+                                        @if($user->kicked_at)
+                                            {{__('Kicked') . ' '. $user->kicked_at}}
+                                        @elseif(!$user->accepted_at)
+                                            {{__('Awaiting')}}
+                                        @else
+                                            {{__('Accepted')}}
+                                        @endif
+                                    </span>
+                                </button>
+                                <div class="dropdown-menu">
+                                    @foreach ($user->allows('status') as $acception)
+                                        <a href="{{route('dashboard.relationship.users.update', ['relationshipUser'=> $user->id])}}" class="dropdown-item fs-10" data-lijax="click" data-method="PUT" data-name="status" data-value="{{$acception}}">
+                                            <i class="fal fa-user-secret"></i> {{__(ucfirst($acception))}}
+                                        </a>
+                                    @endforeach
+                                </div>
+                            @else
+                                @if($user->kicked_at)
+                                    {{__('Kicked') . ' '. $user->kicked_at}}
+                                @elseif(!$user->accepted_at)
+                                    {{__('Awaiting')}}
+                                @else
+                                    {{__('Accepted')}}
+                                @endif
+                            @endif
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
 @endsection
