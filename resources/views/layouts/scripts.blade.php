9e731cb831bf2393011a48172a2bf7ceab3f7e67
baravak <itb.baravak@gmail.com>
2020-06-23 17:16:12 +0430

    somecahgnge

--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -2,6 +2,6 @@
 @parent
 <script src="@staticVersion('/vendors/i18n/i18n.min.js')"></script>
 <script src="@staticVersion('/js/lang/fa.js')"></script>
-<script src="@staticVersion('/js/dashboard.js')"></script>
+<script src="@staticVersion('/js/dashboard.min.js')"></script>
 @endsection
 @include('layouts._scripts')
