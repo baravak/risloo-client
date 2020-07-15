c1343cf2fbe51d118557099de7bacc3035391fdb
baravak <itb.baravak@gmail.com>
2020-07-15 17:07:33 +0430

    bugfix

--- resources/views/dashboard/samples/show.blade.php
+++ resources/views/dashboard/samples/show.blade.php
@@ -5,7 +5,7 @@
     <div class="card-header">
         <h5 class="card-title">
             {{ $sample->scale->title }} <small> / {{$sample->client->name}}</small>
-            @if ($sample->status != 'scored')
+            @if ($sample->status != 'scored' || auth()->isAdmin())
                 <a href="{!!urldecode(route('dashboard.samples.scoring', $sample->id))!!}" data-method="POST" class="badge badge-info fs-12 p-1 lijax">
                     {{__("Scoring")}}
                 </a>
