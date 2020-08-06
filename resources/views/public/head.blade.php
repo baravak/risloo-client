a4825dea420d7f62806edc16b0130df36f5492f3
Ahmad Karimi <ahmadkarimi1991@gmail.com>
2020-08-06 16:42:10 +0430

    Fill meta tags with real data

--- resources/views/public/head.blade.php
+++ resources/views/public/head.blade.php
@@ -4,22 +4,22 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="HTML meta tags are a cornerstone of coding. But which are the most essential? We give you a rundown of all the meta tags you need to know.">
-        <meta name="keywords" content="keyword 1, keyword 2, keyword 3">
-        <meta name="author" content="Author name">
-        <meta name="copyright" content="Copyright owner">
+        <meta name="description" content="شما به عنوان روان‌شناس، روی کار اصلی خود تمرکز کنید؛ دغدغه کارهای جانبی درمان، آموزش و پژوهش را نداشته باشید و به خود آن‌ها فقط فکر کنید. ریسلو بستری یکپارچه است که کارهای شما را در این سه حوزه تسهیل می‌کند.">
+        <meta name="keywords" content="روان‌شناس, درمان, آموزش, پژوهش, ریسلو,">
+        <meta name="author" content="ریسلو">
+        <meta name="copyright" content="ریسلو">
         <meta name="robots" content="index,follow">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <meta property="og:title" content="European Travel Destinations">
-        <meta property="og:description" content="Offering tour packages for individuals or groups.">
-        <meta property="og:image" content="http://euro-travel-example.com/thumbnail.jpg">
-        <meta property="og:url" content="http://euro-travel-example.com/index.htm">
+        <meta property="og:title" content="ریسلو">
+        <meta property="og:description" content="شما به عنوان روان‌شناس، روی کار اصلی خود تمرکز کنید؛ دغدغه کارهای جانبی درمان، آموزش و پژوهش را نداشته باشید و به خود آن‌ها فقط فکر کنید. ریسلو بستری یکپارچه است که کارهای شما را در این سه حوزه تسهیل می‌کند.">
+        <meta property="og:image" content="https://risloo.ir/public/images/logo/logo.svg">
+        <meta property="og:url" content="https://risloo.ir/">
 
-        <meta name="twitter:title" content="European Travel Destinations ">
-        <meta name="twitter:description" content=" Offering tour packages for individuals or groups.">
-        <meta name="twitter:image" content=" http://euro-travel-example.com/thumbnail.jpg">
-        <meta name="twitter:card" content="summary_large_image">
+        <meta name="twitter:title" content="ریسلو">
+        <meta name="twitter:description" content="شما به عنوان روان‌شناس، روی کار اصلی خود تمرکز کنید؛ دغدغه کارهای جانبی درمان، آموزش و پژوهش را نداشته باشید و به خود آن‌ها فقط فکر کنید. ریسلو بستری یکپارچه است که کارهای شما را در این سه حوزه تسهیل می‌کند.">
+        <meta name="twitter:image" content="https://risloo.ir/public/images/logo/logo.svg">
+        <meta name="twitter:card" content="https://risloo.ir/public/images/logo/logo.svg">
 
         @yield('head-styles')
 
