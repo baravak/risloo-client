8d14d42ef88649f051f71bb96c510c2f18d4776a
abouhatef <itb.baravak@gmial.com>
2020-11-29 09:09:49 +0330

    bugfix

--- resources/views/dashboard/samples/show.blade.php
+++ resources/views/dashboard/samples/show.blade.php
@@ -139,11 +139,17 @@
                     <div class="form-group form-group-m">
                         <select type="text" data-action="{{urldecode(route('samples.storeItems', $sample->id, 1))}}" data-method="post" data-item="{{$loop->index + 1}}" data-name="items[{{$loop->index}}][1]" data-merge='{"items[{{$loop->index}}][0]" : {{$loop->index + 1}}}' data-lijax="change" id="item-{{$loop->index}}" class="form-items form-control form-control-m font-weight-bold d-notification" placeholder="&nbsp;" disabled>
                             <option></option>
-                            @foreach ($item->answer->options as $option)
-                                <option {{isset($item->user_answered) && $item->user_answered == $loop->index +1 ? 'selected' : ''}} value="{{$loop->index + 1}}">{{$loop->index + 1}}: {{$option}}</option>
+                            @foreach (isset($item->answer->options) ? $item->answer->options : $item->answer->optional_images as $option)
+                                <option {{isset($item->user_answered) && $item->user_answered == $loop->index +1 ? 'selected' : ''}} value="{{$loop->index + 1}}">
+                                    @isset($item->answer->options)
+                                        {{$loop->index + 1}}: {{$option}}
+                                    @else
+                                        {{$loop->index + 1}}
+                                    @endisset
+                                </option>
                             @endforeach
                         </select>
-                        <label for="item-{{$loop->index}}">{{$loop->index + 1}} - {{$item->text}}</label>
+                        <label for="item-{{$loop->index}}">{{$loop->index + 1}} - {{isset($item->text) ? $item->text : ''}}</label>
                     </div>
                 </div>
             @endforeach
