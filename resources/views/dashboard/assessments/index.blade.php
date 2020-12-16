557e87848baa05006e34dc2db23f93153de8752f
abouhatef <itb.baravak@gmial.com>
2020-12-16 15:14:07 +0330

    pagination assessments

--- resources/views/dashboard/assessments/index.blade.php
+++ resources/views/dashboard/assessments/index.blade.php
@@ -47,4 +47,9 @@
             </div>
         </div>
     </div>
+    <div class="row">
+        <div class="col-12 d-flex justify-content-center">
+            {{$assessments->links()}}
+        </div>
+    </div>
 @endsection
