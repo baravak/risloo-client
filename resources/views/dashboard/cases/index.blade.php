e810723cedfa6f02225f6ea8617ab6a3e4299714
abouhatef <itb.baravak@gmial.com>
2020-12-17 13:24:47 +0330

    cases pagination bugfix

--- resources/views/dashboard/cases/index.blade.php
+++ resources/views/dashboard/cases/index.blade.php
@@ -10,4 +10,9 @@
             @include($cases->count() ? 'dashboard.cases.list' : 'dashboard.emptyContent')
         </div>
     </div>
+    <div class="row">
+        <div class="col-12 d-flex justify-content-center">
+            {{$cases->links()}}
+        </div>
+    </div>
 @endsection
