7eef835a9797b08ed70186a713fa83f369c1ae89
abouhatef <itb.baravak@gmial.com>
2020-12-17 18:33:30 +0330

    change users type list

--- config/users.php
+++ config/users.php
@@ -4,21 +4,21 @@ return [
         'admin' => [
             'icon' => 'far fa-users-crown'
         ],
-        'counseling_center' => [
-            'icon' => 'far fa-hospital-alt'
+        'user' => [
+            'icon' => 'far fa-user'
         ],
         // 'manager' => [
         //     'icon' => 'fal fa-users-cog'
         // ],
-        'operator' => [
-            'icon' => 'fal fa-user-headset'
-        ],
-        'psychologist' => [
-            'icon' => 'fal fa-user-tie'
-        ],
-        'client' => [
-            'icon' => 'far fa-user-shield'
-        ]
+        // 'operator' => [
+        //     'icon' => 'fal fa-user-headset'
+        // ],
+        // 'psychologist' => [
+        //     'icon' => 'fal fa-user-tie'
+        // ],
+        // 'client' => [
+        //     'icon' => 'far fa-user-shield'
+        // ]
     ],
 
     'room_managers' => ['manager', 'operator', 'psychologist', 'under_supervision']
