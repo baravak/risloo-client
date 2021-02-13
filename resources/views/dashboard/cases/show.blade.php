70b1253433fb1ad9ad282e4c6288c6bbdec01124
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-02-13 19:59:49 +0330

    Update case show

--- resources/views/dashboard/cases/show.blade.php
+++ resources/views/dashboard/cases/show.blade.php
@@ -1,203 +1,127 @@
 @extends($layouts->dashboard)
+
 @section('content')
-<div class="row">
-    <div class="col-lg-12">
-        <div class="card mb-3">
-            <div class="card-header">
-                {!!__('Case of :users', ['users' => '<small>' . $case->clients->take(2)->pluck('user.name')->join(', ') . '</small>'])!!}
+    <div class="border border-gray-200 rounded p-4">
+        <div class="flex items-center dir-ltr text-left text-brand">
+            <i class="fal fa-copy mr-1"></i>
+            <span class="font-semibold text-sm">RS966669C</span>
+        </div>
+
+        <div class="mt-2">
+            <div class="flex items-center text-gray-700">
+                <i class="fal fa-file-alt ml-1"></i>
+                <span class="font-semibold">مسئله</span>
             </div>
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-sm-6 col-lg-6 col-xl-3 profile-separator">
-                        <div class="d-flex align-items-center mb-3">
-                            <div href="#" class="media media-xl rounded-circle">
-                                <img src="{{$case->manager->avatar_url->url('large')}}" alt="Avatar">
-                            </div>
-                            <div class="px-3">
-                                <div class="font-weight-bold">{{$case->manager->name}}</div>
-                                @if ($case->room->type == 'room')
-                                    <div class="fs-12">
-                                        <a href="{{route('dashboard.rooms.show', $case->room->id)}}" class="text-decoration-none">
-                                            {{__('Therapy room of :user' , ['user' => $case->room->manager->name])}}
-                                        </a>
-                                    </div>
-                                @endif
-                                <div class="fs-12">
-                                    <a href="{{route('dashboard.centers.show', $case->room->center->id)}}" class="text-decoration-none">
-                                        {{$case->room->center->detail->title}}
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-md-6 col-lg-6 col-xl-3 profile-separator">
-                        <div class="row">
-                            <div class="col-12">
-                                <span class="fs-12">{{__('Problem')}}: </span>
-                                {{$case->detail->chief_complaint}}
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-lg-6 col-xl-3 profile-separator">
-                        <div class="row">
-                            <div class="col-6">
-                                <span class="fs-12">تاریخ ساخت پرونده</span>
-                            </div>
-                            <div class="col-6">
-                                <span class="fs-12 font-weight-bold d-inline-block direction-ltr">@time($case->created_at, 'Y-m-d H:i')</span>
-                            </div>
+            <div class="text-sm text-gray-600 mt-1">
+                <p>ﻟﻮرم اﯾﭙﺴﻮم متن ﺳﺎﺧﺘﮕﯽ ﺑﺎ ﺗﻮﻟﯿﺪ ﺳﺎدﮔﯽ ﻧﺎﻣﻔﻬﻮم از ﺻﻨﻌﺖ ﭼﺎپ، و ﺑﺎ اﺳﺘﻔﺎده از ﻃﺮاﺣﺎن ﮔﺮاﻓﯿﮏ اﺳﺖ، ﭼﺎﭘﮕﺮﻫﺎ و ﻣﺘﻮن ﺑﻠﮑﻪ روزنامه و مجله در ستون و سطر آنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربرهای متنوع با هدف بهبود ابزراهای کاربردی می‌باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می‌طلبد.</p>
+            </div>
+        </div>
 
-                            <div class="col-6">
-                                <span class="fs-12">تاریخ آخرین جلسه</span>
-                            </div>
-                            <div class="col-6">
-                                <span class="fs-12 font-weight-bold d-inline-block direction-ltr">@time($case->created_at, 'Y-m-d H:i')</span>
-                            </div>
-                        </div>
-                    </div>
+        <div class="flex items-center text-xs text-gray-500 mt-4">
+                <i class="fal fa-clock ml-1"></i>
+                <span>۲۰ بهمن ۱۳۹۹ ساعت ۱۷:۴۵</span>
+        </div>
+    </div>
 
-                    <div class="col-sm-6 col-lg-6 col-xl-3">
-                        <div class="row">
-                            <div class="col-6">
-                                <span class="fs-12">جلسات مشاوره</span>
-                            </div>
-                            <div class="col-6">
-                                <span class="fs-12 font-weight-bold">۴۳</span>
-                            </div>
-
-                            <div class="col-6">
-                                <span class="fs-12">تست‌های گرفته‌شده</span>
-                            </div>
-                            <div class="col-6">
-                                <span class="fs-12 font-weight-bold">۱۵۰</span>
-                            </div>
-                        </div>
+    <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-8">
+        <div>
+            <div class="mb-4">
+                <h3 class="flex items-center font-bold text-gray-700 cursor-default">
+                    <span class="w-8 border-t border-gray-200 inline-block ml-3"></span>
+                    <span>روان‌شناسان</span>
+                </h3>
+            </div> 
+            <div class="grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3 gap-2">
+                <a href="#" class="flex items-center border border-gray-200 rounded px-3 py-2 hover:bg-gray-50 transition">
+                    <div class="flex justify-center items-center flex-shrink-0 w-12 h-12 rounded-full overflow-hidden ml-2 bg-gray-200 text-gray-800 text-xs">
+                        <span>م ل</span>
                     </div>
+                    <div class="flex-col items-center">
+                        <div class="font-medium text-sm text-gray-700">محمدجواد لواسانی</div>
+                        <div class="text-xs text-gray-600 mt-1">کد نظام روانپزشکی ۶۰۹۰۵۳۴۲۳</div>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div>
+            <div class="relative mb-4">
+                <div>
+                    <h3 class="flex items-center font-bold text-gray-700 cursor-default">
+                        <span class="w-8 border-t border-gray-200 inline-block ml-3"></span>
+                        <span>مراجعان</span>
+                    </h3>
                 </div>
+                <div>
+                    <button class="absolute top-0 left-0 flex justify-center items-center flex-shrink-0 border border-green-700 text-green-700 px-4 h-7 rounded-full text-xs leading-normal hover:bg-green-50 transition-all">
+                        <i class="fal fa-plus ml-2"></i>
+                        <span class="font-medium">افزودن مراجع</span>
+                    </button>
+                </div>
+            </div> 
+            <div class="grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3 gap-2">
+                <a href="#" class="flex items-center border border-gray-200 rounded px-3 py-2 hover:bg-gray-50 transition">
+                    <div class="flex justify-center items-center flex-shrink-0 w-12 h-12 rounded-full overflow-hidden ml-2 bg-gray-200 text-gray-800 text-xs">
+                        <span>م ر</span>
+                    </div>
+                    <div class="flex items-center">
+                        <div class="font-medium text-sm text-gray-700">محمدحسین رضوانی</div>
+                    </div>
+                </a>
+                <a href="#" class="flex items-center border border-gray-200 rounded px-3 py-2 hover:bg-gray-50 transition">
+                    <div class="flex justify-center items-center flex-shrink-0 w-12 h-12 rounded-full overflow-hidden ml-2 bg-gray-200 text-gray-800 text-xs">
+                        <span>م م</span>
+                    </div>
+                    <div class="flex items-center">
+                        <div class="font-medium text-sm text-gray-700">منیره سادات مرتضوی</div>
+                    </div>
+                </a>
             </div>
         </div>
     </div>
 
-
-    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
-        <div class="card">
-            <div class="card-header">
-                {{__('Clients')}}<sup>({{ $case->clients ? $case->clients->count() : 0 }})
-                    @can('dashboard.cases.manager', [$case])
-                        <small>
-                            <a href="{{route('dashboard.case.users.create', $case->id)}}" class="badge badge-primary p-1">{{__('Add client')}}</a>
-                        </small>
-                    @endcan
+    <div>
+        <div class="mb-4 mt-8">
+            <h3 class="flex items-center font-bold text-gray-700 cursor-default">
+                <span class="w-8 border-t border-gray-200 inline-block ml-3"></span>
+                <span>جلسات</span>
+            </h3>
+        </div>
+        
+        <div class="flex items-center justify-between mb-4">
+            <div class="w-1/2">
+                <input type="search" placeholder="جست‌وجو" class="w-full px-3 h-7 rounded border border-gray-200 font-light placeholder-gray-300">
             </div>
-            <div class="card-body">
-                @if ($case->clients)
-                    @foreach ($case->clients as $client)
-                        <div class="d-flex align-items-center bg-light p-2">
-                            <div class="px-3">
-                                <div class="fs-12 font-weight-bold">
-                                    @displayName($client->user)
-                                    @can('dashboard.cases.manager', [$case])
-                                        <a href="{{route('dashboard.samples.create', ['case' => $case->id, 'client' => $client->id])}}#case" class="badge badge-primary p-1">{{__('Create sampel')}}</a>
-                                    @endcan
+            <div>
+                <button class="flex justify-center items-center flex-shrink-0 border border-green-700 text-green-700 px-4 h-7 rounded-full text-xs leading-normal hover:bg-green-50 transition-all">
+                    <i class="fal fa-plus ml-2"></i>
+                    <span class="font-medium">افزودن جلسه</span>
+                </button>
+            </div>
+        </div>
+
+        <div class="overflow-x-auto">
+            <div class="overflow-hidden border border-gray-200 rounded">
+                <table class="min-w-full divide-y divide-gray-200">
+                    <thead class="bg-gray-50">
+                        <tr>
+                            <th class="px-3 py-2 text-right text-xs font-medium text-gray-500" scope="col">سریال</th>
+                            <th class="px-3 py-2 text-right text-xs font-medium text-gray-500" scope="col">زمان</th>
+                            <th class="px-3 py-2 text-right text-xs font-medium text-gray-500" scope="col">مدت جلسه</th>
+                            <th class="px-3 py-2 text-right text-xs font-medium text-gray-500" scope="col">وضعیت</th>
+                            <th class="px-3 py-2" scope="col"></th>
+                        </tr>
+                    </thead>
+                    <tbody class="divide-y divide-gray-200">
+                        <tr>
+                            <td class="px-3 py-2">
+                                <div claas="flex items-center">
+                                    <span class="text-xs text-gray-700 cursor-default">SE966669A</span>
                                 </div>
-                            </div>
-                        </div>
-                    @endforeach
-                @endif
-            </div>
-        </div>
-    </div>
-
-    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
-        <div class="card">
-            <div class="card-header">
-                {{__('Sessions & reserves')}}<sup>({{ $case->sessions ? $case->sessions->count() : 0 }})
-                    @can('dashboard.cases.manager', [$case])
-                        <small>
-                            <a href="{{route('dashboard.sessions.create', ['case' => $case->id])}}" class="badge badge-primary p-1">{{__('Add session')}}</a>
-                        </small>
-                    @endcan
-                <small>
-                    {{-- <a href="{{route('dashboard.sessions.create', ['case' => $case->id])}}" class="badge badge-secondary p-1">{{__('Reports')}}</a> --}}
-                </small>
-            </div>
-            <div class="card-body">
-                <table class="w-100 table table-striped">
-                    <thead>
-                        <tr>
-                            <th class="text-center">{{ __('#') }}</th>
-                            <th class="text-center">شروع</th>
-                            <th class="text-center">مدت جلسه</th>
-                            <th class="">وضعیت</th>
-                            <th></th>
+                            </td>
                         </tr>
-                    </thead>
-                    <tbody>
-                        @if ($case->sessions)
-                            @foreach ($case->sessions as $session)
-                                @include('dashboard.cases.show.session-list')
-                            @endforeach
-                        @endif
                     </tbody>
-            </table>
+                </table>
             </div>
         </div>
     </div>
-
-    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-4">
-        <div class="card">
-            <div class="card-header">
-                {{__('Samples')}}<sup>({{ $case->samples ? $case->samples->count() : 0 }})
-                    @can('dashboard.cases.manager', [$case])
-                    <small>
-                        <a href="{{route('dashboard.samples.create', ['case' => $case->id])}}#case" class="badge badge-primary p-1">{{__('Create sampel')}}</a>
-                    </small>
-                    @endcan
-            </div>
-            <div class="card-body">
-                <table class="w-100 table table-striped">
-                    <thead>
-                        <tr>
-                            <th class="text-center">{{ __('Title') }}</th>
-                            <th class="text-center">{{ __('Status') }}</th>
-                            <th class="">{{ __('Session') }}</th>
-                            <th class="">{{ __('Client') }}</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @if ($case->samples)
-                            @foreach ($case->samples as $sample)
-                                <tr class="m-2">
-                                    <td>
-                                        <span class="d-block fs-12">
-                                            {{ $sample->title }}
-                                        </span>
-                                        <small class="d-block direction-ltr font-weight-bold">
-                                            {{ $sample->id }}
-                                        </small>
-                                    </td>
-                                    <td>
-                                        {{ __(ucfirst($sample->status)) }}
-                                    </td>
-                                    <td>
-                                        {{ $sample->session_id }}
-                                    </td>
-                                    <td>
-                                        {{ $sample->client->name }}
-                                    </td>
-                                    <td>
-                                        @can('dashboard.cases.manager', [$case])
-                                            @include('components._showLink', ['href' => route('dashboard.samples.show', $sample->id)])
-                                        @endcan
-                                    </td>
-                                </tr>
-                            @endforeach
-                        @endif
-                    </tbody>
-            </table>
-            </div>
-        </div>
-    </div>
-</div>
 @endsection
