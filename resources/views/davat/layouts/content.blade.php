cd13750b1ba73f78233c9c8c596da7c458171d67
Ahmad Karimi <ahmadkarimi1991@gmail.com>
2021-02-14 12:12:37 +0330

    Minor fix

--- resources/views/davat/layouts/content.blade.php
+++ resources/views/davat/layouts/content.blade.php
@@ -1,4 +1,4 @@
-<div id="content" class="flex-1 p-4 mb-16" data-xhr="content">
+<div id="content" class="flex-1 p-4" data-xhr="content">
     @include('layouts.subheader')
     @yield('content')
 </div>
