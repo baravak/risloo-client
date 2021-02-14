dbca63ed87dffb127048307b63e4d9842e337e4c
Ahmad Karimi <ahmadkarimi1991@gmail.com>
2021-02-14 13:11:03 +0330

    Add search field to centers list

--- resources/views/dashboard/centers/list.blade.php
+++ resources/views/dashboard/centers/list.blade.php
@@ -1,3 +1,7 @@
+<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 mb-4 justify-between">
+    <input type="search" class="text-sm border border-gray-200 rounded-sm" placeholder="{{ __('Search') }}">
+</div>
+
 <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4">
     @foreach ($centers as $center)
         @include('dashboard.centers.listRaw')
