ee8bd77d5c4a9d1a0988cd9f20727a2f2ea37c4a
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-02-15 16:37:26 +0330

    Update session report

--- resources/views/dashboard/sessions/report.blade.php
+++ resources/views/dashboard/sessions/report.blade.php
@@ -6,9 +6,15 @@
         </h3>
     </div> 
     <div class="grid grid-cols-1">
-        <div href="#" class="flex items-center justify-between border border-gray-200 rounded p-4 hover:bg-gray-50 transition">
+        {{-- <div href="#" class="flex items-center justify-between border border-gray-200 rounded p-4 hover:bg-gray-50 transition">
             <span class="text-gray-500 text-xs">هنوز گزارشی ثبت نشده است</span>
-            <a href="#" class="flex items-center justify-center rounded-full text-xs text-green-600 border border-green-600 hover:bg-green-50 h-8 px-4">{{ __('Create report') }}</a>
+            <a href="#" class="flex items-center justify-center flex-shrink-0 rounded-full text-xs text-green-600 border border-green-600 hover:bg-green-50 h-8 px-4">{{ __('Create report') }}</a>
+        </div> --}}
+        <div href="#" class="border border-gray-200 rounded p-4 hover:bg-gray-50 transition">
+            <span class="block text-gray-600 text-sm">{{ $session->report }}</span>
+            <div class="text-left mt-2">
+                <a href="#" class="inline-flex justify-center items-center text-xs text-gray-600 border border-gray-600 hover:bg-gray-50 rounded-full h-7 px-4">ویرایش جلسه</a>
+            </div>
         </div>
     </div>
 </div>
