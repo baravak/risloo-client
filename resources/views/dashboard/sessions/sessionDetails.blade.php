85542bc5540b6609dd0402e661a178b07df8a9c3
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-02-15 11:28:49 +0330

    Update session details

--- resources/views/dashboard/sessions/sessionDetails.blade.php
+++ resources/views/dashboard/sessions/sessionDetails.blade.php
@@ -2,26 +2,23 @@
     <div>
         <div class="flex items-center text-sm text-gray-700">
             <i class="fal fa-calendar-alt leading-normal ml-2"></i>
-            <span>دوشنبه ۲۲ بهمن ۱۳۹۹ ساعت ۱۴:۰۰</span>
+            <span>@time($session->started_at, '%A %d %B %y ساعت H:i')</span>
         </div>
         <div class="flex items-center text-sm text-gray-700 mt-2">
             <i class="fal fa-clock leading-normal ml-2"></i>
-            <span>۹۰ دقیقه</span>
+            <span>{{ __(':time minute(s)', ['time' => $session->duration]) }}</span>
         </div>
         <div class="flex items-center text-sm text-gray-700 mt-2">
-            {{-- <span class="text-sm text-yellow-500">در انتظار تشکیل جلسه</span> --}}
-            {{-- <span class="text-sm text-red-500">لغو شده توسط مراجع</span> --}}
-            <span class="text-sm text-green-500">در جلسه</span>
-            {{-- <span class="text-sm text-gray-500">پایان یافته</span> --}}
+            <span class="text-sm text-gray-400">{{ __(ucfirst($session->status)) }}</span>
         </div>
     </div>
     <div class="flex flex-col justify-between">
         <div class="flex items-center dir-ltr text-left text-brand">
             <i class="fal fa-clipboard mr-2 text-xl"></i>
-            <span class="font-semibold text-sm">SE9666669</span>
+            <span class="font-semibold text-sm">{{ $session->id }}</span>
         </div>
         <div>
-            <a href="#" class="flex items-center justify-center border border-gray-500 rounded-full text-xs text-gray-600 h-7 px-2 hover:bg-gray-50">ویرایش جلسه</a>
+            <a href="#" class="flex items-center justify-center border border-gray-500 rounded-full text-xs text-gray-600 h-7 px-2 hover:bg-gray-50">{{ __('Edit session') }}</a>
         </div>
     </div>
 </div>
