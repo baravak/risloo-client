2d3e8e9c9f60c5919847c813e86333e16d33ef17
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-02-15 18:57:38 +0330

    Update assessments

---
+++ resources/views/dashboard/assessments/index.blade.bak.php
@@ -0,0 +1,55 @@
+@extends($layouts->dashboard)
+
+@section('content')
+    <div class="card mb-3">
+        <div class="card-header">
+            {{ __('Assessments') }} <sup>({{ $assessments->total() }})</sup>
+            @filterBadge($assessments)
+        </div>
+        <div class="card-body p-0">
+            <div class="table-responsive">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>@sortView($assessments, 'id', '#')</th>
+                            <th>@sortView($assessments, 'scale')</th>
+                            <th>@sortView($assessments, 'edition')</th>
+                            <th>@sortView($assessments, 'version')</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($assessments as $assessment)
+                            <tr>
+                                <td>
+                                    @id($assessment)
+                                </td>
+                                <td>
+                                    {{$assessment->title}}
+                                </td>
+                                <td>
+                                    {{$assessment->edition}}
+                                </td>
+                                <td>
+                                    {{$assessment->version}}
+                                </td>
+                                <td>
+                                    @can('dashboard.sessions.create')
+                                        <a href="{{route('dashboard.samples.create', ['scale' => substr($assessment->id, 1)])}}" class="text-decoration-none">
+                                            <i class="fas fa-flask fs-14"></i> {{__('Create sample')}}
+                                        </a>
+                                    @endcan
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-12 d-flex justify-content-center">
+            {{$assessments->links()}}
+        </div>
+    </div>
+@endsection
--- resources/views/dashboard/assessments/index.blade.php
+++ resources/views/dashboard/assessments/index.blade.php
@@ -1,55 +1,18 @@
 @extends($layouts->dashboard)
 
 @section('content')
-    <div class="card mb-3">
-        <div class="card-header">
-            {{ __('Assessments') }} <sup>({{ $assessments->total() }})</sup>
-            @filterBadge($assessments)
+    <div>
+        <div class="mb-4 mt-8">
+            <h3 class="flex items-center font-bold text-gray-700 cursor-default">
+                <span class="w-8 border-t border-gray-200 inline-block ml-3"></span>
+                <span>{{ __('Assessments') }}</span>
+                <span class="text-xs text-gray-600 font-light mr-1">({{ $assessments->total() }})</span>
+            </h3>
         </div>
-        <div class="card-body p-0">
-            <div class="table-responsive">
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th>@sortView($assessments, 'id', '#')</th>
-                            <th>@sortView($assessments, 'scale')</th>
-                            <th>@sortView($assessments, 'edition')</th>
-                            <th>@sortView($assessments, 'version')</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($assessments as $assessment)
-                            <tr>
-                                <td>
-                                    @id($assessment)
-                                </td>
-                                <td>
-                                    {{$assessment->title}}
-                                </td>
-                                <td>
-                                    {{$assessment->edition}}
-                                </td>
-                                <td>
-                                    {{$assessment->version}}
-                                </td>
-                                <td>
-                                    @can('dashboard.sessions.create')
-                                        <a href="{{route('dashboard.samples.create', ['scale' => substr($assessment->id, 1)])}}" class="text-decoration-none">
-                                            <i class="fas fa-flask fs-14"></i> {{__('Create sample')}}
-                                        </a>
-                                    @endcan
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
+        
+        <div class="flex items-center w-full mb-4">
+            <input type="search" placeholder="{{ __('Search') }}" class="w-full px-3 h-7 rounded border border-gray-200 font-light text-sm placeholder-gray-300">
         </div>
-    </div>
-    <div class="row">
-        <div class="col-12 d-flex justify-content-center">
-            {{$assessments->links()}}
-        </div>
+        @include($assessments && $assessments->count() ? 'dashboard.assessments.assessmentsList' : 'dashboard.assessments.emptyAssessments')
     </div>
 @endsection
