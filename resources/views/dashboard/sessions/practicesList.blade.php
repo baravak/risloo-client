af58249216bc1e4fce6de068bb0064466a781a04
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-02-15 17:23:15 +0330

    Update session practices list

--- resources/views/dashboard/sessions/practicesList.blade.php
+++ resources/views/dashboard/sessions/practicesList.blade.php
@@ -39,6 +39,33 @@
                                 </div>
                             </td>
                         </tr>
+                        <tr>
+                            <td class="px-3 py-2 whitespace-nowrap">
+                                <div claas="flex items-center">
+                                    <span class="text-xs text-gray-700 block text-right dir-ltr cursor-default">P-9666697</span>
+                                </div>
+                            </td>
+                            <td class="px-3 py-2 whitespace-nowrap">
+                                <div claas="flex items-center">
+                                    <span class="text-xs text-gray-700 cursor-default">تمرین شماره 1</span>
+                                </div>
+                            </td>
+                            <td class="px-3 py-2 whitespace-nowrap">
+                                <div claas="flex items-center">
+                                    <span class="text-xs text-gray-700 cursor-default">با دقت به دیزاین نگاه بینداز و آنالیز کن. آفرین</span>
+                                </div>
+                            </td>
+                            <td class="px-3 py-2 whitespace-nowrap">
+                                <div claas="flex items-center">
+                                    <a href="#"><i class="fal fa-download text-smleading-relaxed text-gray-600 hover:text-blue-600"></i></a>
+                                </div>
+                            </td>
+                            <td class="px-3 py-2 whitespace-nowrap">
+                                <div claas="flex items-center">
+                                    <span class="text-xs text-gray-700 cursor-default">ندارد</span>
+                                </div>
+                            </td>
+                        </tr>
                 </tbody>
             </table>
         </div>
