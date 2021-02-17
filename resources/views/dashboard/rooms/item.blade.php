bb47fd23587045e23115e44e31ec6a7a19277560
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-02-17 17:59:54 +0330

    Update room item

--- resources/views/dashboard/rooms/item.blade.php
+++ resources/views/dashboard/rooms/item.blade.php
@@ -1,4 +1,4 @@
-<a href="#" class="flex items-center sm:block border border-gray-200 rounded p-4 sm:px-4 sm:py-10 hover:bg-gray-50 overflow-hidden transition">
+<a href="{{ $room->route('show') }}" class="flex items-center sm:block border border-gray-200 rounded p-4 sm:px-4 sm:py-10 hover:bg-gray-50 overflow-hidden transition">
     <div class="flex justify-center items-center flex-shrink-0 w-20 h-20 ml-4 mb-0 sm:mb-4 sm:mx-auto bg-gray-300 text-gray-600 rounded-full border border-gray-200 overflow-hidden">
         @avatarOrName($room->manager)
     </div>
