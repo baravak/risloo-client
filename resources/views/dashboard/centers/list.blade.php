275b16521d4a29eafe5fd52c7704d24ede1d70d6
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-02-17 14:14:06 +0330

    Update centers

--- resources/views/dashboard/centers/list.blade.php
+++ resources/views/dashboard/centers/list.blade.php
@@ -6,7 +6,7 @@
     </a>
 </div>
 
-<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4">
+<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">
     @foreach ($centers as $center)
         @include('dashboard.centers.listRaw')
     @endforeach
