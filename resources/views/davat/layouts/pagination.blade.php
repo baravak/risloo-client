84ae1f21889bf73a6090acb2e91e84249aa12b31
Ahmad Karimi <ahmadkarimi1991@gmail.com>
2021-02-17 16:40:42 +0330

    Fix pagination

--- resources/views/davat/layouts/pagination.blade.php
+++ resources/views/davat/layouts/pagination.blade.php
@@ -2,9 +2,11 @@
     <nav class="flex justify-center items-center flex-wrap mt-8 text-sm font-medium">
         {{-- Previous Page Link --}}
         @if ($paginator->onFirstPage())
-            <span class="flex justify-center items-center flex-shrink-0 h-7 ml-2 text-gray-300 cursor-not-allowed">{{ __('Previous') }}</span>
+            @if (false)
+                <span class="flex justify-center items-center flex-shrink-0 h-7 ml-2 text-gray-300 cursor-not-allowed">{{ __('Previous') }}</span>
+            @endif
         @else
-            <a href="{{ $paginator->previousPageUrl() }}" class="flex justify-center items-center flex-shrink-0 h-7 ml-2 text-gray-700">{{ __('Previous') }}</a>
+            <a href="{{ $paginator->previousPageUrl() }}" class="flex justify-center items-center flex-shrink-0 h-7 ml-2 text-gray-700 leading-normal">{{ __('Previous') }}</a>
         @endif
 
         {{-- Pagination Elements --}}
@@ -18,9 +20,9 @@
             @if (is_array($element))
                 @foreach ($element as $page => $url)
                     @if ($page == $paginator->currentPage())
-                        <span class="flex justify-center items-center flex-shrink-0 w-7 h-7 ml-2 bg-brand text-white rounded-full cursor-default">{{ $page }}</span>
+                        <span class="flex justify-center items-center flex-shrink-0 w-7 h-7 ml-2 bg-brand text-white rounded-full cursor-default leading-normal">{{ $page }}</span>
                     @else
-                        <a href="{{ $url }}" class="flex justify-center items-center flex-shrink-0 w-7 h-7 ml-2 text-gray-700 rounded-full hover:bg-gray-100 transition">{{ $page }}</a>
+                        <a href="{{ $url }}" class="flex justify-center items-center flex-shrink-0 w-7 h-7 ml-2 text-gray-700 rounded-full hover:bg-gray-100 transition leading-normal">{{ $page }}</a>
                     @endif
                 @endforeach
             @endif
@@ -28,9 +30,11 @@
 
         {{-- Next Page Link --}}
         @if ($paginator->hasMorePages())
-            <a href="{{ $paginator->nextPageUrl() }}" class="flex justify-center items-center flex-shrink-0 h-7 text-gray-700">{{ __('Next') }}</a>
+            <a href="{{ $paginator->nextPageUrl() }}" class="flex justify-center items-center flex-shrink-0 h-7 text-gray-700 leading-normal">{{ __('Next') }}</a>
         @else
-            <span class="flex justify-center items-center flex-shrink-0 h-7 text-gray-300 cursor-not-allowed">{{ __('Next') }}</span>
+            @if (false)
+                <span class="flex justify-center items-center flex-shrink-0 h-7 text-gray-300 cursor-not-allowed">{{ __('Next') }}</span>
+            @endif
         @endif
     </nav>
 @endif
