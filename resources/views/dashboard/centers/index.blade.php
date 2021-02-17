88f058cebb401e1ebe06e4784eeb5e0ea65dd66a
abouhatef <itb.baravak@gmial.com>
2021-02-17 20:53:17 +0330

    enable quick_search in centers

--- resources/views/dashboard/centers/index.blade.php
+++ resources/views/dashboard/centers/index.blade.php
@@ -1,6 +1,13 @@
 @extends($layouts->dashboard)
 
 @section('content')
-    @include($centers->count() ? 'dashboard.centers.list' : 'dashboard.emptyContent')
+    <div class="flex justify-between items-center flex-wrap mb-4">
+        <input data-basePage="{{isset($global->page) ? $global->page : ''}}" data-xhrBase="center-items" data-lijax="300" data-name="q" data-state="both" id="quick_search" value="{{request()->q}}" type="search" class="flex-1 ml-4 text-sm border border-gray-200 rounded-sm" placeholder="{{ __('Search') }}">
+        <a href="{{ route('dashboard.centers.create') }}" class="flex items-center justify-center flex-shrink-0 w-10 sm:w-auto h-10 sm:px-4 text-sm text-green-700 border border-green-700 rounded-full hover:bg-green-50 transition" title="{{ __('Create new center') }}">
+            <i class="fal fa-plus sm:ml-2"></i>
+            <span class="hidden sm:inline">{{ __('Create new center') }}</span>
+        </a>
+    </div>
+    @include('dashboard.centers.list')
     {{ $centers->links() }}
 @endsection
--- resources/views/dashboard/centers/list.blade.php
+++ resources/views/dashboard/centers/list.blade.php
@@ -1,13 +1,9 @@
-<div class="flex justify-between items-center flex-wrap mb-4">
-    <input type="search" class="flex-1 ml-4 text-sm border border-gray-200 rounded-sm" placeholder="{{ __('Search') }}">
-    <a href="{{ route('dashboard.centers.create') }}" class="flex items-center justify-center flex-shrink-0 w-10 sm:w-auto h-10 sm:px-4 text-sm text-green-700 border border-green-700 rounded-full hover:bg-green-50 transition" title="{{ __('Create new center') }}">
-        <i class="fal fa-plus sm:ml-2"></i>
-        <span class="hidden sm:inline">{{ __('Create new center') }}</span>
-    </a>
-</div>
-
-<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">
-    @foreach ($centers as $center)
-        @include('dashboard.centers.listRaw')
-    @endforeach
+<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4" data-xhr="center-items">
+    @if ($centers->count())
+        @foreach ($centers as $center)
+            @include('dashboard.centers.listRaw')
+        @endforeach
+    @else
+        @include('dashboard.emptyContent')
+    @endif
 </div>
