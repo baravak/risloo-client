b262828e7466bc50300f768d05367469acbdb073
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-02-18 21:28:26 +0330

    Update assessments

--- resources/views/dashboard/assessments/emptyAssessments.blade.php
+++ resources/views/dashboard/assessments/emptyAssessments.blade.php
@@ -1,3 +1,3 @@
 <div class="flex items-center justify-center border border-gray-200 rounded text-sm text-gray-400 px-4 py-8">
-    <span>هنوز هیچ ارزیابی‌ای در سامانه بارگزاری نشده است</span>
+    <span>{{ __('Empty Assessments Error') }}</span>
 </div>
