d6d622cc150e4d7e054430d2727b07a5bbf8a099
abouhatef <itb.baravak@gmial.com>
2021-02-18 11:29:11 +0330

    update fa.json & center remove personal clinic prefix detail

--- resources/views/dashboard/centers/listRaw.blade.php
+++ resources/views/dashboard/centers/listRaw.blade.php
@@ -2,16 +2,26 @@
     <div class="h-24 bg-gray-100 border-b border-gray-200"></div>
 
     <div class="flex justify-center items-center flex-shrink-0 w-24 h-24 mx-auto -mt-12 bg-gray-300 text-gray-600 rounded-full border-4 border-white overflow-hidden">
-        @avatarOrName($center->detail)
+        @if ($center->type == 'personal_clinic')
+                @avatarOrName($center->manager)
+            @else
+                @avatarOrName($center->detail)
+            @endif
     </div>
 
     <div class="p-4">
-        <div class="text-sm sm:text-base text-center text-gray-700 font-bold">@displayName($center->detail)</div>
+        <div class="text-sm sm:text-base text-center text-gray-700 font-bold">
+            @if ($center->type == 'personal_clinic')
+                @displayName($center->manager)
+            @else
+                @displayName($center->detail)
+            @endif
+        </div>
 
         @if ($center->type == 'counseling_center')
             <div class="text-xs text-center text-gray-700 mt-2">{{ $center->manager->name }}</div>
         @else
-            <div class="text-xs text-center text-gray-500 mt-2">{{ __('Clinic') }}</div>
+            <div class="text-xs text-center text-gray-500 mt-2">{{ __('Personal clinic') }}</div>
         @endif
     </div>
 </a>
