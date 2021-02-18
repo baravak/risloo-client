5a0106f89f86dd1f3eee5c41b414d4228587b054
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-02-18 15:32:31 +0330

    Update room

--- resources/views/dashboard/rooms/show.blade.php
+++ resources/views/dashboard/rooms/show.blade.php
@@ -5,56 +5,30 @@
         <div class="h-20 sm:h-28 bg-gradient-to-b from-blue-100 to-blue-50 border-b border-gray-200"></div>
         <div class="relative p-4">
                 <div class="absolute top-3 left-3 flex">
-                    <button class="flex justify-center items-center flex-shrink-0 border border-green-700 text-green-700 px-4 h-9 rounded-full text-sm leading-normal hover:bg-green-50 transition-all">
-                        <i class="fal fa-plus ml-2"></i>
-                        <span class="font-medium">افزودن پرونده</span>
-                    </button>
+                    <a href="#" class="flex justify-center items-center flex-shrink-0 border border-gray-500 text-gray-600 hover:bg-gray-100 px-4 h-9 rounded-full text-sm leading-normal transition-all">
+                        <span class="font-medium">{{ __('Edit') }}</span>
+                    </a>
                 </div>
             <div class="flex justify-center items-center flex-shrink-0 w-24 h-24 md:w-32 md:h-32 -mt-16 md:-mt-20 bg-gray-300 text-gray-600 text-2xl rounded-full border-4 border-white overflow-hidden mb-4 relative">@avatarOrName($room->manager)</div>
             <h2 class="font-bold text-lg text-gray-900 cursor-default">@displayName($room->manager)</h2>
         </div>
     </div>
 
-    <div class="flex items-center mt-8 mb-4 cursor-default">
-        <span class="w-8 border-t border-gray-200 inline-block ml-3"></span>
-        <h3 class="font-bold text-gray-800">{{ __('Cases') }}</h3>
+    <div class="mb-4 mt-8">
+        <h3 class="flex items-center font-bold text-gray-700 cursor-default">
+            <span class="w-8 border-t border-gray-200 inline-block ml-3"></span>
+            <span>{{ __('Cases') }}</span>
+            <span class="text-xs text-gray-600 font-light mr-2" data-xhr="total">({{ $cases->total() }})</span>
+        </h3>
     </div>
 
-    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">
-        @foreach ($cases as $case)
-            <a href="#" class="border border-gray-200 rounded hover:bg-gray-50">
-                <div class="p-4">
-                    <div class="flex items-center justify-between">
-                        <div>
-                            <span class="text-green-600 text-xs">{{ $case->status }}</span>
-                        </div>
-                        <div class="dir-ltr text-left text-gray-500">
-                            <i class="fal fa-copy mr-1"></i>
-                            <span class="font-semibold text-sm">{{ $case->id }}</span>
-                        </div>
-                    </div>
-                    <div class="mt-4">
-                        @foreach ($case->clients as $client)
-                        <div class="flex items-center">
-                            <div class="flex items-center justify-center flex-shrink-0 w-8 h-8 bg-gray-300 text-xs text-gray-600 rounded-full border-2 border-white overflow-hidden ml-1">
-                                @avatarOrName($client->user)
-                            </div>
-                            <span class="text-xs text-gray-500">@displayName($client->user)</span>
-                        </div>
-                        @endforeach
-                    </div>
-                </div>
-                <div class="flex items-center justify-between bg-gray-50 border-t border-gray-200 mt-2 px-4 py-3">
-                        <div class="text-xs text-gray-500">
-                            <i class="fal fa-clock ml-1"></i>
-                            <span>۲۰ آذر ۱۳۹۹</span>
-                        </div>
-                        <div class="text-xs text-gray-500">
-                            <i class="fal fa-user-friends ml-1"></i>
-                            <span>۳ جلسه</span>
-                        </div>
-                </div>
-            </a>
-        @endforeach
+    <div class="flex justify-between items-center flex-wrap mb-4">
+        @include('layouts.quick_search')
+        <a href="{{ route('dashboard.cases.create') }}" class="flex items-center justify-center flex-shrink-0 w-10 sm:w-auto h-10 sm:px-4 text-sm text-green-700 border border-green-700 rounded-full hover:bg-green-50 transition" title="{{ __('Create new case') }}">
+            <i class="fal fa-plus sm:ml-2"></i>
+            <span class="hidden sm:inline">{{ __('Create new case') }}</span>
+        </a>
     </div>
+
+    @include($cases->count() ? 'dashboard.cases.items' : 'dashboard.emptyContent')
 @endsection
