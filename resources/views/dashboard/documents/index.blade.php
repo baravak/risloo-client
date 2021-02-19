7e5ac8d140c6cba381aa636156713f7c556a0169
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-02-19 13:29:45 +0330

    Update documents

--- resources/views/dashboard/documents/index.blade.php
+++ resources/views/dashboard/documents/index.blade.php
@@ -1,27 +1,25 @@
 @extends($layouts->dashboard)
 
 @section('content')
-<div>
-    <div class="mb-4 mt-8">
-        <h3 class="flex items-center font-bold text-gray-700 cursor-default">
-            <span class="w-8 border-t border-gray-200 inline-block ml-3"></span>
-            <span>{{ __('Documents') }}</span>
-            <span class="text-xs text-gray-600 font-light mr-2">({{ $documents->total() }})</span>
-        </h3>
-    </div>
-    
-    <div class="flex items-center justify-between mb-4">
-        <div class="w-1/2">
-            <input type="search" placeholder="{{ __('Search') }}" class="w-full px-3 h-7 rounded border border-gray-200 font-light text-sm placeholder-gray-300">
+    <div>
+        <div class="mb-4 mt-8">
+            <h3 class="flex items-center font-bold text-gray-700 cursor-default">
+                <span class="w-8 border-t border-gray-200 inline-block ml-3"></span>
+                <span>{{ __('Documents') }}</span>
+                <span class="text-xs text-gray-600 font-light mr-2" data-xhr="total">({{ $documents->total() }})</span>
+            </h3>
         </div>
-        <div>
-            <a href="{{ route('dashboard.documents.create') }}" alt="{{ __('Create new document') }}" class="flex justify-center items-center flex-shrink-0 border border-green-700 text-green-700 px-4 h-7 rounded-full text-xs leading-normal hover:bg-green-50 transition-all">
-                <i class="fal fa-plus ml-2"></i>
-                <span class="font-medium">{{ __('Create new document') }}</span>
+
+        <div class="flex justify-between items-center flex-wrap mb-4">
+            @include('layouts.quick_search')
+            <a href="{{ route('dashboard.documents.create') }}" class="flex items-center justify-center flex-shrink-0 w-10 sm:w-auto h-9 sm:px-4 text-sm text-green-700 border border-green-700 rounded-full hover:bg-green-50 transition mr-4" title="{{ __('Create new document') }}">
+                <i class="fal fa-plus sm:ml-2"></i>
+                <span class="hidden sm:inline">{{ __('Create new document') }}</span>
             </a>
         </div>
+        
+        @include($documents->count() ? 'dashboard.documents.list' : 'dashboard.documents.emptyDocuments')
+
+        {{$documents->links()}}
     </div>
-    @include($documents->count() ? 'dashboard.documents.list' : 'dashboard.documents.emptyDocuments')
-</div>
-{{$documents->links()}}
 @endsection
