6f722b6b75043912ced5c17ceefbd3f7366605ce
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-02-20 14:14:48 +0330

    Minor fix

--- resources/views/dashboard/create.blade.php
+++ resources/views/dashboard/create.blade.php
@@ -3,7 +3,7 @@
 @section('content')
                 @yield('before_content')
                 @section('form-tag')
-                    <form class="m-auto w-full sm:w-1/3" action="@yield('form_action', request()->create(isset(${$module->result}) ? ${$module->result}->route('update') : route("$module->resource.store", ($module->parent ? request()->route()->parameters[$module->parent] : null)), 'GET', request()->all())->getUri())" method="POST">
+                    <form class="m-auto w-full sm:w-1/2 md:w-1/3" action="@yield('form_action', request()->create(isset(${$module->result}) ? ${$module->result}->route('update') : route("$module->resource.store", ($module->parent ? request()->route()->parameters[$module->parent] : null)), 'GET', request()->all())->getUri())" method="POST">
                         @csrf
                         <input type="hidden" name="_method" value="{{$module->action == 'edit' ? 'PUT' : 'POST'}}">
                             @yield('form_content')
@@ -12,7 +12,7 @@
                                     @yield('form-title', __(($module->action == 'create' ? "Create " : 'Edit ') . $module->singular))
                                 </button>
                                 @if(request()->callback)
-                                    <a href="{{request()->callback}}" class="text-gray-500 hover:text-gray-700">{{ __('Cancel') }}</a>
+                                    <a href="{{request()->callback}}" class="text-gray-500 hover:text-gray-700 text-sm">{{ __('Cancel') }}</a>
                                     @elseif(Route::has($module->resource . '.index'))
                                     <a href="{{route($module->resource . '.index', $module->parent ? request()->route()->parameters[$module->parent] : null)}}" class="text-gray-500 hover:text-gray-700">{{ __('Cancel') }}</a>
                                 @endif
