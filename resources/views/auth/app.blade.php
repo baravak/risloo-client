3bd18484ac4ddabf1b3f94f25738f4680831cf79
Ahmad Karimi <ahmadkarimi1991@gmail.com>
2021-02-20 19:36:50 +0330

    Add images to enter page

--- resources/views/auth/app.blade.php
+++ resources/views/auth/app.blade.php
@@ -35,14 +35,16 @@
         </div>
 
         <div class="absolute bottom-0 w-full">
-            <img src="/images/graphics/001.png" alt="{{ __('App Title') }}" class="w-auto max-h-60 mx-auto 2xl:hidden">
-            <img src="/images/graphics/002.png" alt="{{ __('App Title') }}" class="w-auto max-h-60 mx-auto hidden">
-            <img src="/images/graphics/003.png" alt="{{ __('App Title') }}" class="w-auto max-h-60 mx-auto hidden">
-            <img src="/images/graphics/004.png" alt="{{ __('App Title') }}" class="w-auto max-h-60 mx-auto hidden">
-            <img src="/images/graphics/005.png" alt="{{ __('App Title') }}" class="w-auto max-h-60 mx-auto hidden">
-            <img src="/images/graphics/006.png" alt="{{ __('App Title') }}" class="w-auto max-h-60 mx-auto hidden">
-            <img src="/images/graphics/007.png" alt="{{ __('App Title') }}" class="w-auto max-h-60 mx-auto hidden">
-            <img src="/images/graphics/main-linear.png" alt="{{ __('App Title') }}" class="w-auto max-h-60 mx-auto hidden 2xl:block">
+            <img src="/images/graphics/1.png" alt="{{ __('App Title') }}" class="w-auto max-h-60 mx-auto md:hidden">
+            <img src="/images/graphics/2.png" alt="{{ __('App Title') }}" class="w-auto max-h-60 mx-auto hidden">
+            <img src="/images/graphics/3.png" alt="{{ __('App Title') }}" class="w-auto max-h-60 mx-auto hidden">
+            <img src="/images/graphics/4.png" alt="{{ __('App Title') }}" class="w-auto max-h-60 mx-auto hidden">
+            <img src="/images/graphics/5.png" alt="{{ __('App Title') }}" class="w-auto max-h-60 mx-auto hidden">
+            <img src="/images/graphics/6.png" alt="{{ __('App Title') }}" class="w-auto max-h-60 mx-auto hidden">
+            <img src="/images/graphics/7.png" alt="{{ __('App Title') }}" class="w-auto max-h-60 mx-auto hidden">
+            <img src="/images/graphics/linear-2.png" alt="{{ __('App Title') }}" class="w-full mx-auto hidden md:block xl:hidden">
+            <img src="/images/graphics/linear-3.png" alt="{{ __('App Title') }}" class="w-full mx-auto hidden xl:block 2xl:hidden">
+            <img src="/images/graphics/linear-5.png" alt="{{ __('App Title') }}" class="w-full hidden 2xl:block">
         </div>
     </div>
 @endsection
