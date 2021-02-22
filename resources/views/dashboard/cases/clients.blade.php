2e1428fdb93855a38b49d7bc9f33ff85bcf444af
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-02-22 18:21:20 +0330

    Minor fix

--- resources/views/dashboard/cases/clients.blade.php
+++ resources/views/dashboard/cases/clients.blade.php
@@ -18,14 +18,14 @@
     </div>
     <div class="grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3 gap-2">
         @foreach ($case->clients as $client)
-            <span class="flex items-center border border-gray-200 rounded px-3 py-2 hover:bg-gray-50 transition">
+            <div class="flex items-center border border-gray-200 rounded px-3 py-2 hover:bg-gray-50 transition cursor-default">
                 <div class="flex justify-center items-center flex-shrink-0 w-12 h-12 rounded-full overflow-hidden ml-2 bg-gray-200 text-gray-800 text-xs">
                     {{-- @avatarOrName($client) --}}
                 </div>
                 <div class="flex items-center">
                     <div class="font-medium text-sm text-gray-700">@displayName($client->user)</div>
                 </div>
-            </span>
+            </div>
         @endforeach
     </div>
 </div>
--- resources/views/dashboard/cases/therapists.blade.php
+++ resources/views/dashboard/cases/therapists.blade.php
@@ -7,7 +7,7 @@
         </h3>
     </div>
     <div class="grid grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3 gap-2">
-        <span class="flex items-center border border-gray-200 rounded px-3 py-2 hover:bg-gray-50 transition">
+        <div class="flex items-center border border-gray-200 rounded px-3 py-2 hover:bg-gray-50 transition cursor-default">
             <div class="flex justify-center items-center flex-shrink-0 w-12 h-12 rounded-full overflow-hidden ml-2 bg-gray-200 text-gray-800 text-xs">
                 @avatarOrName($case->manager)
             </div>
@@ -15,6 +15,6 @@
                 <div class="font-medium text-sm text-gray-700">@displayName($case->manager)</div>
                 {{-- <div class="text-xs text-gray-600 mt-1">کد نظام روانپزشکی ۶۰۹۰۵۳۴۲۳</div> --}}
             </div>
-        </span>
+        </div>
     </div>
 </div>
