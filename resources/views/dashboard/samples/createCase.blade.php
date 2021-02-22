8b19d25fc96d085f9a8f324c151cd45cd4c01860
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-02-22 09:59:33 +0330

    Update create sample

--- resources/views/dashboard/samples/createCase.blade.php
+++ resources/views/dashboard/samples/createCase.blade.php
@@ -7,57 +7,24 @@
             @endisset
         </select>
     </div>
-</div>
 
-    <div class="form-group mb-0" id="client-list">
-        <div id="client-null-tamplate" class="{{ isset($case) ? 'hidden' : '' }}">
-            <label>{{__('Clients')}}</label>
-        </div>
+    <div class="mt-4 {{ isset($case) ? 'hidden' : '' }}">
+        <h3 class="block mb-2 text-sm text-gray-700 font-medium">{{ __('Clients') }}</h3>
         @isset($case)
             @foreach ($case->clients as $client)
-                <div class="richak richak-xs richak-secondary richak-checkbox">
-                    <input type="checkbox" id="client-{{$client->id}}" name="client_id[]" value="{{$client->id}}" @radioChecked(request()->client, $client->id)>
-                    <label for="client-{{$client->id}}">
-                        <span class="richak-icon"></span>
-                        <span>
-                            <div class="d-flex align-items-center fs-12 d-inline-block">
-                                <span class="media media-sm media-primary">
-                                    @isset($client->avatars->samll)
-                                        <img src="{{$client->avatars->small->url}}">
-                                    @else
-                                    <span>{{$client->user->shortName}}</span>
-                                    @endisset
-                                </span>
-                                <div class="pr-1">
-                                    <div class="data-name">@displayName($client->user)</div>
-                                </div>
-                            </div>
-                        </span>
+                <div class="mt-1">
+                    <label class="inline-flex items-center group">
+                        <input type="checkbox" id="client-{{ $client->id }}" name="client_id[]" value="{{$client->id}}" @radioChecked(request()->client, $client->id) class="w-3.5 h-3.5 border border-gray-600 focus:ring-2 focus:ring-offset-2">
+                        <span class="text-sm text-gray-600 mr-2 group-hover:text-blue-600">@displayName ($client->user)</span>  
                     </label>
                 </div>
             @endforeach
         @endisset
     </div>
-    <div class="form-group form-group-m">
-        <select class="select2-select form-control form-control-m has-clear" data-template="sessions" name="session_id" data-title="manager.name manager.id" id="session_id" data-url="{{isset($case) ? route('dashboard.sessions.index', ['case' => $case->id]) : ''}}" data-url-pattern="{{route('dashboard.sessions.index', ['case' => '%%'])}}">
-        </select>
-        <label for="session_id">{{__('Session')}}</label>
-    </div>
 
-    <div class="richak richak-xs richak-secondary richak-checkbox d-none" id="client-template">
-        <input type="checkbox">
-        <label>
-            <span class="richak-icon"></span>
-            <span>
-                <div class="d-flex align-items-center fs-12 d-inline-block">
-                    <span class="media media-sm media-primary">
-                        <img>
-                    </span>
-                    <div class="pr-1">
-                        <div class="data-name"></div>
-                    </div>
-                </div>
-            </span>
-        </label>
+    <div class="mt-4">
+        <label for="session_id" data-alias="manager_id" class="block mb-2 text-sm text-gray-700 font-medium">{{ __('Session') }}</label>
+        <select data-template="sessions" name="session_id" data-title="manager.name manager.id" id="session_id" data-url="{{ isset($case) ? route('dashboard.sessions.index', ['case' => $case->id]) : '' }}" data-url-pattern="{{ route('dashboard.sessions.index', ['case' => '%%']) }} class="select2-select">
+        </select>
     </div>
 </div>
