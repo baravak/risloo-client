942ff98373de62ca22545f1448526e2cc47166d3
Ahmad Karimi <ahmadkarimi1991@gmail.com>
2021-02-23 16:34:43 +0330

    Minor change

--- resources/views/davat/layouts/_head-styles.blade.php
+++ resources/views/davat/layouts/_head-styles.blade.php
@@ -16,4 +16,16 @@
     @endif
 
     <link rel="stylesheet" href="@staticVersion('/css/davat.css')">
+
+    <!-- Hotjar Tracking Code for risloo.ir -->
+    <script>
+        (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:2262407,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    </script>
 @endsection
