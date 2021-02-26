d372126724f6164352d25604cac767de53e71d33
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-02-26 22:47:03 +0330

    Update create sample

--- resources/views/dashboard/samples/create.blade.php
+++ resources/views/dashboard/samples/create.blade.php
@@ -3,7 +3,7 @@
     <div>
         <div class="mt-4">
             <label for="scale_id" data-alias="manager_id" class="block mb-2 text-sm text-gray-700 font-medium">{{ __('Scale') }}</label>
-            <select multiple name="scale_id[]" id="scale_id" data-url="{{ route('dashboard.assessments.index') }}" class="select2-select">
+            <select class="select2-select" multiple name="scale_id[]" id="scale_id" data-url="{{ route('dashboard.assessments.index') }}">
                 @isset($scale)
                     <option value="{{$scale->id}}" data-json="{{$scale}}" selected>{{$scale->title}}</option>
                 @endisset
@@ -12,7 +12,7 @@
 
         <div class="mt-4">
             <label for="room_id" data-alias="manager_id" class="block mb-2 text-sm text-gray-700 font-medium">{{ __('Room') }}</label>
-            <select data-relation="case_id room_client_id" data-template="room" name="room_id" data-title="manager.name manager.id" data-avatar="manager.avatar.tiny.url manager.avatar.small.url" id="room_id" data-url="{{ route('dashboard.rooms.index' , ['my_management' => 1]) }}" class="select2-select">
+            <select class="select2-select" data-relation="case_id room_client_id" data-template="room" name="room_id" data-title="manager.name manager.id" data-avatar="manager.avatar.tiny.url manager.avatar.small.url" id="room_id" data-url="{{ route('dashboard.rooms.index' , ['my_management' => 1]) }}">
                 @isset($case)
                 <option value="{{$case->room->id}}" data-json="{{$case->room}}" selected>{{$case->room->manager->name}}</option>
                 @endisset
--- resources/views/dashboard/samples/createCase.blade.php
+++ resources/views/dashboard/samples/createCase.blade.php
@@ -1,7 +1,7 @@
 <div>
     <div class="mt-4">
         <label for="case_id" data-alias="manager_id" class="block mb-2 text-sm text-gray-700 font-medium">{{ __('Case') }}</label>
-        <select data-template="case_clients" name="case_id" data-title="manager.name manager.id" id="case_id" data-url="{{ isset($case) ? route('dashboard.cases.index', ['room' => $case->room->id]) : '' }}" data-url-pattern="{{ route('dashboard.cases.index', ['room' => '%%']) }}" data-relation="session_id" class="select2-select">
+        <select class="select2-select" data-template="case_clients" name="case_id" data-title="manager.name manager.id" id="case_id" data-url="{{ isset($case) ? route('dashboard.cases.index', ['room' => $case->room->id]) : '' }}" data-url-pattern="{{ route('dashboard.cases.index', ['room' => '%%']) }}" data-relation="session_id">
             @isset($case)
             <option value="{{$case->id}}" data-json='{{$case}}' selected>{{$case->clients->pluck('user.name')->join('-')}}</option>
             @endisset
@@ -24,7 +24,7 @@
 
     <div class="mt-4">
         <label for="session_id" data-alias="manager_id" class="block mb-2 text-sm text-gray-700 font-medium">{{ __('Session') }}</label>
-        <select data-template="sessions" name="session_id" data-title="manager.name manager.id" id="session_id" data-url="{{ isset($case) ? route('dashboard.sessions.index', ['case' => $case->id]) : '' }}" data-url-pattern="{{ route('dashboard.sessions.index', ['case' => '%%']) }} class="select2-select">
+        <select class="select2-select" data-template="sessions" name="session_id" data-title="manager.name manager.id" id="session_id" data-url="{{ isset($case) ? route('dashboard.sessions.index', ['case' => $case->id]) : '' }}" data-url-pattern="{{ route('dashboard.sessions.index', ['case' => '%%']) }}">
         </select>
     </div>
 </div>
