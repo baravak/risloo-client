b6b42c3c3c2d68bbac741c95c6d6160df1f19314
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-02-27 10:31:19 +0330

    Update create room

--- resources/views/dashboard/rooms/create.blade.php
+++ resources/views/dashboard/rooms/create.blade.php
@@ -3,7 +3,7 @@
     <div>
         <div class="mt-4">
             <label for="counseling_center_id" data-alias="manager_id" class="block mb-2 text-sm text-gray-700 font-medium">{{ __('Therapy center') }}</label>
-            <select data-template="center" name="counseling_center_id" id="counseling_center_id" data-url="{{ route('dashboard.centers.index', ['my_position' => 'manager', 'type' => 'counseling_center']) }}" data-relation="psychologist_id" data-placeholder="{{ __('Select :attribute', ['attribute' => __('Therapy center')]) }} class="select2-select">
+            <select class="select2-select" data-template="center" name="counseling_center_id" id="counseling_center_id" data-url="{{ route('dashboard.centers.index', ['my_position' => 'manager', 'type' => 'counseling_center']) }}" data-relation="psychologist_id" data-placeholder="{{ __('Select :attribute', ['attribute' => __('Therapy center')]) }}">
                 @if (isset($center))
                     <option value="{{$center->id}}" data-json="{{$center}}">
                         {{$center->detail->title}}
@@ -14,7 +14,7 @@
 
         <div class="mt-4">
             <label for="psychologist_id" data-alias="manager_id" class="block mb-2 text-sm text-gray-700 font-medium">{{ __('Psychologist') }}</label>
-            <select data-template="users" name="psychologist_id" data-avatar="user.avatar.small.url" data-id="id" data-title="user.name" id="psychologist_id" data-url="{{ isset($center) ? route('dashboard.center.users.index', ['center' => $center->id, 'position' => join(',',config('users.room_managers')), 'has_room' => 'no']) : '' }}" data-url-pattern="{{ route('dashboard.center.users.index', ['center' => '%%', 'position' => join(',',config('users.room_managers')), 'has_room' => 'no']) }}" data-placeholder="{{ __('Select :attribute', ['attribute' => __('Psychologist')]) }} class="select2-select">
+            <select class="select2-select" data-template="users" name="psychologist_id" data-avatar="user.avatar.small.url" data-id="id" data-title="user.name" id="psychologist_id" data-url="{{ isset($center) ? route('dashboard.center.users.index', ['center' => $center->id, 'position' => join(',',config('users.room_managers')), 'has_room' => 'no']) : '' }}" data-url-pattern="{{ route('dashboard.center.users.index', ['center' => '%%', 'position' => join(',',config('users.room_managers')), 'has_room' => 'no']) }}" data-placeholder="{{ __('Select :attribute', ['attribute' => __('Psychologist')]) }}">
                 @if (isset($user))
                     <option value="{{$user->id}}" data-json="{{$user}}">
                         {{$user->user->name}}
