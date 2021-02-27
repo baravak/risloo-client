211fa63a691191d453c16fa0cc6ad6680e848b32
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-02-27 11:10:43 +0330

    Update create practice

--- resources/views/dashboard/practices/create.blade.php
+++ resources/views/dashboard/practices/create.blade.php
@@ -1,17 +1,17 @@
 @extends('dashboard.create')
 @section('form_content')
-<div class="form-group form-group-m">
-    <input type="text" class="form-control form-control-m" id="title" name="title" placeholder="&nbsp;" autocomplete="off" @formValue($practice->title)>
-    <label for="title">{{__('Title')}}</label>
-</div>
+    <div class="mt-4">
+        <label for="title" class="block mb-2 text-sm text-gray-700 font-medium">{{ __('Title') }}</label>
+        <input type="text" name="title" id="title" autocomplete="off" @formValue($practice->title) class="border border-gray-500 h-10 rounded px-4 w-full text-sm focus:border-brand focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-opacity-60">
+    </div>
 
-<div class="form-group form-group-m">
-    <textarea type="text" class="form-control form-control-m" id="content" name="content" placeholder="&nbsp;" autocomplete="off" @formValue($practice->content)></textarea>
-    <label for="content">{{__('Content')}}</label>
-</div>
+    <div class="mt-4">
+        <label for="content" class="block mb-2 text-sm text-gray-700 font-medium">{{ __('Content') }}</label>
+        <textarea id="content" name="content" autocomplete="off" @formValue($practice->content) class="resize-none border border-gray-500 h-24 rounded px-4 py-2 w-full text-sm placeholder-gray-300 focus:border-brand focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-opacity-60"></textarea>
+    </div>
 
-<div class="form-group form-group-m">
-    <input type="file" class="form-control form-control-m" id="attachment" name="attachment" placeholder="&nbsp;" autocomplete="off">
-    <label for="attachment">{{__('Attachment')}}</label>
-</div>
+    <div class="mt-4">
+        <label for="attachment" class="block mb-2 text-sm text-gray-700 font-medium">{{ __('Attachment') }}</label>
+        <input type="file" name="attachment" id="attachment" autocomplete="off">
+    </div>
 @endsection
