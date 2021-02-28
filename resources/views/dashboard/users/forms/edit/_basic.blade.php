ce8354a81935b90a41bac6e2db3371c95fb1bd43
Ahmad Karimi <ahmadkarimi1991@gmail.com>
2021-02-28 20:00:50 +0330

    Minor fix

--- resources/views/dashboard/users/forms/edit/_basic.blade.php
+++ resources/views/dashboard/users/forms/edit/_basic.blade.php
@@ -1,11 +1,7 @@
-<form action="{{route('dashboard.users.update', ['user' => $user->id])}}" method="POST">
+<form action="{{ route('dashboard.users.update', ['user' => $user->id]) }}" method="POST">
     @method('PUT')
-    <div>
-        @include('dashboard.users.forms.basic')
-        <div class="flex justify-end">
-            <button type="submit" class="items-center min-w-min w-36 h-9 px-4 bg-brand text-white text-sm rounded-full hover:bg-blue-800 transition mt-6">
-                {{ __('Edition') }}
-            </button>
-        </div>
+    @include('dashboard.users.forms.basic')
+    <div class="flex justify-end mt-6">
+        <button type="submit" class="w-36 h-9 px-4 bg-brand text-white text-sm rounded-full hover:bg-brand-600 transition">{{ __('Edition') }}</button>
     </div>
 </form>
