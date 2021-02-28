22ecaf31a0928d7a883c4d7b2ddadbf855769773
Ahmad Karimi <ahmadkarimi1991@gmail.com>
2021-02-28 20:30:19 +0330

    Add spinner to edit button

--- resources/views/dashboard/users/forms/edit/_basic.blade.php
+++ resources/views/dashboard/users/forms/edit/_basic.blade.php
@@ -2,6 +2,9 @@
     @method('PUT')
     @include('dashboard.users.forms.basic')
     <div class="flex justify-end mt-6">
-        <button type="submit" class="w-36 h-9 px-4 bg-brand text-white text-sm rounded-full hover:bg-brand-600 transition">{{ __('Edition') }}</button>
+        <button type="submit" class="relative w-36 h-9 leading-9 px-4 bg-brand text-white text-sm rounded-full hover:bg-brand-600 transition">
+            <span>{{ __('Edition') }}</span>
+            <i class="fad fa-spinner-third absolute top-2.5 left-4 animate-spin hidden"></i>
+        </button>
     </div>
 </form>
