0e62ffc5bbd58b545776e19335834e2dc14c1023
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-03-01 10:54:40 +0330

    Update room users list

--- resources/views/dashboard/rooms/show.blade.php
+++ resources/views/dashboard/rooms/show.blade.php
@@ -4,11 +4,16 @@
     <div class="border border-gray-200 rounded-sm overflow-hidden">
         <div class="h-20 sm:h-28 bg-gradient-to-b from-blue-100 to-blue-50 border-b border-gray-200"></div>
         <div class="relative p-4">
-                {{-- <div class="absolute top-3 left-3 flex">
-                    <a href="" class="flex justify-center items-center flex-shrink-0 border border-gray-500 text-gray-600 hover:bg-gray-100 px-4 h-9 rounded-full text-sm leading-normal transition-all">
+                <div class="absolute top-3 left-3 flex">
+
+                    <a href="{{ route('dashboard.room.users.index', $room->id) }}" title="{{ __('Users') }}" class="flex justify-center items-center flex-shrink-0 text-brand border border-brand hover:bg-blue-50 w-9 h-9 rounded-full transition">
+                        <i class="fal fa-users"></i>
+                    </a>
+
+                    <a href="" class="flex justify-center items-center flex-shrink-0 border border-gray-500 text-gray-600 hover:bg-gray-100 px-4 h-9 rounded-full text-sm leading-normal transition-all mr-2">
                         <span class="font-medium">{{ __('Edit') }}</span>
                     </a>
-                </div> --}}
+                </div>
             <div class="flex justify-center items-center flex-shrink-0 w-24 h-24 md:w-32 md:h-32 -mt-16 md:-mt-20 bg-gray-300 text-gray-600 text-2xl rounded-full border-4 border-white overflow-hidden mb-4 relative">@avatarOrName($room->manager)</div>
             <h2 class="font-bold text-lg text-gray-900 cursor-default">@displayName($room->manager)</h2>
         </div>
