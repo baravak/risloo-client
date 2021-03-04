f2ba64b23a6f3f14dd3ad5423a438f573f445055
Ahmad Karimi <ahmadkarimi1991@gmail.com>
2021-03-04 20:00:39 +0330

    Minor fix

--- resources/views/dashboard/home/centers.blade.php
+++ resources/views/dashboard/home/centers.blade.php
@@ -7,10 +7,12 @@
     @endif
 </div>
 
-<div data-xhr="center-items">
-    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
-        @foreach ($user->centers ?: [] as $center)
-            @include('dashboard.centers.listRaw')
-        @endforeach
+@if ($user->centers && $user->centers->count() > 0)
+    <div data-xhr="center-items">
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
+            @foreach ($user->centers ?: [] as $center)
+                @include('dashboard.centers.listRaw')
+            @endforeach
+        </div>
     </div>
-</div>
+@endif
