f4f20d129abaa3965bebf443f7a940bdd7483680
Ahmad Karimi <ahmadkarimi1991@gmail.com>
2021-03-04 19:56:11 +0330

    Minor fix

--- resources/views/dashboard/home/cases.blade.php
+++ resources/views/dashboard/home/cases.blade.php
@@ -7,8 +7,10 @@
     @endif
 </div>
 
-<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
-    @foreach ($user->cases ?: [] as $case)
-        @include('dashboard.cases.item')
-    @endforeach
-</div>
+@if ($user->cases && $user->cases->count() > 0)
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
+        @foreach ($user->cases ?: [] as $case)
+            @include('dashboard.cases.item')
+        @endforeach
+    </div>
+@endif
