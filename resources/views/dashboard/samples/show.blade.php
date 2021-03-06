c85e9dafd56542dd25d16b203b9181455e611f97
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-03-06 15:18:12 +0330

    Minor fix

--- resources/views/dashboard/samples/show.blade.php
+++ resources/views/dashboard/samples/show.blade.php
@@ -118,7 +118,7 @@
 
         <div class="border border-gray-200 rounded p-4 mt-4">
             <h4 class="font-semibold text-gray-700 mb-4">{{ __('Sample items') }}</h4>
-            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
                 @foreach ($sample->items as $item)
                     <div>
                         @include('dashboard.samples.shows.'. $item->answer->type)
