a7e16dfdf8766228e700ac5340d72a66aa9658b8
Ahmad Karimi <ahmadkarimi1991@gmail.com>
2021-03-06 13:47:04 +0330

    Minor fix

--- resources/views/davat/layouts/main.blade.php
+++ resources/views/davat/layouts/main.blade.php
@@ -1,5 +1,5 @@
 @section('main')
-    <main id="main" class="flex-1 flex flex-col">
+    <main id="main" class="flex-1 flex flex-col bg-white">
         @include('layouts.header')
         @include('layouts.content')
     </main>
