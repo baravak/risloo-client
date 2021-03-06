04fd6e98d9f490fecf488a920ddc28b00182b2f4
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-03-06 08:46:32 +0330

    Update sample show

--- resources/views/dashboard/samples/show.blade.php
+++ resources/views/dashboard/samples/show.blade.php
@@ -1,147 +1,130 @@
 @extends($layouts->dashboard)
 
 @section('content')
-<div class="card mb-3" id="sample-show" data-status="{{$sample->status}}" data-sample="{{$sample->id}}">
-    <div class="card-header">
-        <h5 class="card-title">
-            {{ $sample->scale->title }} <small> / {{$sample->client ? $sample->client->name: ''}}</small>
-            <a href="{!!urldecode(route('dashboard.samples.scoring', $sample->id))!!}" data-lijax-preload="scoring-preload" data-lijax-succsess="" id="scoring-btn" data-method="POST" class="badge badge-info fs-12 p-1 lijax {{($sample->status =='closed' || (auth()->isAdmin() && $sample->status == 'done') || $sample->score_last_version != $sample->score_current_version ) && $sample->client ? '' : 'd-none'}}">
-                {{__("Scoring")}}
-            </a>
-            <span id="scoring-preload" class="badge badge-light fs-12 p-1 {{$sample->status == 'scoring' ? '' : 'd-none'}}">
-                <i class="fas fa-cog fa-spin"></i> {{__("Scoring")}}...
-            </span>
-            @if (in_array($sample->status, ['seald', 'open']))
-                <a href="{!! urldecode(route('dashboard.samples.close', $sample->id))!!}" class="badge badge-info fs-12 p-1 lijax status-action" data-method="PUT">
-                    {{__('Close sample')}}
-                </a>
-            @elseif($sample->status == 'closed')
-                <a href="{!! urldecode(route('dashboard.samples.open', $sample->id))!!}" class="badge badge-secondary fs-12 p-1 lijax status-action" data-method="PUT">
-                    {{__('Open sample')}}
-                </a>
-            @endif
-            @if (config('app.env') == 'local' && in_array($sample->status, ['seald', 'open']))
-            <span class="dropdown">
-                <button class="btn btn-secondary dropdown-toggle fs-12 p-1" type="button" id="assessmentFill" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    پرکردن
-                </button>
-                <div class="dropdown-menu" aria-labelledby="assessmentFill">
-                    <form action="{{config('app.server')}}/command/assessment/fill/{{substr($sample->id, 1)}}" class="fs-12 p-1">
-                        <label class="d-block">
-                            <input type="checkbox" name="replace" checked>
-                            <span>ویرایش داده‌های قبلی</span>
-                        </label>
-                        <label class="d-block">
-                            <input type="checkbox" name="empty">
-                            <span>فیلد خالی</span>
-                        </label>
-                        <label class="d-block">
-                            <input type="checkbox" name="validity">
-                            <span>اعتبارسنجی</span>
-                        </label>
-                        <button type="submit">پرکردن</button>
-                    </form>
-                </div>
-            </span>
-            @endif
-            <span class="dropdown {{in_array($sample->status, ['seald', 'open', 'closed', 'scoring']) ? 'd-none' : ''}}" id="profile-export-menu">
-                <button class="btn btn-secondary dropdown-toggle fs-12 p-1" type="button" id="profile-export" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <i class="fas fa-cog fa-spin {{$sample->status == 'done' ? 'd-none' : ''}}"></i>
-                    خروجی‌ها
-                </button>
-                <div class="dropdown-menu" aria-labelledby="profile-export" id="profile-export-list">
-                    @if ($sample->profiles)
-                        @foreach ($sample->profiles->getAttributes() as $key => $item)
-                        <a href="{!!$item->url!!}" target="_blank" data-type="{{$key}}" class="dropdown-item fs-12 profile-link profile-{{$key}}">
-                            {{ strtoupper(str_replace('_', ' ', str_replace('profile_', '', $key)))}}
-                        </a>
-                        @endforeach
-                    @endif
-                </div>
-            </span>
-        </h5>
-        <div class="text-center col-12 col-xl-6 mx-auto" id="profile_svg">
-            @if ($sample->status == 'scoring')
-                <i class="fas fa-cog fa-spin"></i>
-            @endif
-            @isset($sample->profiles->profile_svg)
-                <img src="{{$sample->profiles->profile_svg->url}}" class="d-block profile-svg">
-            @endisset
+    <div id="sample-show" data-status="{{$sample->status}}" data-sample="{{$sample->id}}">
+        <div class="mt-8 mb-4">
+            <h3 class="heading">{{ $sample->scale->title }}</h3>
         </div>
-    </div>
-    @includeIf('dashboard.samples.scales.' . substr($sample->scale->id, 1), ['scoring'=> (object) ['profiles' => $sample->profiles, 'score' => $sample->score, 'id' => $sample->id]])
-</div>
-<div class="card mb-3">
-    <div class="card-header">
-        <h5 class="card-title">
-            مواد آزمون
-        </h5>
-    </div>
-    <div class="card-body">
-        @if (in_array($sample->status, ['open', 'seald']))
-            <div class="row">
-                <div class="col-12 col-lg-6">
-                    <div class="richak richak-xs richak-secondary richak-checkbox">
-                        <input type="checkbox" id="editable">
-                        <label for="editable">
-                            <span class="richak-icon"></span>
-                            <span>
-                                <div class="d-flex align-items-center fs-12 d-inline-block">
-                                    <div class="pr-1">
-                                        قابلیت ویرایش فرم
-                                    </div>
-                                </div>
-                            </span>
-                        </label>
+
+        <div class="flex items-center justify-between border border-gray-200 rounded p-4">
+            <div class="flex items-center text-sm text-gray-600 cursor-default">
+                <i class="fal fa-user mb-1 ml-2"></i>
+                <span>{{ $sample->client ? $sample->client->name: '' }}</span>
+            </div>
+            <div>
+                <a href="{!! urldecode(route('dashboard.samples.scoring', $sample->id)) !!}" class="inline-flex items-center px-4 h-8 text-xs text-white bg-brand hover:bg-brand-600 rounded-full transition ml-1 {{($sample->status =='closed' || (auth()->isAdmin() && $sample->status == 'done') || $sample->score_last_version != $sample->score_current_version ) && $sample->client ? '' : 'hidden'}}" data-lijax-preload="scoring-preload" data-lijax-succsess="" id="scoring-btn" data-method="POST">
+                    {{ __("Scoring") }}
+                </a>
+                @if (in_array($sample->status, ['seald', 'open']))
+                <a href="{!! urldecode(route('dashboard.samples.close', $sample->id)) !!}" class="inline-flex items-center px-4 h-8 text-xs text-red-600 hover:text-white hover:bg-red-600 border border-red-600 rounded-full transition lijax status-action ml-1" data-method="PUT">
+                    {{ __("Close sample") }}
+                </a>
+                @elseif($sample->status == 'closed')
+                <a href="{!! urldecode(route('dashboard.samples.open', $sample->id)) !!}" class="inline-flex items-center px-4 h-8 text-xs text-brand hover:text-white hover:bg-brand border border-brand rounded-full transition lijax status-action ml-1" data-method="PUT">
+                    {{ __("Open sample") }}
+                </a>
+                @endif
+
+                @if (config('app.env') == 'local' && in_array($sample->status, ['seald', 'open']))
+                <div class="relative inline-flex dropdown">
+                    <button class="flex items-center px-4 h-8 text-xs text-brand hover:text-white hover:bg-brand border border-brand rounded-full transition" type="button" id="assessmentFill" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        {{ __('Fill in') }}
+                        <i class="fal fa-chevron-down mr-2"></i>
+                    </button>
+                    <div class="hidden" aria-labelledby="assessmentFill">
+                        <form action="{{config('app.server')}}/command/assessment/fill/{{substr($sample->id, 1)}}" class="absolute w-52 left-0 top-10 p-4 rounded bg-white border border-gray-200 shadow-lg">
+                            <label class="flex items-center group">
+                                <input type="checkbox" name="replace" checked class="w-3.5 h-3.5 border border-gray-600 rounded-sm focus:ring-1 focus:ring-offset-1">
+                                <span class="text-sm text-gray-600 mr-2 group-hover:text-blue-600">{{ __('Edit prev data') }}</span>
+                            </label>
+                            <label class="flex items-center mt-2 group">
+                                <input type="checkbox" name="empty" class="w-3.5 h-3.5 border border-gray-600 rounded-sm focus:ring-1 focus:ring-offset-1">
+                                <span class="text-sm text-gray-600 mr-2 group-hover:text-blue-600">{{ __('Empty field') }}</span>
+                            </label>
+                            <label class="flex items-center mt-2 group">
+                                <input type="checkbox" name="validity" class="w-3.5 h-3.5 border border-gray-600 rounded-sm focus:ring-1 focus:ring-offset-1">
+                                <span class="text-sm text-gray-600 mr-2 group-hover:text-blue-600">{{ __('Validation') }}</span>
+                            </label>
+                            <div class="flex justify-center mt-4">
+                                <button type="submit" class="flex items-center px-4 py-2 text-xs text-white bg-brand hover:bg-brand-600 rounded-full transition">{{ __("Fill in") }}</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                @endif
+
+                <div class="relative inline-flex dropdown {{in_array($sample->status, ['seald', 'open', 'closed', 'scoring']) ? 'hidden' : ''}}" id="profile-export-menu">
+                    <button class="flex items-center px-4 h-8 text-xs text-brand hover:text-white hover:bg-brand border border-brand rounded-full transition dropdown-toggle" type="button" id="profile-export" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        {{ __('Exports') }}
+                        <i class="fal fa-chevron-down mr-2"></i>
+                    </button>
+                    <div aria-labelledby="profile-export" id="profile-export-list" class="hidden absolute w-28 py-2 left-0 top-10 rounded bg-white border border-gray-200 shadow-lg dropdown-menu">
+                        @if ($sample->profiles)
+                            @foreach ($sample->profiles->getAttributes() as $key => $item)
+                                <a href="{!!$item->url!!}" data-type="{{$key}}" class="dropdown-item direct profile-{{$key}} block w-full p-1 text-center rounded text-gray-600 hover:text-brand hover:bg-gray-100">
+                                    {{ strtoupper(str_replace('_', ' ', str_replace('profile_', '', $key))) }}
+                                </a>
+                            @endforeach
+                        @endif
                     </div>
                 </div>
             </div>
-        @endif
-        <div class="row">
-            <div class="col-12">
-                <h6>عمومی</h6>
-            </div>
-            <div class="col-12 col-md-6">
-                <div class="form-group form-group-m">
-                    <input @formValue($sample->cornometer) disabled class="form-control form-control-m d-notification form-items font-weight-bold" type="number" name="cornometer" id="cornometer" placeholder="&nbsp;" data-lijax="500" data-method="put">
-                    <label for="cornometer">زمان انجام <sup>دقیقه</sup></label>
+        </div>
+        @includeIf('dashboard.samples.scales.' . substr($sample->scale->id, 1), ['scoring'=> (object) ['profiles' => $sample->profiles, 'score' => $sample->score, 'id' => $sample->id]])
+    </div>
+
+    <div>
+        <div class="flex items-center justify-between mt-8">
+            <h3 class="heading">{{ __('Sample details') }}</h3>
+
+            @if (in_array($sample->status, ['open', 'seald']))
+                <label class="flex items-center group px-4 py-2 border border-gray-300 rounded hover:border-brand">
+                    <input type="checkbox" id="editable" class="w-3.5 h-3.5 border border-gray-600 hover:border-brand rounded-sm focus:ring-1 focus:ring-offset-1">
+                    <span class="text-sm text-gray-600 mr-2 group-hover:text-brand">{{ __('Editable') }}</span>
+                </label>
+            @endif
+        </div>
+
+        <div class="border border-gray-200 rounded p-4 mt-4">
+            <h4 class="font-semibold text-gray-700 mb-4">{{ __('General') }}</h4>
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                <div>
+                    <label for="cornometer" class="block mb-2 text-sm text-gray-700 font-medium">{{ __('Time to do') }} ({{ __('Minutes') }})</label>
+                    <input @formValue($sample->cornometer) disabled type="number" name="cornometer" id="cornometer" placeholder="&nbsp;" data-lijax="500" data-method="put" class="border border-gray-500 h-10 rounded px-4 w-full text-sm focus:border-brand focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-opacity-60">
                 </div>
             </div>
         </div>
-        <div class="row mt-3">
-            <div class="col-12">
-                <h6>پیش‌نیازها</h6>
-            </div>
-            @foreach ($sample->prerequisites as $prerequisite)
-            <div class="col-12 col-lg-6">
-                <div class="form-group form-group-m">
-                    @if (in_array($prerequisite->answer->type, ['options', 'select']))
-                        <select type="text" data-action="{{urldecode(route('samples.storeItems', $sample->id, 1))}}" data-method="post" data-prerequisite="{{$loop->index + 1}}" data-name="prerequisites[{{$loop->index}}][1]" data-merge='{"prerequisites[{{$loop->index}}][0]" : {{$loop->index + 1}}}' data-lijax="change" id="prerequisite-{{$loop->index}}" class="form-items form-control form-control-m font-weight-bold d-notification" placeholder="&nbsp;" disabled>
+
+        <div class="border border-gray-200 rounded p-4 mt-4">
+            <h4 class="font-semibold text-gray-700 mb-4">{{ __('Prerequisites') }}</h4>
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                @foreach ($sample->prerequisites as $prerequisite)
+                    <div>
+                        <label for="prerequisite-{{$loop->index}}" class="block mb-2 text-sm text-gray-700 font-medium">{{$loop->index + 1}} - {{$prerequisite->text}}</label>
+                        @if (in_array($prerequisite->answer->type, ['options', 'select']))
+                        <select type="text" data-action="{{urldecode(route('samples.storeItems', $sample->id, 1))}}" data-method="post" data-prerequisite="{{$loop->index + 1}}" data-name="prerequisites[{{$loop->index}}][1]" data-merge='{"prerequisites[{{$loop->index}}][0]" : {{$loop->index + 1}}}' data-lijax="change" id="prerequisite-{{$loop->index}}" placeholder="&nbsp;" disabled class="border border-gray-500 h-10 rounded px-4 w-full text-sm focus:border-brand focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-opacity-60">
                             <option></option>
                             @foreach ($prerequisite->answer->options as $option)
                                 <option {{isset($prerequisite->user_answered) && $prerequisite->user_answered == $loop->index +1 ? 'selected' : ''}} value="{{$loop->index + 1}}" @selectChecked($prerequisite->user_answered, $loop->index + 1)>{{$loop->index + 1}}: {{$option}}</option>
                             @endforeach
                         </select>
-                    @elseif (in_array($prerequisite->answer->type, ['text', 'number']))
-                        <input @formValue($prerequisite->user_answered) disabled class="form-items form-control form-control-m font-weight-bold d-notification" type="{{$prerequisite->answer->type}}" id="prerequisite-{{$loop->index}}" placeholder="&nbsp;">
-                    @endif
-                    <label for="prerequisite-{{$loop->index}}">{{$loop->index + 1}} - {{$prerequisite->text}}</label>
-                </div>
+                        @elseif (in_array($prerequisite->answer->type, ['text', 'number']))
+                            <input @formValue($prerequisite->user_answered) disabled type="{{$prerequisite->answer->type}}" id="prerequisite-{{$loop->index}}" placeholder="&nbsp;" class="border border-gray-500 h-10 rounded px-4 w-full text-sm focus:border-brand focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-opacity-60">
+                        @endif
+                    </div>
+                @endforeach
             </div>
-            @endforeach
         </div>
-        <div class="row mt-3">
-            <div class="col-12">
-                <h6>گویه‌ها</h6>
-            </div>
-            @foreach ($sample->items as $item)
-                <div class="col-12 col-xl-6">
-                    <div class="form-group form-group-m">
+
+        <div class="border border-gray-200 rounded p-4 mt-4">
+            <h4 class="font-semibold text-gray-700 mb-4">{{ __('Sample items') }}</h4>
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                @foreach ($sample->items as $item)
+                    <div>
                         @include('dashboard.samples.shows.'. $item->answer->type)
                     </div>
-                </div>
-            @endforeach
+                @endforeach
+            </div>
         </div>
     </div>
-</div>
 @endsection
