24f41c4ac472ec1c220a78fd2106ffff6f814d26
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-03-07 22:23:18 +0330

    Update sample

--- resources/views/dashboard/samples/show.blade.php
+++ resources/views/dashboard/samples/show.blade.php
@@ -2,71 +2,73 @@
 
 @section('content')
     <div id="sample-show" data-status="{{$sample->status}}" data-sample="{{$sample->id}}">
-        <div class="mt-8 mb-4">
-            <h3 class="heading">{{ $sample->scale->title }}</h3>
-        </div>
-
-        <div class="flex items-center justify-between border border-gray-200 rounded p-4">
-            <div class="flex items-center text-sm text-gray-600 cursor-default">
+        <div class="border border-gray-200 rounded p-4">
+            <h3 class="font-bold text-gray-900 cursor-default">{{ $sample->scale->title }}</h3>
+            <div class="flex items-center text-sm text-gray-500 cursor-default mt-4">
                 <i class="fal fa-user mb-1 ml-2"></i>
                 <span>{{ $sample->client ? $sample->client->name: '' }}</span>
                 {{-- <span>این نمونه مراجع ندارد. ابتدا مراجع را انتخاب نمایید.</span> --}}
             </div>
-            <div>
-                <a href="{!! urldecode(route('dashboard.samples.scoring', $sample->id)) !!}" class="inline-flex items-center px-4 h-8 text-xs text-white bg-brand hover:bg-brand-600 rounded-full transition ml-1 {{($sample->status =='closed' || (auth()->isAdmin() && $sample->status == 'done') || $sample->score_last_version != $sample->score_current_version ) && $sample->client ? '' : 'hidden'}}" data-lijax-preload="scoring-preload" data-lijax-succsess="" id="scoring-btn" data-method="POST">
-                    {{ __("Scoring") }}
-                </a>
-                @if (in_array($sample->status, ['seald', 'open']))
-                <a href="{!! urldecode(route('dashboard.samples.close', $sample->id)) !!}" class="inline-flex items-center px-4 h-8 text-xs text-red-600 hover:text-white hover:bg-red-600 border border-red-600 rounded-full transition lijax status-action ml-1" data-method="PUT">
-                    {{ __("Close sample") }}
-                </a>
-                @elseif($sample->status == 'closed')
-                <a href="{!! urldecode(route('dashboard.samples.open', $sample->id)) !!}" class="inline-flex items-center px-4 h-8 text-xs text-brand hover:text-white hover:bg-brand border border-brand rounded-full transition lijax status-action ml-1" data-method="PUT">
-                    {{ __("Open sample") }}
-                </a>
-                @endif
-
-                @if (config('app.env') == 'local' && in_array($sample->status, ['seald', 'open']))
-                <div class="relative inline-flex dropdown ml-1">
-                    <button class="flex items-center px-4 h-8 text-xs text-brand hover:text-white hover:bg-brand border border-brand rounded-full transition" type="button" id="assessmentFill" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        {{ __('Fill in') }}
-                        <i class="fal fa-chevron-down mr-2"></i>
-                    </button>
-                    <div aria-labelledby="assessmentFill" class="absolute left-0 top-10 w-52 p-4 rounded bg-white border border-gray-200 shadow-lg">
-                        <form action="{{ config('app.server')}}/command/assessment/fill/{{substr($sample->id, 1) }}">
-                            <label class="flex items-center group">
-                                <input type="checkbox" name="replace" checked class="w-3.5 h-3.5 border border-gray-600 rounded-sm focus:ring-1 focus:ring-offset-1">
-                                <span class="text-sm text-gray-600 mr-2 group-hover:text-blue-600">{{ __('Edit prev data') }}</span>
-                            </label>
-                            <label class="flex items-center mt-2 group">
-                                <input type="checkbox" name="empty" class="w-3.5 h-3.5 border border-gray-600 rounded-sm focus:ring-1 focus:ring-offset-1">
-                                <span class="text-sm text-gray-600 mr-2 group-hover:text-blue-600">{{ __('Empty field') }}</span>
-                            </label>
-                            <label class="flex items-center mt-2 group">
-                                <input type="checkbox" name="validity" class="w-3.5 h-3.5 border border-gray-600 rounded-sm focus:ring-1 focus:ring-offset-1">
-                                <span class="text-sm text-gray-600 mr-2 group-hover:text-blue-600">{{ __('Validation') }}</span>
-                            </label>
-                            <div class="flex justify-center mt-4">
-                                <button type="submit" class="flex items-center px-4 py-2 text-xs text-white bg-brand hover:bg-brand-600 rounded-full transition">{{ __("Fill in") }}</button>
-                            </div>
-                        </form>
-                    </div>
+            <div class="flex flex-col sm:flex-row sm:items-end sm:justify-between mt-2 sm:mt-0">
+                <div>
+                    <span class="text-xs text-gray-400 cursor-default">{{ __(ucfirst($sample->status)) }}</span>
                 </div>
-                @endif
+                <div class="mt-2 sm:mt-0">
+                    <a href="{!! urldecode(route('dashboard.samples.scoring', $sample->id)) !!}" class="inline-flex items-center px-4 h-8 text-xs text-white bg-brand hover:bg-brand-600 rounded-full transition ml-1 {{($sample->status =='closed' || (auth()->isAdmin() && $sample->status == 'done') || $sample->score_last_version != $sample->score_current_version ) && $sample->client ? '' : 'hidden'}}" data-lijax-preload="scoring-preload" data-lijax-succsess="" id="scoring-btn" data-method="POST">
+                        {{ __("Scoring") }}
+                    </a>
+                    @if (in_array($sample->status, ['seald', 'open']))
+                    <a href="{!! urldecode(route('dashboard.samples.close', $sample->id)) !!}" class="inline-flex items-center px-4 h-8 text-xs text-red-600 hover:text-white hover:bg-red-600 border border-red-600 rounded-full transition lijax status-action ml-1" data-method="PUT">
+                        {{ __("Close sample") }}
+                    </a>
+                    @elseif($sample->status == 'closed')
+                    <a href="{!! urldecode(route('dashboard.samples.open', $sample->id)) !!}" class="inline-flex items-center px-4 h-8 text-xs text-brand hover:text-white hover:bg-brand border border-brand rounded-full transition lijax status-action ml-1" data-method="PUT">
+                        {{ __("Open sample") }}
+                    </a>
+                    @endif
 
-                <div class="relative inline-flex dropdown {{in_array($sample->status, ['seald', 'open', 'closed', 'scoring']) ? 'hidden' : ''}}" id="profile-export-menu">
-                    <button class="flex items-center px-4 h-8 text-xs text-brand hover:text-white hover:bg-brand border border-brand rounded-full transition dropdown-toggle" type="button" id="profile-export" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        {{ __('Exports') }}
-                        <i class="fal fa-chevron-down mr-2"></i>
-                    </button>
-                    <div aria-labelledby="profile-export" id="profile-export-list" class="absolute left-0 top-10 rounded bg-white border border-gray-200 shadow-lg dropdown-menu">
-                        @if ($sample->profiles)
-                            @foreach ($sample->profiles->getAttributes() as $key => $item)
-                                <a href="{!!$item->url!!}" data-type="{{$key}}" class="dropdown-item direct profile-{{$key}} block w-full p-1 text-center rounded text-gray-600 hover:text-brand hover:bg-gray-100">
-                                    {{ strtoupper(str_replace('_', ' ', str_replace('profile_', '', $key))) }}
-                                </a>
-                            @endforeach
-                        @endif
+                    @if (config('app.env') == 'local' && in_array($sample->status, ['seald', 'open']))
+                    <div class="relative inline-flex dropdown ml-1">
+                        <button class="flex items-center px-4 h-8 text-xs text-brand hover:text-white hover:bg-brand border border-brand rounded-full transition" type="button" id="assessmentFill" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            {{ __('Fill in') }}
+                            <i class="fal fa-chevron-down mr-2"></i>
+                        </button>
+                        <div aria-labelledby="assessmentFill" class="absolute left-0 top-10 w-52 p-4 rounded bg-white border border-gray-200 shadow-lg">
+                            <form action="{{ config('app.server')}}/command/assessment/fill/{{substr($sample->id, 1) }}">
+                                <label class="flex items-center group">
+                                    <input type="checkbox" name="replace" checked class="w-3.5 h-3.5 border border-gray-600 rounded-sm focus:ring-1 focus:ring-offset-1">
+                                    <span class="text-sm text-gray-600 mr-2 group-hover:text-blue-600">{{ __('Edit prev data') }}</span>
+                                </label>
+                                <label class="flex items-center mt-2 group">
+                                    <input type="checkbox" name="empty" class="w-3.5 h-3.5 border border-gray-600 rounded-sm focus:ring-1 focus:ring-offset-1">
+                                    <span class="text-sm text-gray-600 mr-2 group-hover:text-blue-600">{{ __('Empty field') }}</span>
+                                </label>
+                                <label class="flex items-center mt-2 group">
+                                    <input type="checkbox" name="validity" class="w-3.5 h-3.5 border border-gray-600 rounded-sm focus:ring-1 focus:ring-offset-1">
+                                    <span class="text-sm text-gray-600 mr-2 group-hover:text-blue-600">{{ __('Validation') }}</span>
+                                </label>
+                                <div class="flex justify-center mt-4">
+                                    <button type="submit" class="flex items-center px-4 py-2 text-xs text-white bg-brand hover:bg-brand-600 rounded-full transition">{{ __("Fill in") }}</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    @endif
+
+                    <div class="relative inline-flex dropdown {{in_array($sample->status, ['seald', 'open', 'closed', 'scoring']) ? 'hidden' : ''}}" id="profile-export-menu">
+                        <button class="flex items-center px-4 h-8 text-xs text-brand hover:text-white hover:bg-brand border border-brand rounded-full transition dropdown-toggle" type="button" id="profile-export" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            {{ __('Exports') }}
+                            <i class="fal fa-chevron-down mr-2"></i>
+                        </button>
+                        <div aria-labelledby="profile-export" id="profile-export-list" class="absolute left-0 top-10 rounded bg-white border border-gray-200 shadow-lg dropdown-menu">
+                            @if ($sample->profiles)
+                                @foreach ($sample->profiles->getAttributes() as $key => $item)
+                                    <a href="{!!$item->url!!}" data-type="{{$key}}" class="dropdown-item direct profile-{{$key}} block w-full p-1 text-center rounded text-gray-600 hover:text-brand hover:bg-gray-100">
+                                        {{ strtoupper(str_replace('_', ' ', str_replace('profile_', '', $key))) }}
+                                    </a>
+                                @endforeach
+                            @endif
+                        </div>
                     </div>
                 </div>
             </div>
--- resources/views/samples/formAside.blade.php
+++ resources/views/samples/formAside.blade.php
@@ -25,7 +25,7 @@
 
     <div class="mb-4">
         <label class="flex items-center group">
-            <input id="skip" type="checkbox" class="w-3.5 h-3.5 border border-gray-500 rounded-sm focus:ring-1 focus:ring-offset-1">
+            <input id="skip" type="checkbox" class="w-3.5 h-3.5 border border-gray-500 hover:border-brand rounded-sm focus:ring-1 focus:ring-offset-1">
             <span class="text-sm text-gray-500 mr-2 group-hover:text-blue-600">{{ __('jump to empty test') }}</span>
         </label>
     </div>
