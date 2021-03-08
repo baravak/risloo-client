03f4668e701abb817b02ed35d3a43d6fd2aa47d9
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-03-08 08:32:39 +0330

    Update sample information

--- resources/views/samples/panel/information.blade.php
+++ resources/views/samples/panel/information.blade.php
@@ -1,9 +1,7 @@
-<div data-panel="information" style="display: none" class="mb-5">
-    <div class="card-title" >اطلاعات جمعیت‌شناختی و عمومی</div>
-    <div>
-        <h4>
-            بعد از تکمیل اطلاعات زیر به صفحه بعد رفته و پاسخ‌دهی را شروع کنید
-        </h4>
-        @include('samples.prerequisite')
+<div data-panel="information" style="hidden">
+    <div class="border border-gray-300 p-4 rounded mb-4">
+        <h2 class="font-medium text-lg mb-4">اطلاعات جمعیت‌شناختی و عمومی</h2>
+        <p class="text-gray-700">بعد از تکمیل اطلاعات زیر به صفحه بعد رفته و پاسخ‌دهی را شروع کنید</p>
     </div>
+    @include('samples.prerequisite')
 </div>
