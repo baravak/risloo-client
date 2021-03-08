540cba4665e025fd1f958ca802767b179032f75a
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-03-08 22:16:43 +0330

    Update sample radio text item

--- resources/views/samples/panel/items.blade.php
+++ resources/views/samples/panel/items.blade.php
@@ -1,11 +1,32 @@
-<div data-panel="item" style="display: none" class="mb-5">
-    <div class="card-title" >...</div>
-    <div id="item-section"></div>
-    <div id="template" style="display: none">
-        <div class="radio">
-            <input type="radio">
-            <label>
-                <span></span>
+<div data-panel="item" class="hidden">
+    <h3 class="text-lg text-gray-700 font-semibold mb-8">آیا از یک ماه گذشته تا به امروز کاملاً احساس کرده‌اید که خوب و سالم هستید؟</h3>
+    <div id="item-section">
+        <div class="sample-item-radio mb-4">
+            <input type="radio" value="1" name="options" id="1" class="absolute w-0 h-0 opacity-0">
+            <label for="1">
+                <span class="number">1</span>
+                <span class="answer">بیشتر از همیشه</span>
+            </label>
+        </div>
+        <div class="sample-item-radio mb-4">
+            <input type="radio" value="2" name="options" id="2" class="absolute w-0 h-0 opacity-0">
+            <label for="2">
+                <span class="number">2</span>
+                <span class="answer">مثل همیشه</span>
+            </label>
+        </div>
+        <div class="sample-item-radio mb-4">
+            <input type="radio" value="3" name="options" id="3" class="absolute w-0 h-0 opacity-0">
+            <label for="3">
+                <span class="number">3</span>
+                <span class="answer">بدتر از همیشه</span>
+            </label>
+        </div>
+        <div class="sample-item-radio mb-4">
+            <input type="radio" value="4" name="options" id="4" class="absolute w-0 h-0 opacity-0">
+            <label for="4">
+                <span class="number">4</span>
+                <span class="answer">خیلی بدتر از همیشه</span>
             </label>
         </div>
     </div>
