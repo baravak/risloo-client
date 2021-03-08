ab00bf20c6a9c0041b00d16c820d152675689f5a
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-03-08 08:40:52 +0330

    Minor fix

--- resources/views/samples/panel/information.blade.php
+++ resources/views/samples/panel/information.blade.php
@@ -1,4 +1,4 @@
-<div data-panel="information" style="hidden">
+<div data-panel="information" style="display:none">
     <div class="border border-gray-300 p-4 rounded mb-4">
         <h2 class="font-medium text-lg mb-4">اطلاعات جمعیت‌شناختی و عمومی</h2>
         <p class="text-gray-700">بعد از تکمیل اطلاعات زیر به صفحه بعد رفته و پاسخ‌دهی را شروع کنید</p>
