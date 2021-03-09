b7cef03225226fa3edbf6171f7c78f3547728903
Ahmad Karimi <ahmadkarimi1991@gmail.com>
2021-03-09 17:47:32 +0330

    Fix sessions select2

--- resources/views/dashboard/sessions/select2.blade.php
+++ resources/views/dashboard/sessions/select2.blade.php
@@ -1,10 +1,11 @@
 @foreach ($sessions as $session)
     <div data-id="{{ $session->id }}">
-        <div data-selection>
-            <div>{{ $session->id }}</div>
-            <div>@time($session->started_at, '%A %d %B %y ساعت H:i')</div>
-            <div>{{ __(':time minute(s)', ['time' => $session->duration]) }}</div>
-            <div>{{ __(ucfirst($session->status)) }}</div>
+        <div data-selection class="flex justify-between items-center text-xs">
+            <div>
+                <div class="font-bold">{{ $session->id }}</div>
+                <div>@time($session->started_at, '%A %d %B %y ساعت H:i')</div>
+            </div>
+            <span>{{ __(ucfirst($session->status)) }}</span>
         </div>
     </div>
 @endforeach
