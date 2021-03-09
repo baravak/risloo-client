fa420884db345812bbbe5b86d6c38bf1b968efb9
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-03-09 12:30:05 +0330

    Update sample radio image item

--- resources/views/samples/formBody.blade.php
+++ resources/views/samples/formBody.blade.php
@@ -4,20 +4,22 @@
         <h1 class="font-bold text-2xl text-gray-800">{{$sample->scale->title}}</h1>
         <span class="text-sm text-gray-500 mr-2">{{ $sample->edition }}</span>
     </div>
-    <div id="content" class="flex">
-        <div class="flex-1 pl-4">
+    <div id="content" class="flex flex-col lg:flex-row">
+        <div class="lg:flex-1 lg:pl-4 mb-4 lg:mb-0">
             <div class="flex h-1 rounded-full overflow-hidden bg-gray-200 mb-2">
                 <div id="progress" class="bg-brand transition" role="progressbar" style="width: 0%" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
             </div>
             <div class="border border-gray-300 p-4 rounded">
-                @includeWhen($sample->prerequisites, 'samples.panel.information')
+                {{-- @includeWhen($sample->prerequisites, 'samples.panel.information')
                 @include('samples.panel.description')
                 @include('samples.panel.items')
-                @include('samples.panel.close')
+                @include('samples.panel.close') --}}
+                @include('samples.items.imageItem')
             </div>
         </div>
-
-        @include('samples.formAside')
+        <div class="lg:flex-shrink-0 lg:w-56">
+            @include('samples.formAside')
+        </div>
     </div>
 </div>
 
