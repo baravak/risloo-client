f4d59fffe7172da34bdc77bd0949f12e3bec63c4
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-03-09 18:16:37 +0330

    Update sample form disconnected

--- resources/views/samples/formAside.blade.php
+++ resources/views/samples/formAside.blade.php
@@ -25,24 +25,4 @@
 <div class="mb-4 text-xs text-gray-500 mr-2 group-hover:text-blue-600" id="nav-empty-answers">
 </div>
 
-<div class="hidden text-xs bg-yellow-50 border border-yellow-200 rounded p-4" role="alert" id="sync_alert">
-    <p class="text-gray-700 font-semibold mb-2">ارتباط شما با سرور قطع شده است!</p>
-    <ul class="mt-4 pr-2 text-gray-600">
-        <li class="mt-2">شما می‌توانید بدون مشکل به کار خود ادامه دهید</li>
-        <li class="mt-1">سیستم به صورت خودکار این ارتباط را بررسی می‌کند و در صورت اتصال گزینه‌های پاسخ‌داده شده را ذخیره می‌کند</li>
-        <li>
-            اگر تا انتهای نمونه‌گیری ارتباط شما برقرار نشد طبق مراحل زیر عمل کنید:
-            <ol class="mt-2 pr-2">
-                <li>
-                    - بر روی <a href="" class="text-blue-600 hover:text-blue-700 direct" id="download">دریافت فایل تست</a> کلیک کنید
-                </li>
-                <li class="mt-1">
-                    - فایل دانلودی را پیش خود نگه‌داری کنید
-                </li>
-                <li class="mt-1">
-                    - فایل را به صورت دستی به روان‌شناس یا اپراتور مرکز مشاوره تحویل داده یا مستقیما در پنل کاربری خود بارگزاری کنید
-                </li>
-            </ol>
-        </li>
-    </ul>
-</div>
+@include('samples.disconnected')
