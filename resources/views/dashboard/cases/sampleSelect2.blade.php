6dc9bf8f1a464dbc63d6321109a66681766ed0c4
Ahmad Karimi <ahmadkarimi1991@gmail.com>
2021-03-09 17:57:51 +0330

    Fix cases select2

--- resources/views/dashboard/cases/sampleSelect2.blade.php
+++ resources/views/dashboard/cases/sampleSelect2.blade.php
@@ -1,16 +1,16 @@
 @foreach ($cases as $case)
     <div data-id="{{ $case->id }}">
         <div data-selection>
-            <div>{{ $case->id }}</div>
-            <div>{{ $case->clients->pluck('user.name')->join(', ') }}</div>
+            <div class="text-sm">{{ $case->id }}</div>
+            <div class="text-xs">{{ $case->clients->pluck('user.name')->join(', ') }}</div>
         </div>
 
         <div data-xhr="case-clients">
             @foreach ($case->clients as $client)
-                <div>
-                    <input type="checkbox" name="client_id[]" value="{{ $client->id }}">
-                    {{ $client->name }}
-                </div>
+                <label class="block mt-2 group">
+                    <input type="checkbox" name="client_id[]" value="{{ $client->id }}" class="w-3.5 h-3.5 align-middle border border-gray-500 hover:border-brand rounded-sm focus:ring-1 focus:ring-offset-1">
+                    <span class="text-sm text-gray-500 mr-2 group-hover:text-blue-600">{{ $client->name }}</span>
+                </label>
             @endforeach
         </div>
     </div>
