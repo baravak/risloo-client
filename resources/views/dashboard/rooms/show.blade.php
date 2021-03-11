d7cae2132c7d9578eb9d95a180feae8030faeaae
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-03-11 22:04:34 +0330

    Update cases form and heading

--- resources/views/dashboard/rooms/show.blade.php
+++ resources/views/dashboard/rooms/show.blade.php
@@ -20,18 +20,10 @@
         </div>
     </div>
     @can('viewAny', [App\TherapyCase::class, $room])
-        <div class="mb-4 mt-8">
-            <h3 class="flex items-center font-bold text-gray-700 cursor-default">
-                <span class="w-8 border-t border-gray-200 inline-block ml-3"></span>
-                <span>{{ __('Cases') }}</span>
-                <span class="text-xs text-gray-600 font-light mr-2" data-xhr="total">({{ $cases->total() }})</span>
-            </h3>
-        </div>
-
-        <div class="flex justify-between items-center flex-wrap mb-4">
-            @include('layouts.quick_search')
+        <div class="flex justify-between items-center flex-wrap mb-4 mt-8">
+            <h3 class="heading" data-total="({{ $cases->total() }})" data-xhr="total">{{ __('Cases') }}</h3>
             @can('create', [App\TherapyCase::class, $room])
-                <a href="{{ route('dashboard.room.cases.create', $room->id) }}" class="flex items-center justify-center flex-shrink-0 w-10 sm:w-auto h-9 sm:px-4 text-sm text-green-700 border border-green-700 rounded-full hover:bg-green-50 transition mr-4" title="{{ __('Create new case') }}">
+                <a href="{{ route('dashboard.room.cases.create', $room->id) }}" class="flex items-center justify-center flex-shrink-0 w-9 sm:w-auto h-9 sm:px-4 text-sm text-green-700 border border-green-700 rounded-full hover:bg-green-50 transition mr-4" title="{{ __('Create new case') }}">
                     <i class="fal fa-plus sm:ml-2"></i>
                     <span class="hidden sm:inline">{{ __('Create new case') }}</span>
                 </a>
