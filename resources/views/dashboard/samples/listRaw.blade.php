e9ad1c19e885e0a675d76e4bf529cd727bf4276c
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-03-11 09:09:14 +0330

    Minor fix

--- resources/views/dashboard/samples/listRaw.blade.php
+++ resources/views/dashboard/samples/listRaw.blade.php
@@ -6,8 +6,8 @@
     </td>
     <td class="px-3 py-2 whitespace-nowrap">
         <div claas="flex items-center">
-            <span class="block text-xs font-medium text-gray-700 cursor-default">{{ $sample->scale->title }}</span>
-            <span class="block text-gray-400 font-light text-xs">{{$sample->edition ? __('Edition :title', ['title' => $sample->edition]) .' - ' : ''}} {{ __('Version :ver', ['ver' => $sample->version]) }}</span>
+            <div class="flex"><span class="text-xs font-medium text-gray-700 cursor-default">{{ $sample->scale->title }}</span></div>
+            <div class="flex mt-1"><span class="text-gray-400 font-light text-xs">{{$sample->edition ? __('Edition :title', ['title' => $sample->edition]) .' - ' : ''}} {{ __('Version :ver', ['ver' => $sample->version]) }}</span></div>
         </div>
     </td>
     <td class="px-3 py-2 whitespace-nowrap">
@@ -21,9 +21,9 @@
     </td>
     <td class="px-3 py-2 whitespace-nowrap">
         <div claas="flex items-center">
-            <a href="{{ $sample->room->route('show') }}" class="text-xs text-gray-700 hover:text-blue-500">{{ __('Therapy room of :user', ['user' => $sample->room->manager->name]) }}</a>
+            <div class="flex"><a href="{{ $sample->room->route('show') }}" class="text-xs text-gray-700 hover:text-blue-500">{{ __('Therapy room of :user', ['user' => $sample->room->manager->name]) }}</a></div>
             @if ($sample->case)
-                <a class="block text-xs text-gray-500 hover:text-blue-500" href="{{ route('dashboard.cases.show', $sample->case->id) }}">@lang('Case') {{ $sample->case->id }}</a>
+                <div class="flex mt-1"><a class="text-xs text-gray-500 hover:text-blue-500" href="{{ route('dashboard.cases.show', $sample->case->id) }}">@lang('Case') {{ $sample->case->id }}</a></div>
             @endif
         </div>
     </td>
--- resources/views/dashboard/sessions/listRaw.blade.php
+++ resources/views/dashboard/sessions/listRaw.blade.php
@@ -1,50 +1,52 @@
 <tr>
     <td class="px-3 py-2 whitespace-nowrap">
         <div claas="flex items-center">
-            <span class="text-xs text-gray-700 block text-right dir-ltr cursor-default">{{ $session->id }}</span>
+            <span class="text-xs text-gray-600 block text-right dir-ltr cursor-default">{{ $session->id }}</span>
         </div>
     </td>
     <td class="px-3 py-2 whitespace-nowrap">
         <div claas="flex items-center">
-            <a href="{{ $session->case->room->route('show') }}" class="text-xs text-gray-700 block">
+            <div class="flex">
+                <a href="{{ $session->case->room->route('show') }}" class="text-xs text-gray-600 hover:text-blue-500 transition">
                 @lang('Therapy room of :user', ['user' => $session->case->room->manager->name])
-            </a>
-            <a href="{{ $session->case->room->center->route('show') }}" class="text-xs text-gray-500 block mt-1">
-                @if ($session->case->room->center->type == 'personal_clinic')
-                    @lang('Personal clinic')
-                @else
-                    {{ $session->case->room->center->detail->title }}
-                @endif
-            </a>
+                </a>
+            </div>
+            <div class="flex mt-1">
+                <a href="{{ $session->case->room->center->route('show') }}" class="text-xs text-gray-500 hover:text-blue-500 transition">
+                    @if ($session->case->room->center->type == 'personal_clinic')
+                        @lang('Personal clinic')
+                    @else
+                        {{ $session->case->room->center->detail->title }}
+                    @endif
+                </a>
+            </div>
         </div>
     </td>
     <td class="px-3 py-2 whitespace-nowrap">
         <div claas="flex items-center">
-            <a href="{{ $session->case->route('show') }}" class="text-xs text-gray-700 block">{{ $session->case->id }}</a>
+            <a href="{{ $session->case->route('show') }}" class="text-xs text-gray-600 hover:text-blue-500 transition">{{ $session->case->id }}</a>
         </div>
     </td>
     <td class="px-3 py-2 whitespace-nowrap">
         <div claas="flex items-center">
                 @foreach ($session->case->clients as $client)
-                    <a href="{{ route('dashboard.center.users.show', ['center' => $session->case->room->center->id, 'user' => $client->id]) }}" class="text-xs text-gray-700">
-                        @displayName($client)
-                    </a>
-                    @if (!$loop->last)
-                        <span class="text-xs text-gray-500">
-                        -
-                        </span>
-                    @endif
+                    <div class="flex">
+                        <a href="{{ route('dashboard.center.users.show', ['center' => $session->case->room->center->id, 'user' => $client->id]) }}" class="text-xs text-gray-600 hover:text-blue-500 transition">
+                            @displayName($client)
+                        </a>
+                    </div>
                 @endforeach
         </div>
     </td>
     <td class="px-3 py-2 whitespace-nowrap">
         <div claas="flex items-center">
-            <span class="text-xs text-gray-700 cursor-default">@time($session->started_at, '%A %d %B %y ساعت H:i')</span>
+            <div class="flex"><span class="text-xs text-gray-600 cursor-default">@time($session->started_at, '%A %d %B %y')</span></div>
+            <div class="flex"><span class="text-xs text-gray-600 cursor-default">@time($session->started_at, 'ساعت H:i')</span></div>
         </div>
     </td>
     <td class="px-3 py-2 whitespace-nowrap">
         <div claas="flex items-center">
-            <span class="text-xs text-gray-700 cursor-default">{{ __(':time minute(s)', ['time' => $session->duration]) }}</span>
+            <span class="text-xs text-gray-600 cursor-default">{{ __(':time minute(s)', ['time' => $session->duration]) }}</span>
         </div>
     </td>
     <td class="px-3 py-2 whitespace-nowrap">
