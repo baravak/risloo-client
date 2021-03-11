edac5d32d7c35b4850e8f2a8a8efdbad60b4740b
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-03-11 08:34:07 +0330

    Update sessions index

--- resources/views/dashboard/sessions/index.blade.php
+++ resources/views/dashboard/sessions/index.blade.php
@@ -5,11 +5,7 @@
             <h3 class="heading" data-total="({{ $sessions->total() }})" data-xhr="total">{{ __('Therapy sessions') }}</h3>
         </div>
 
-        <div class="flex justify-between items-center flex-wrap mb-4">
-            @include('layouts.quick_search')
-        </div>
-
-        @include($sessions->count() ? 'dashboard.sessions.list' : 'dashboard.emptySessions')
+        @include($sessions->count() ? 'dashboard.sessions.list' : 'dashboard.sessions.emptySessions')
 
         {{ $sessions->links() }}
     </div>
