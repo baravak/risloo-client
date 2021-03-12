d94df548a2658da5545ebe139d1912c080a9b3f1
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-03-12 11:43:23 +0330

    Update session select2

--- resources/views/dashboard/sessions/select2.blade.php
+++ resources/views/dashboard/sessions/select2.blade.php
@@ -1,12 +1,15 @@
 @foreach ($sessions as $session)
     <div data-id="{{ $session->id }}">
         <div data-selection class="flex flex-col text-xs cursor-pointer pt-1">
-            <div class="font-semibold text-gray-700">{{ $session->id }}</div>
+            <div class="inline-flex">
+                <span class="font-semibold text-gray-800">{{ $session->id }}</span>
+                <span class="mr-1 text-gray-600 font-normal">({{ __(ucfirst($session->status)) }})</span>
+            </div>
             <div class="inline-flex text-gray-500 font-light">
-                <span class="sm:hidden">@time($session->started_at, 'Y.m.d - H:i')</span>
-                <span class="hidden sm:block">@time($session->started_at, '%A %d %B %y ساعت H:i')</span>
-                <span class="mx-1">-</span>
-                <span>{{ __(ucfirst($session->status)) }}</span>
+                <span class="sm:hidden">@time($session->started_at, 'Y.m.d / س H:i /')</span>
+                <span class="hidden sm:block">@time($session->started_at, '%A Y.m.d / ساعت H:i /')</span>
+                <span class="mr-1 sm:hidden">{{ __(':time minute(s)', ['time' => $session->duration]) }}</span>
+                <span class="mr-1 hidden sm:block">به مدت {{ __(':time minute(s)', ['time' => $session->duration]) }}</span>
             </div>
         </div>
     </div>
