d24664599ff2170d9fce70cf46ca18bf8b44c87f
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-03-12 10:31:25 +0330

    Update session practice

--- resources/views/dashboard/sessions/practiceRaw.blade.php
+++ resources/views/dashboard/sessions/practiceRaw.blade.php
@@ -1,43 +1,45 @@
 <tr data-xhr="practice-{{ $practice->id }}">
     <td class="px-3 py-2 whitespace-nowrap">
         <div claas="flex items-center">
-            <span class="text-xs text-gray-700 block text-right dir-ltr cursor-default">{{$practice->id}}</span>
+            <span class="text-xs text-gray-600 block text-right dir-ltr cursor-default">{{$practice->id}}</span>
         </div>
     </td>
     <td class="px-3 py-2 whitespace-nowrap">
         <div claas="flex items-center">
-            <span class="text-xs text-gray-700 cursor-default">{{$practice->title}}</span>
+            <span class="text-xs text-gray-600 cursor-default">{{$practice->title}}</span>
         </div>
     </td>
     <td class="px-3 py-2 whitespace-nowrap">
         <div claas="flex items-center">
-            <span class="text-xs text-gray-700 cursor-default">{{ $practice->description }}</span>
+            <span class="text-xs text-gray-600 cursor-default">{{ $practice->description }}</span>
         </div>
     </td>
     <td class="px-3 py-2 whitespace-nowrap">
         <div claas="flex items-center">
-            <span class="text-xs text-gray-700 cursor-default">
-                @if ($practice->getRelationValue('attachments'))
-                    <a href="{{$practice->attachments->get('original')->url}}" target="_blank">{{__('Practice')}}</a>
-                @else
-                    {{__('No attachment')}}
-                @endif
-            </span>
+            @if ($practice->getRelationValue('attachments'))
+                <a href="{{$practice->attachments->get('original')->url}}" target="_blank" class="text-xs text-gray-600 hover:text-blue-600">
+                    <i class="far fa-cloud-download"></i>
+                    <span>{{ __('Downlaod attachment') }}</span>
+                </a>
+            @else
+                <span class="text-xs text-gray-600 cursor-default">{{__('No attachment')}}</span>
+            @endif
         </div>
     </td>
     <td class="px-3 py-2 whitespace-nowrap">
         <div claas="flex items-center">
-            <span class="text-xs text-gray-700 cursor-default">
-                @if ($practice->homework)
-                <a href="{{ $practice->homework->url}}" target="_blank">{{ __('Homework') }}</a>
-                @else
-                    <label class="btn btn-secondary btn-sm fs-10 cursor-pointer" for="homework-{{ $practice->id }}">
-                            <i class="far fa-cloud-upload"></i>
-                            {{ __('Upload homework') }}
-                    </label>
-                    <input data-action="{{ route('dashboard.sessions.practices.homework.store', ['session' => $session->id, $practice->id]) }}" data-method="post" data-lijax="change" type="file" class="hidden" name="attachment" id="homework-{{ $practice->id }}">
-                @endif
-            </span>
+            @if ($practice->homework)
+                <a href="{{ $practice->homework->url}}" target="_blank" class="text-xs text-gray-600 hover:text-blue-600">
+                    <i class="far fa-cloud-download"></i>
+                    <span>{{ __('Download homework') }}</span>
+                </a>
+            @else
+                <label for="homework-{{ $practice->id }}" class="text-xs text-gray-600 hover:text-blue-600 cursor-pointer">
+                        <i class="far fa-cloud-upload"></i>
+                        {{ __('Upload homework') }}
+                </label>
+                <input data-action="{{ route('dashboard.sessions.practices.homework.store', ['session' => $session->id, $practice->id]) }}" data-method="post" data-lijax="change" type="file" class="hidden" name="attachment" id="homework-{{ $practice->id }}">
+            @endif
         </div>
     </td>
 </tr>
--- resources/views/dashboard/sessions/practicesList.blade.php
+++ resources/views/dashboard/sessions/practicesList.blade.php
@@ -13,36 +13,8 @@
                 </thead>
                 <tbody class="divide-y divide-gray-200">
                     @foreach ($practices as $practice)
-                    @include('dashboard.sessions.practiceRaw')
+                        @include('dashboard.sessions.practiceRaw')
                     @endforeach
-{{--
-                        <tr>
-                            <td class="px-3 py-2 whitespace-nowrap">
-                                <div claas="flex items-center">
-                                    <span class="text-xs text-gray-700 block text-right dir-ltr cursor-default">P-9666697</span>
-                                </div>
-                            </td>
-                            <td class="px-3 py-2 whitespace-nowrap">
-                                <div claas="flex items-center">
-                                    <span class="text-xs text-gray-700 cursor-default">تمرین شماره 1</span>
-                                </div>
-                            </td>
-                            <td class="px-3 py-2 whitespace-nowrap">
-                                <div claas="flex items-center">
-                                    <span class="text-xs text-gray-700 cursor-default">با دقت به دیزاین نگاه بینداز و آنالیز کن. آفرین</span>
-                                </div>
-                            </td>
-                            <td class="px-3 py-2 whitespace-nowrap">
-                                <div claas="flex items-center">
-                                    <a href="#"><i class="fal fa-download text-smleading-relaxed text-gray-600 hover:text-blue-600"></i></a>
-                                </div>
-                            </td>
-                            <td class="px-3 py-2 whitespace-nowrap">
-                                <div claas="flex items-center">
-                                    <span class="text-xs text-gray-700 cursor-default">ندارد</span>
-                                </div>
-                            </td>
-                        </tr> --}}
                 </tbody>
             </table>
         </div>
