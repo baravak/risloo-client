31d34b221c56628c5d306966a72494613ec0ce36
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-03-12 09:28:55 +0330

    Minor fix

--- resources/views/dashboard/room-users/create.blade.php
+++ resources/views/dashboard/room-users/create.blade.php
@@ -1,9 +1,7 @@
 @extends('dashboard.create')
 @section('form_content')
-    <div class="border border-gray-200 rounded p-4 mt-8">
-        <div>
-            <label for="user_id" data-alias="manager_id" class="block mb-2 text-sm text-gray-700 font-medium">{{ __('User') }}</label>
-            <select class="select2-select" multiple data-template="users" name="user_id[]" data-title="user.name id" data-id="id" id="user_id" data-url="{{ route('dashboard.center.users.index', ['center' => $center->id, 'acceptation_room' => $room->id]) }}" data-avatar="user.avatar.tiny.url user.avatar.small.url"></select>
-        </div>
+    <div>
+        <label for="user_id" data-alias="manager_id" class="block mb-2 text-sm text-gray-700 font-medium">{{ __('User') }}</label>
+        <select class="select2-select" multiple data-template="users" name="user_id[]" data-title="user.name id" data-id="id" id="user_id" data-url="{{ route('dashboard.center.users.index', ['center' => $center->id, 'acceptation_room' => $room->id]) }}" data-avatar="user.avatar.tiny.url user.avatar.small.url"></select>
     </div>
 @endsection
