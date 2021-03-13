689dc8a6c49a97b167c2cf895651d167cc8f27ff
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-03-13 17:35:16 +0330

    Review users

--- resources/views/dashboard/users/forms/tabList.blade.bak.php
+++
@@ -1,3 +0,0 @@
-    <li class="nav-item">
-        <a class="nav-link direct fs-14" id="public-key-tab" data-toggle="tab" href="#public-key" role="tab" aria-controls="public-key" aria-selected="false">{{__('Public key')}}</a>
-    </li>
