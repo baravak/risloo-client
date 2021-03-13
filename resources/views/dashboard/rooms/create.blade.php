97eda377f71e6407fbd5255663362e489c036991
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-03-13 20:38:52 +0330

    Update create room

--- resources/views/dashboard/rooms/create.blade.php
+++ resources/views/dashboard/rooms/create.blade.php
@@ -1,20 +1,18 @@
 @extends('dashboard.create')
 @section('form_content')
     <div>
-        <div class="mt-4">
-            <label for="psychologist_id" class="block mb-2 text-sm text-gray-700 font-medium">{{ __('Psychologist') }}</label>
-            <select class="select2-select"  name="psychologist_id"  id="psychologist_id" data-url="{{ route('dashboard.center.users.index', ['center' => $center->id, 'position' => join(',',config('users.room_managers')), 'has_room' => 'no'])}}" data-placeholder="{{ __('Select :attribute', ['attribute' => __('Psychologist')]) }}">
-                @if (isset($user))
-                    <option value="{{$user->id}}">
-                        {{$user->name}}
-                    </option>
-                @endif
-            </select>
+        <label for="psychologist_id" class="block mb-2 text-sm text-gray-700 font-medium">{{ __('Psychologist') }}</label>
+        <select class="select2-select"  name="psychologist_id"  id="psychologist_id" data-url="{{ route('dashboard.center.users.index', ['center' => $center->id, 'position' => join(',',config('users.room_managers')), 'has_room' => 'no'])}}" data-placeholder="{{ __('Select :attribute', ['attribute' => __('Psychologist')]) }}">
             @if (isset($user))
-            <div data-for="psychologist_id">
-                @include('dashboard.center-users.select2', ['users' => [$user]])
-            </div>
+                <option value="{{$user->id}}">
+                    {{$user->name}}
+                </option>
             @endif
+        </select>
+        @if (isset($user))
+        <div data-for="psychologist_id">
+            @include('dashboard.center-users.select2', ['users' => [$user]])
         </div>
+        @endif
     </div>
 @endsection
