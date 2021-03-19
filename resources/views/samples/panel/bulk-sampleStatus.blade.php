83645c281c29bc46fafb5ae80d23f0f18e667c6d
abouhatef <itb.baravak@gmial.com>
2021-03-19 12:53:10 +0330

    chain sample

--- resources/views/samples/panel/bulk-sampleStatus.blade.php
+++ resources/views/samples/panel/bulk-sampleStatus.blade.php
@@ -1,28 +1,14 @@
-<div class="hidden">
+<div data-nav="chain" data-title="@lang('Chain')" class="hidden">
     <div class="mb-8">
         <h2 class="font-medium text-gray-800 text-lg mb-2">وضعیت نمونه گروهی</h2>
     </div>
 
     <div class="cursor-default bulk-sampleStatus">
-        <div class="flex items-center text-gray-500">
-            <i class="fal fa-circle ml-4 relative"></i>
-            <span>آزمون تیپ‌نمای مایرز-بریگز</span>
-        </div>
-        <div class="flex items-center text-brand font-semibold mt-6">
-            <i class="fas fa-chevron-circle-left ml-4 relative"></i>
-            <span>پرسشنامه 16 عاملی شخصیت کتل</span>
-        </div>
-        <div class="flex items-center text-gray-500 mt-6">
-            <i class="fal fa-circle ml-4 relative"></i>
-            <span>آزمون اضطراب کتل (CAQ)</span>
-        </div>
-        <div class="flex items-center text-gray-500 mt-6">
-            <i class="fal fa-circle ml-4 relative"></i>
-            <span>آزمون افسردگی بک</span>
-        </div>
-        <div class="flex items-center text-gray-500 mt-6">
-            <i class="fal fa-circle ml-4 relative"></i>
-            <span>پرسشنامه نئو</span>
-        </div>
+        @foreach ($sample->chain->list as $chain)
+            <div class="flex items-center {{ $chain->id == $sample->id ? 'text-brand font-semibold' : 'text-gray-500' }} {{ !$loop->first ? 'mt-6' : '' }}">
+                <i class="{{ $chain->id == $sample->id ? 'fas fa-chevron-circle-left' : 'fal fa-circle' }} ml-4 relative"></i>
+                <span>{{ $chain->title }}</span>
+            </div>
+        @endforeach
     </div>
 </div>
