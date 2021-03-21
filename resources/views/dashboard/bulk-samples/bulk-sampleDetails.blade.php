6ccc621d5b92b0d9606a4c1b5f8da067f56e565b
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-03-21 16:59:54 +0330

    Update bulk sample details

--- resources/views/dashboard/bulk-samples/bulk-sampleDetails.blade.php
+++ resources/views/dashboard/bulk-samples/bulk-sampleDetails.blade.php
@@ -1,10 +1,10 @@
 <div class="flex justify-between border border-gray-300 rounded p-4 cursor-default">
     <div>
         <div class="flex items-center relative mb-4">
-            <a href="{{ route('dashboard.centers.show', $bulkSample->room->center->id) }}" class="flex justify-center items-center flex-shrink-0 w-12 h-w-12 bg-gray-300 text-gray-600 text-xs rounded-full border-2 border-white overflow-hidden">
+            <a href="{{ route('dashboard.centers.show', $bulkSample->room->center->id) }}" class="flex justify-center items-center flex-shrink-0 w-12 h-12 bg-gray-300 text-gray-600 text-xs rounded-full border-2 border-white overflow-hidden">
                 <img src="@attachmentLink($bulkSample->room->center->detail->avatar, 'samll')" class="w-full h-full object-cover object-center">
             </a>
-            <a href="{{ route('dashboard.rooms.show', $bulkSample->room->id) }}" class="flex justify-center items-center flex-shrink-0 w-8 h-w-8 bg-gray-300 text-gray-600 text-xs rounded-full border-2 border-white overflow-hidden absolute top-6 right-6">
+            <a href="{{ route('dashboard.rooms.show', $bulkSample->room->id) }}" class="flex justify-center items-center flex-shrink-0 w-8 h-8 bg-gray-300 text-gray-600 text-xs rounded-full border-2 border-white overflow-hidden absolute top-6 right-6">
                 <img src="@attachmentLink($bulkSample->room->manager->avatar, 'samll')" class="w-full h-full object-cover object-center">
             </a>
             <div class="flex flex-col mr-4">
@@ -17,6 +17,10 @@
                 <i class="fal fa-clipboard text-sm eading-normal ml-2"></i>
                 <span class="text-xs">@lang('Case :serial', ['serial' => $bulkSample->case->id])</span>
             </a>
+            {{-- <span class="flex items-center text-gray-600 cursor-default">
+                <i class="fal fa-clipboard text-sm eading-normal ml-2"></i>
+                <span class="text-xs">{{ __('Without case') }}</span>
+            </span> --}}
         @endif
         <div class="flex items-center text-sm text-gray-700 mt-2">
             <span class="text-sm text-gray-400">@lang(ucfirst($bulkSample->status))</span>
@@ -27,9 +31,11 @@
             <i class="fal fa-copy mr-2 text-xl"></i>
             <span class="font-semibold text-sm">{{ $bulkSample->id }}</span>
         </div>
-        <div>
-            <a href="{{ route('auth', ['authorized_key'=> $bulkSample->link]) }}" target="_blank" class="flex items-center justify-center border border-gray-500 rounded-full text-xs text-gray-600 h-8 px-2 hover:bg-gray-50">لینک ثبت نام</a>
-            <span class="text-xs inline-block border p-1 rounded" data-clipboard-text="{{ route('auth', ['authorized_key' => $bulkSample->link]) }}">کپی</span>
+        <div class="inline-flex items-center border border-gray-600 rounded-full text-xs text-gray-600">
+            <div data-clipboard-text="{{ route('auth', ['authorized_key' => $bulkSample->link]) }}" class="pr-3 pl-2 py-2 cursor-pointer border-l border-gray-300 hover:bg-gray-100 transition rounded-r-full">
+                <i class="fal fa-copy relative top-0.5"></i>
+            </div>
+            <a href="{{ route('auth', ['authorized_key' => $bulkSample->link]) }}" target="_blank" class="pl-3 pr-2 py-2 hover:bg-gray-100 transition rounded-l-full">{{ __('Registration link') }}</a>
         </div>
     </div>
 </div>
