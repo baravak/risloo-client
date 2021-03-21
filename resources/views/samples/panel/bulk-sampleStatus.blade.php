3eea585dabe84f230bf7d7cf961aca41430d0237
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-03-21 12:20:49 +0330

    Update bulk sample status

--- resources/views/samples/panel/bulk-sampleStatus.blade.php
+++ resources/views/samples/panel/bulk-sampleStatus.blade.php
@@ -1,6 +1,6 @@
 <div data-nav="chain" data-title="@lang('Chain')" class="hidden">
     <div class="mb-8">
-        <h2 class="font-medium text-gray-800 text-lg mb-2">وضعیت نمونه گروهی</h2>
+        <h2 class="font-medium text-gray-800 text-lg mb-2">{{ __('bulk sample status') }}</h2>
     </div>
 
     <div class="cursor-default bulk-sampleStatus">
@@ -10,5 +10,13 @@
                 <span>{{ $chain->title }}</span>
             </div>
         @endforeach
+        {{-- <div class="flex items-center text-gray-500 opacity-50">
+            <i class="fal fa-check-circle ml-4 relative"></i>
+            <span>حالت انجام شده‌ی غیرفعال</span>
+        </div>
+        <a href="#" class="flex items-center text-gray-500 hover:text-brand">
+            <i class="fal fa-circle ml-4 relative"></i>
+            <span>حالت انجام نشده‌ی لینک دار</span>
+        </a> --}}
     </div>
 </div>
