d5b0e40f4dbef608e943c9ff2e125727a819a310
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-03-21 17:43:21 +0330

    Update center user profile

--- resources/views/dashboard/center-users/show.blade.php
+++ resources/views/dashboard/center-users/show.blade.php
@@ -25,80 +25,76 @@
         </div>
     </div>
 
-    <div class="flex mt-8">
-
-        <div class="w-1/3 pl-2">
-
-            <div class="mb-4">
-                <h3 class="heading" data-total="({{ $user->rooms ? $user->rooms->count() : 0 }})" data-xhr="total">{{ __('Therapy rooms') }}</h3>
-            </div>
-
-            <div class="grid grid-cols-1 2xl:grid-cols-2 gap-4">
-                <div class="border border-gray-200 rounded">
-                    @foreach ($user->rooms ?: [] as $room)
-                        <a href="{{ $room->route('show') }}" class="flex items-center p-2 hover:bg-gray-50 transition border-b border-gray-200">
-                            <div class="flex justify-center items-center flex-shrink-0 w-14 h-14 bg-gray-300 text-gray-600 rounded-full border-2 border-white overflow-hidden">
-                                @avatarOrName($room->manager)
-                            </div>
-                            <div class="mr-4">
-                                <span class="block text-sm text-gray-700 font-medium">@displayName($room->manager)</span>
-                            </div>
-                        </a>
-                    @endforeach
-                    {{-- <a href="#" class="flex justify-center items-center py-4 hover:bg-gray-50 transition text-xs text-gray-400">بیشتر...</a> --}}
-                </div>
-            </div>
+    <div class="mt-8">
+        <div class="mb-4">
+            <h3 class="heading" data-total="({{ $user->rooms ? $user->rooms->count() : 0 }})" data-xhr="total">{{ __('Therapy rooms') }}</h3>
         </div>
 
-        <div class="w-2/3 pr-2">
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">
+            @foreach ($user->rooms ?: [] as $room)
+                <a href="{{ $room->route('show') }}" class="border border-gray-200 rounded hover:bg-gray-50 transition">
+                    <div class="h-16 bg-gray-100 border-b border-gray-200"></div>
 
-            <div class="mb-4">
-                <h3 class="heading" data-total="({{ $user->cases ? $user->cases->count() : 0 }})" data-xhr="total">{{ __('Cases') }}</h3>
-            </div>
-
-            <div class="overflow-x-auto">
-                <div class="align-middle inline-block min-w-full">
-                    <div class="overflow-hidden border border-gray-200 rounded">
-                        <table class="min-w-full divide-y divide-gray-200">
-                            <thead class="bg-gray-50">
-                                <tr>
-                                    <th class="px-3 py-2 text-right text-xs font-medium text-gray-500" scope="col">{{ __('Serial') }}</th>
-                                    <th class="px-3 py-2 text-right text-xs font-medium text-gray-500" scope="col">{{ __('Room') }}</th>
-                                    <th class="px-3 py-2 text-right text-xs font-medium text-gray-500" scope="col">{{ __('Sessions') }}</th>
-                                    <th class="px-3 py-2" scope="col"></th>
-                                </tr>
-                            </thead>
-                            <tbody class="divide-y divide-gray-200">
-                                @foreach ($user->cases ?: [] as $case)
-                                    <tr data-xhr="case-list">
-                                        <td class="px-3 py-2 whitespace-nowrap">
-                                            <div claas="flex items-center">
-                                                <span class="text-xs text-gray-700 block text-right dir-ltr cursor-default">{{ $case->id }}</span>
-                                            </div>
-                                        </td>
-                                        <td class="px-3 py-2 whitespace-nowrap">
-                                            <div claas="flex items-center">
-                                                <a href="{{ $case->room->route('show') }}" class="text-xs text-gray-700 hover:text-brand">@displayName($case->room->manager)</a>
-                                            </div>
-                                        </td>
-                                        <td class="px-3 py-2 whitespace-nowrap">
-                                            <div claas="flex items-center">
-                                                <span class="text-xs text-gray-700 cursor-default">{{ $case->sessions_count }}</span>
-                                            </div>
-                                        </td>
-                                        <td class="px-3 py-2 whitespace-nowrap text-left dir-ltr">
-                                            <div class="inline-block mr-4">
-                                                <a href="{{ $case->route('show') }}"><i class="fal fa-eye text-sm text-gray-600 hover:text-blue-600 relative top-0.5"></i></a>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
+                    <div class="flex justify-center items-center flex-shrink-0 w-20 h-20 mx-auto -mt-10 bg-gray-300 text-gray-600 rounded-full border-4 border-white overflow-hidden">
+                        @avatarOrName($room->manager)
                     </div>
+
+                    <div class="p-4">
+                        <div class="text-sm sm:text-base text-center text-gray-700 font-medium">
+                            <span>@displayName($room->manager)</span>
+                        </div>
+                    </div>
+                </a>
+            @endforeach
+        </div>
+    </div>
+
+    <div class="mt-8">
+        <div class="mb-4">
+            <h3 class="heading" data-total="({{ $user->cases ? $user->cases->count() : 0 }})" data-xhr="total">{{ __('Cases') }}</h3>
+        </div>
+
+        <div class="overflow-x-auto">
+            <div class="align-middle inline-block min-w-full">
+                <div class="overflow-hidden border border-gray-200 rounded">
+                    <table class="min-w-full divide-y divide-gray-200">
+                        <thead class="bg-gray-50">
+                            <tr>
+                                <th class="px-3 py-2 text-right text-xs font-medium text-gray-500" scope="col">{{ __('Serial') }}</th>
+                                <th class="px-3 py-2 text-right text-xs font-medium text-gray-500" scope="col">{{ __('Room') }}</th>
+                                <th class="px-3 py-2 text-right text-xs font-medium text-gray-500" scope="col">{{ __('Sessions') }}</th>
+                                <th class="px-3 py-2" scope="col"></th>
+                            </tr>
+                        </thead>
+                        <tbody class="divide-y divide-gray-200">
+                            @foreach ($user->cases ?: [] as $case)
+                                <tr data-xhr="case-list">
+                                    <td class="px-3 py-2 whitespace-nowrap">
+                                        <div claas="flex items-center">
+                                            <span class="text-xs text-gray-700 block text-right dir-ltr cursor-default">{{ $case->id }}</span>
+                                        </div>
+                                    </td>
+                                    <td class="px-3 py-2 whitespace-nowrap">
+                                        <div claas="flex items-center">
+                                            <a href="{{ $case->room->route('show') }}" class="text-xs text-gray-700 hover:text-brand">@displayName($case->room->manager)</a>
+                                        </div>
+                                    </td>
+                                    <td class="px-3 py-2 whitespace-nowrap">
+                                        <div claas="flex items-center">
+                                            <span class="text-xs text-gray-700 cursor-default">{{ $case->sessions_count }}</span>
+                                        </div>
+                                    </td>
+                                    <td class="px-3 py-2 whitespace-nowrap text-left dir-ltr">
+                                        <div class="inline-block mr-4">
+                                            <a href="{{ $case->route('show') }}"><i class="fal fa-eye text-sm text-gray-600 hover:text-blue-600 relative top-0.5"></i></a>
+                                        </div>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
-            {{-- <a href="#" class="flex justify-center items-center py-2 hover:bg-gray-50 transition text-xs text-gray-400 border border-gray-200 rounded mt-2">بیشتر...</a> --}}
         </div>
     </div>
 
