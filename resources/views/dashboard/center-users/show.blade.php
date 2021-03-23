2594a3341b2309db04cf376557f59e6f2e0ca926
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-03-23 18:45:36 +0430

    Minor fix

--- resources/views/dashboard/center-users/show.blade.php
+++ resources/views/dashboard/center-users/show.blade.php
@@ -126,8 +126,8 @@
                                         </div>
                                     </td>
                                     <td class="px-3 py-2 whitespace-nowrap">
-                                        <div class="flex items-center">
-                                            <span class="block text-xs font-medium text-gray-700 cursor-default">{{ $sample->scale->title }}</span>
+                                        <div class="flex flex-col cursor-default">
+                                            <span class="block text-xs font-medium text-gray-700">{{ $sample->scale->title }}</span>
                                             <span class="block text-gray-400 font-light text-xs">
                                                 {{$sample->edition ? __('Edition :title', ['title' => $sample->edition]) .' - ' : ''}} {{ __('Version :ver', ['ver' => $sample->version]) }}
                                             </span>
