ce0da0c971017dbfd46652cbbb620b7bfa41eecb
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-03-24 19:35:16 +0430

    Update do sample button

--- resources/views/dashboard/assessments/assessmentsList.blade.php
+++ resources/views/dashboard/assessments/assessmentsList.blade.php
@@ -37,7 +37,7 @@
                         <td class="px-3 py-2 whitespace-nowrap text-left dir-ltr">
                             @can('dashboard.sessions.create')
                             <div class="inline-flex">
-                                <a href="{{ route('dashboard.samples.create', ['scale' => substr($assessment->id, 1)]) }}" class="block px-4 py-1 text-xs text-brand border border-brand hover:bg-blue-50 rounded-full">{{ __('Create sample') }}</a>
+                                <a href="{{ route('dashboard.samples.create', ['scale' => substr($assessment->id, 1)]) }}" class="block px-4 py-1 text-xs text-brand border border-brand hover:bg-brand hover:text-white rounded-full transition">{{ __('Create sample') }}</a>
                             </div>
                             @endcan
                         </td>
