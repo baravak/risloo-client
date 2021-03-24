e7acc9b6e5e7ef8ade18e39092849cad16a6ed3c
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-03-24 13:22:42 +0430

    Update header user

--- resources/views/davat/layouts/headerUser.blade.php
+++ resources/views/davat/layouts/headerUser.blade.php
@@ -1,11 +1,25 @@
 <div class="flex" data-xhr="user-header-nav">
-    @if (false)
-        <a href="#" class="flex justify-center items-center w-12 h-12 border border-gray-300 text-gray-400 rounded hover:bg-gray-50 ml-2">
-            <i class="fal fa-bell text-xl text-gray-500"></i>
+
+    <a href="{{ route('dashboard.users.me') }}" class="flex items-center border border-gray-300 rounded px-3 h-12 hover:bg-gray-50 transition dropdown-toggle focus">
+        <div class="flex justify-center items-center flex-shrink-0 w-8 h-8 rounded overflow-hidden ml-3 bg-blue-500 text-white border border-gray-200 text-xs" data-xhr="short-avatar">
+            @avatarOrName(auth()->user())
+        </div>
+        <div class="flex justify-between items-center">
+            <div class="font-medium text-xs text-gray-700">{{ auth()->user()->name ?: auth()->user()->id }}</div>
+        </div>
+    </a>
+
+    @if (auth()->user()->response('current'))
+        <a href="{{ route('auth.back') }}" data-lijax='click' data-method='post' class="flex justify-center items-center w-12 h-12 border border-gray-300 text-gray-400 rounded hover:bg-gray-50 hover:text-brand mr-2 group direct" title="{{ __('Admin') }}">
+            <i class="fal fa-user-crown text-xl text-gray-500 group-hover:text-brand"></i>
         </a>
     @endif
 
-    <div class="relative dropdown">
+    <a href="{{ route('logout') }}" data-lijax='click' data-method='post' class="flex justify-center items-center w-12 h-12 border border-gray-300 text-gray-400 rounded hover:bg-gray-50 hover:text-red-500 mr-2 group focus direct" title="{{ __('Logout') }}">
+        <i class="fal fa-sign-out text-xl text-gray-500 group-hover:text-red-500 transform rotate-180"></i>
+    </a>
+
+    {{-- <div class="relative dropdown">
         <button type="button" class="flex items-center border border-gray-300 rounded px-3 h-12 hover:bg-gray-50 transition dropdown-toggle focus">
             <div class="flex justify-center items-center flex-shrink-0 w-8 h-8 rounded overflow-hidden ml-3 bg-blue-500 text-white border border-gray-200 text-xs" data-xhr="short-avatar">
                 @avatarOrName(auth()->user())
@@ -13,10 +27,10 @@
             <div class="flex justify-between items-center">
                 <div>
                     <div class="font-medium text-xs text-gray-700">{{ auth()->user()->name ?: auth()->user()->id }}</div>
-                    {{-- <div class="text-xs text-green-500 mt-1 dir-ltr">
+                    <div class="text-xs text-green-500 mt-1 dir-ltr">
                         <span class="inline-block">تومان</span>
                         <span class="inline-block">۱۰۰,۰۰۰,۰۰۰</span>
-                    </div> --}}
+                    </div>
                 </div>
             </div>
         </button>
@@ -25,11 +39,5 @@
             <a href="#" class="block text-sm text-gray-700 text-center py-3 hover:bg-gray-100 border-b border-gray-100 transition">@lang('Financial treasuries')</a>
             <a href="{{ route('logout') }}" data-lijax='click' data-method='post' title="{{ __('Logout') }}" class="block text-sm text-red-600 text-center py-3 hover:bg-gray-100 transition">@lang('Logout')</a>
         </div>
-    </div>
-
-    @if (auth()->user()->response('current'))
-        <a href="{{ route('auth.back') }}" data-lijax='click' data-method='post' class="flex justify-center items-center w-12 h-12 border border-gray-300 text-gray-400 rounded hover:bg-gray-50 hover:text-brand mr-2 group direct" title="{{ __('Admin') }}">
-            <i class="fal fa-user-crown text-xl text-gray-500 group-hover:text-brand"></i>
-        </a>
-    @endif
+    </div> --}}
 </div>
