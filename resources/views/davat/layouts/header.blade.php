be0ce4b15d8be8a7576a1e5282516ede0e821de4
Ahmad Karimi <ahmadkarimi1991@gmail.com>
2021-03-25 13:14:58 +0430

    Add aria label to menu button

--- resources/views/davat/layouts/header.blade.php
+++ resources/views/davat/layouts/header.blade.php
@@ -1,6 +1,6 @@
 <header id="header" class="flex justify-between items-center h-20 px-4">
     <div>
-        <button id="aside-open" class="lg:hidden flex justify-center items-center w-12 h-12 border border-gray-300 text-gray-400 rounded hover:bg-gray-50">
+        <button id="aside-open" aria-label="@lang('Menu')" class="lg:hidden flex justify-center items-center w-12 h-12 border border-gray-300 text-gray-400 rounded hover:bg-gray-50">
             <i class="fal fa-bars text-xl text-gray-500"></i>
         </button>
     </div>
