2e99e4e679440b6b244f74fdf41fd33707f1c14f
Ahmad Karimi <ahmadkarimi1991@gmail.com>
2021-03-25 08:12:23 +0430

    Disable hotjar

--- resources/views/davat/layouts/_head-styles.blade.php
+++ resources/views/davat/layouts/_head-styles.blade.php
@@ -29,17 +29,19 @@
     </style>
     <link rel="stylesheet" href="@staticVersion('/css/davat.css')" media="print" onload="this.media='all'; this.onload = null">
 
-    <!-- Hotjar Tracking Code for risloo.ir -->
-    @if (config('app.env') != 'local')
-        <script>
-            (function(h,o,t,j,a,r){
-                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                h._hjSettings={hjid:2262407,hjsv:6};
-                a=o.getElementsByTagName('head')[0];
-                r=o.createElement('script');r.async=1;
-                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                a.appendChild(r);
-            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-        </script>
+    @if (false)
+        <!-- Hotjar Tracking Code for risloo.ir -->
+        @if (config('app.env') != 'local')
+            <script>
+                (function(h,o,t,j,a,r){
+                    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                    h._hjSettings={hjid:2262407,hjsv:6};
+                    a=o.getElementsByTagName('head')[0];
+                    r=o.createElement('script');r.async=1;
+                    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                    a.appendChild(r);
+                })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            </script>
+        @endif
     @endif
 @endsection
