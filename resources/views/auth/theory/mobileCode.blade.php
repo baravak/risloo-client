9b7d3b527ac643a693a247a86b0f087750b46737
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-03-26 20:06:01 +0430

    Update auth

--- resources/views/auth/theory/mobileCode.blade.php
+++ resources/views/auth/theory/mobileCode.blade.php
@@ -1,9 +1,9 @@
 @section('auth-form')
-    <div class="mb-4">
-        <input type="authorized_key"  disabled value="{{ $theory->response('authorized_key') }}" class="w-full text-sm text-left dir-ltr placeholder-right border border-gray-200 rounded-sm" id="authorized_key" name="authorized_key">
+    <div class="mb-2">
+        <input type="authorized_key"  disabled value="{{ $theory->response('authorized_key') }}" class="w-full text-sm text-left dir-ltr text-gray-400 border border-gray-200 rounded h-10 px-3" id="authorized_key" name="authorized_key">
     </div>
     <div class="mb-4">
-        <input type="password" class="w-full text-sm text-left dir-ltr placeholder-right border border-gray-200 rounded-sm" id="code" name="code" placeholder="{{ __('Code')}} ">
+        <input type="password" class="w-full h-10 text-sm text-left dir-ltr placeholder-right border border-gray-200 rounded" id="code" name="code" placeholder="{{ __('Sms code')}} ">
     </div>
 
     <button class="w-full h-10 text-sm rounded-full bg-brand text-white hover:bg-brand-700 transition mb-8 focus">{{ __('Check') }}</button>
--- resources/views/auth/theory/recovery.blade.php
+++ resources/views/auth/theory/recovery.blade.php
@@ -1,9 +1,9 @@
 @section('auth-form')
 <div class="mb-4">
-    <input type="authorized_key"  disabled value="{{ $theory->response('authorized_key') }}" class="w-full text-sm text-left dir-ltr placeholder-right border border-gray-200 rounded-sm" id="authorized_key" name="authorized_key">
+    <input type="authorized_key"  disabled value="{{ $theory->response('authorized_key') }}" class="w-full text-sm text-left dir-ltr text-gray-400 border border-gray-200 rounded h-10 px-3" id="authorized_key" name="authorized_key">
 </div>
     <div class="mb-4">
-        <input type="password" class="w-full text-sm text-left dir-ltr placeholder-right border border-gray-200 rounded-sm" id="password" name="password" placeholder="{{ __('Password') }}">
+        <input type="password" class="w-full h-10 text-sm text-left dir-ltr placeholder-right border border-gray-200 rounded" id="password" name="password" placeholder="{{ __('Password') }}">
     </div>
 
     <button class="w-full h-10 text-sm rounded-full bg-brand text-white hover:bg-brand-700 transition mb-8 focus">{{ __('Change password' )}}</button>
