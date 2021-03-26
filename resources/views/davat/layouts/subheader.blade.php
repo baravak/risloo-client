98f13abe3ea26aebfa9cabb64744ebd4ea14ec2e
Ahmad Karimi <ahmadkarimi1991@gmail.com>
2021-03-27 03:34:45 +0430

    Fix breadcrumbs responsive

--- resources/views/davat/layouts/subheader.blade.php
+++ resources/views/davat/layouts/subheader.blade.php
@@ -1,5 +1,5 @@
 @if (Breadcrumbs::exists(\Route::getCurrentRoute()->getAction('as')))
-    <nav class="flex items-center mb-4" data-xhr="subheader">
+    <nav class="flex flex-wrap items-center mb-4" data-xhr="subheader">
         {{ Breadcrumbs::render(\Route::getCurrentRoute()->getAction('as'), get_defined_vars()) }}
     </nav>
 @endif
