3213056e79508779d3da55855a8b046fc917a594
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-03-27 18:16:05 +0430

    Minor fix

--- resources/views/auth/theory/mobileCode.blade.php
+++ resources/views/auth/theory/mobileCode.blade.php
@@ -3,7 +3,7 @@
         <input type="authorized_key"  disabled value="{{ $theory->response('authorized_key') }}" class="w-full text-sm text-left dir-ltr text-gray-400 border border-gray-200 rounded h-10 px-3" id="authorized_key" name="authorized_key">
     </div>
     <div class="mb-4">
-        <input type="password" class="w-full h-10 text-sm text-left dir-ltr placeholder-right border border-gray-200 rounded" id="code" name="code" placeholder="{{ __('Sms code')}} ">
+        <input type="password" class="w-full h-10 text-sm text-left dir-ltr placeholder-right border border-gray-200 rounded" id="code" name="code" placeholder="{{ __('Sms code')}}" autocomplete="off" autofocus>
     </div>
 
     <button class="flex items-center justify-center w-full h-10 text-sm rounded-full bg-brand text-white hover:bg-brand-600 transition mb-8 focus" title="{{ __('Check') }}">{{ __('Check') }}</button>
