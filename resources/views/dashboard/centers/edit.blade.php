3afd44334a83138a65ff182e60cf526bc78b70f0
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-03-27 08:24:39 +0430

    Fix center edit tabs

--- resources/views/dashboard/centers/edit.blade.php
+++ resources/views/dashboard/centers/edit.blade.php
@@ -2,8 +2,8 @@
 @section('form-tag')
     <div class="mx-auto w-full sm:w-1/2">
         <ul data-tabs>
-            <li><a class="focus" href="#information-tab" data-tabby-default class="direct">{{ __('information') }}</a></li>
-            <li><a class="focus" href="#avatar-tab" class="direct">{{ __('Avatar') }}</a></li>
+            <li><a href="#information-tab" data-tabby-default class="direct focus flex" title="{{ __('information') }}">{{ __('information') }}</a></li>
+            <li><a href="#avatar-tab" class="direct focus flex" title="{{ __('Avatar') }}">{{ __('Avatar') }}</a></li>
         </ul>
 
             <div id="information-tab" class="p-4 border border-gray-200 rounded">
