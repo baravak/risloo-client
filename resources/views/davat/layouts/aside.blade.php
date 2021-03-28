ab25a2db5cd6cc00148a70dc1c816400f1a908b6
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-03-28 14:40:53 +0430

    Update aside menu

--- resources/views/davat/layouts/aside.blade.php
+++ resources/views/davat/layouts/aside.blade.php
@@ -1,7 +1,7 @@
 @section('aside')
     <aside id="aside" class="bg-gray-50 border-l border-gray-200 overflow-y-auto">
         <div class="flex items-center h-20 px-2">
-            <h1 class="text-2xl font-black text-brand">
+            <h1 class="text-2xl variable-font-black text-brand">
                 <a href="/" target="_blank" class="block">{{ __('App Title') }}</a>
             </h1>
         </div>
@@ -11,7 +11,7 @@
                 <a href="{{ route('dashboard.home') }}" data-metarget-default class="flex justify-between items-center h-12 px-6 rounded text-gray-900 hover:bg-gray-200 transition">
                     <div class="flex items-center">
                         <i class="fal fa-tachometer-alt-fastest ml-2"></i>
-                        <span class="font-light">{{ __('Dashboard') }}</span>
+                        <span class="variable-font-light pt-1">{{ __('Dashboard') }}</span>
                     </div>
                 </a>
             </li>
@@ -19,7 +19,7 @@
                 <a href="{{ route('dashboard.centers.index') }}" data-metarget="centers-index" class="flex justify-between items-center h-12 px-6 rounded text-gray-900 hover:bg-gray-200 transition">
                     <div class="flex items-center">
                         <i class="fal fa-building ml-2"></i>
-                        <span class="font-light">{{ __('Therapy centers') }}</span>
+                        <span class="variable-font-light pt-1">{{ __('Therapy centers') }}</span>
                     </div>
                 </a>
 
@@ -62,7 +62,7 @@
                     <a href="{{ route('dashboard.rooms.index') }}" data-metarget="rooms" data-metarget-pattern="^/dashboard/rooms.*" class="flex justify-between items-center h-12 px-6 rounded text-gray-900 hover:bg-gray-200 transition">
                         <div class="flex items-center">
                             <i class="fal fa-door-open ml-2"></i>
-                            <span class="font-light">{{ __('Rooms') }}</span>
+                            <span class="variable-font-light pt-1">{{ __('Rooms') }}</span>
                         </div>
                     </a>
                 </li>
@@ -70,7 +70,7 @@
                     <a href="{{ route('dashboard.cases.index') }}" data-metarget="cases" data-metarget-pattern="^/dashboard/cases.*" class="flex justify-between items-center h-12 px-6 rounded text-gray-900 hover:bg-gray-200 transition">
                         <div class="flex items-center">
                             <i class="fal fa-folders ml-2"></i>
-                            <span class="font-light">{{ __('Cases') }}</span>
+                            <span class="variable-font-light pt-1">{{ __('Cases') }}</span>
                         </div>
                     </a>
                 </li>
@@ -79,7 +79,7 @@
                 <a href="{{ route('dashboard.sessions.index') }}" data-metarget="session" data-metarget-pattern="^/dashboard/session.*" class="flex justify-between items-center h-12 px-6 rounded text-gray-900 hover:bg-gray-200 transition">
                     <div class="flex items-center">
                         <i class="fal fa-user-friends ml-2"></i>
-                        <span class="font-light">{{ __('Sessions') }}</span>
+                        <span class="variable-font-light pt-1">{{ __('Sessions') }}</span>
                     </div>
                 </a>
             </li>
@@ -88,7 +88,7 @@
                 <a href="{{ route('dashboard.users.index') }}" data-metarget="users" data-metarget-pattern="^/dashboard/users.*" class="flex justify-between items-center h-12 px-6 rounded text-gray-900 hover:bg-gray-200 transition">
                     <div class="flex items-center">
                         <i class="fal fa-users ml-2"></i>
-                        <span class="font-light">{{ __('Users') }}</span>
+                        <span class="variable-font-light pt-1">{{ __('Users') }}</span>
                     </div>
                 </a>
             </li>
@@ -97,7 +97,7 @@
                 <a href="{{ route('dashboard.assessments.index') }}" data-metarget="assessments" data-metarget-pattern="^/dashboard/assessments.*" class="flex justify-between items-center h-12 px-6 rounded text-gray-900 hover:bg-gray-200 transition">
                     <div class="flex items-center">
                         <i class="fal fa-balance-scale ml-2"></i>
-                        <span class="font-light">{{ __('Assessments') }}</span>
+                        <span class="variable-font-light pt-1">{{ __('Assessments') }}</span>
                     </div>
                 </a>
             </li>
@@ -105,7 +105,7 @@
                 <a href="{{ route('dashboard.samples.index') }}" data-metarget="samples" data-metarget-pattern="^/dashboard/samples.*" class="flex justify-between items-center h-12 px-6 rounded text-gray-900 hover:bg-gray-200 transition">
                     <div class="flex items-center">
                         <i class="fal fa-vial ml-2"></i>
-                        <span class="font-light">{{ __('Samples') }}</span>
+                        <span class="variable-font-light pt-1">{{ __('Samples') }}</span>
                     </div>
                 </a>
                 @if (auth()->isAdmin() || (auth()->centers() && auth()->centers()->whereIn('acceptation.position', ['manager', 'operator', 'psychologist'])->first()))
@@ -121,7 +121,7 @@
                     <a href="{{ route('dashboard.documents.index') }}" data-metarget="samples" data-metarget-pattern="^/dashboard/documents.*" class="flex justify-between items-center h-12 px-6 rounded text-gray-900 hover:bg-gray-200 transition">
                         <div class="flex items-center">
                             <i class="fal fa-file-certificate ml-2"></i>
-                            <span class="font-light">{{ __('Documents') }}</span>
+                            <span class="variable-font-light pt-1">{{ __('Documents') }}</span>
                         </div>
                     </a>
                 </li>
