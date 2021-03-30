fff539bfefc156b3d681bf03566fd4cfad5b4299
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-03-30 21:10:53 +0430

    Minor change

--- resources/views/davat/layouts/headerUser.blade.php
+++ resources/views/davat/layouts/headerUser.blade.php
@@ -15,7 +15,7 @@
         </a>
     @endif
 
-    <a href="{{ route('logout') }}" data-lijax='click' data-method='post' class="flex justify-center items-center w-12 h-12 border border-gray-300 text-gray-400 rounded hover:bg-gray-50 hover:text-red-500 mr-2 group focus direct" title="{{ __('Logout') }}">
+    <a href="{{ route('logout') }}" data-lijax='click' data-method='post' class="flex justify-center items-center w-12 h-12 border border-gray-300 text-gray-400 rounded hover:bg-gray-50 hover:text-red-500 mr-2 group focus-current ring-red-500 direct" title="{{ __('Logout') }}">
         <i class="fal fa-sign-out text-xl text-gray-500 group-hover:text-red-500 transform rotate-180"></i>
     </a>
 
