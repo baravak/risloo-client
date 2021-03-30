c6bf1dcce06186c8879098c9d98c5a54c1690552
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-03-30 21:23:10 +0430

    Review user create

--- resources/views/dashboard/users/_createTypes.blade.php
+++ resources/views/dashboard/users/_createTypes.blade.php
@@ -1,7 +1,7 @@
 @foreach (config('users.types', []) as $type => $options)
     <div class="mt-1">
         <label class="inline-flex items-center group">
-            <input type="radio" name="type" id="type-{{$type}}" value="{{$type}}" @radioChecked($user->type, $type) class="w-3.5 h-3.5 border border-gray-600 focus:ring-2 focus:ring-offset-2">
+            <input type="radio" name="type" id="type-{{$type}}" value="{{$type}}" @radioChecked($user->type, $type) class="w-3.5 h-3.5 border border-gray-600 focus">
             <span class="text-sm text-gray-600 mr-2 group-hover:text-blue-600">{{ __(ucfirst($type)) }}</span>
         </label>
     </div>
