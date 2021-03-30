0e6bd8146fa1629607ca42802dc8ee4b0865e8c0
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-03-30 16:19:07 +0430

    Fix quick search

--- resources/views/davat/layouts/quick_search.blade.php
+++ resources/views/davat/layouts/quick_search.blade.php
@@ -1,4 +1,4 @@
-<div class="relative flex w-full">
+<div class="relative flex flex-1">
     <input data-basePage="{{isset($global->page) ? $global->page : ''}}" data-xhrBase="quick_search" data-lijax="800" data-name="q" data-state="both" id="quick_search" value="{{request()->q}}" type="search" class="flex-1 text-sm border border-gray-200 rounded h-9 pr-2 pl-8 focus" data-remove-query="page" placeholder="{{ __('Search') }}">
     <span class="spinner"></span>
 </div>
