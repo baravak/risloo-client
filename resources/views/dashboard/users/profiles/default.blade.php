873189fde41984cfdf9a946b741f2c1be1330b3a
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-04-01 13:01:47 +0430

    Review user profile

--- resources/views/dashboard/users/profiles/default.blade.php
+++ resources/views/dashboard/users/profiles/default.blade.php
@@ -1,53 +1,45 @@
 @extends($layouts->dashboard)
-
 @section('content')
     <div class="border border-gray-200 rounded-sm overflow-hidden">
         <div class="h-20 sm:h-28 bg-gradient-to-b from-blue-100 to-blue-50 border-b border-gray-200"></div>
         <div class="relative p-4">
-                <div class="absolute top-3 left-3 flex">
-
-                    @can('update', [$user])
-                        <a href="{{ $user->route('edit') }}" class="flex justify-center items-center flex-shrink-0 border border-gray-500 text-gray-600 hover:bg-gray-50 px-4 h-9 rounded-full text-sm leading-normal transition-all">
-                            <span class="font-medium">{{ __('Edit') }}</span>
+            <div class="absolute top-3 left-3 flex">
+                @can('update', [$user])
+                    <a href="{{ $user->route('edit') }}" class="flex justify-center items-center flex-shrink-0 border border-gray-500 text-gray-500 hover:bg-gray-50 w-9 sm:w-auto px-4 h-9 rounded-full text-sm transition" title="{{ __('Edit') }}" aria-label="{{ __('Edit') }}">
+                        <i class="fal fa-edit"></i>
+                        <span class="mr-2 hidden sm:inline">{{ __('Edit') }}</span>
+                    </a>
+                @endcan
+                @if ($user->type != 'admin')
+                    @can('admin', App\User::class)
+                        <a href="{{ route('auth.as', ['user' => $user->id]) }}"  data-lijax="click" data-method="POST" class="flex justify-center items-center flex-shrink-0 border border-brand text-brand hover:bg-blue-50 w-9 sm:w-auto px-4 h-9 rounded-full text-sm transition mr-2" title="{{ __('Login to this...') }}" aria-label="{{ __('Login to this...') }}">
+                            <i class="fal fa-user-cog"></i>
+                            <span class="mr-2 hidden sm:inline">{{ __('Login to this...') }}</span>
                         </a>
                     @endcan
-                    @if ($user->type != 'admin')
-                        @can('admin', App\User::class)
-                            <a href="{{ route('auth.as', ['user' => $user->id]) }}"  data-lijax="click" data-method="POST" class="flex justify-center items-center flex-shrink-0 border border-blue-600 text-blue-600 hover:bg-blue-50 w-9 sm:w-auto px-4 h-9 rounded-full text-sm leading-normal transition-all mr-2">
-                                <i class="fal fa-user-cog"></i>
-                                <span class="mr-2 hidden sm:inline">{{ __('Login to this...') }}</span>
-                            </a>
-                        @endcan
-                    @endif
-                </div>
-            <div class="flex justify-center items-center flex-shrink-0 w-24 h-24 md:w-32 md:h-32 -mt-16 md:-mt-20 bg-gray-300 text-gray-600 text-2xl rounded-full border-4 border-white overflow-hidden mb-4 relative">@avatarOrName($user)</div>
+                @endif
+            </div>
 
-            <h2 class="font-bold text-lg text-gray-900 cursor-default">{{ $user->name ?: __('Anonymouse') }}</h2>
+            <div class="flex justify-center items-center flex-shrink-0 w-24 h-24 md:w-32 md:h-32 -mt-16 md:-mt-20 bg-gray-300 text-gray-600 text-2xl rounded-full border-4 border-white overflow-hidden mb-4 cursor-default relative">@avatarOrName($user)</div>
 
-            <div class="text-sm text-gray-600 mt-1 cursor-default en">{{ $user->username }}</div>
+            <h2 class="variable-font-bold text-lg text-gray-900 cursor-default">{{ $user->name ?: __('Anonymouse') }}</h2>
+
+            <div class="text-sm text-gray-600 mt-1 cursor-default dir-ltr text-right en">{{ $user->username }}</div>
 
             <div class="flex flex-wrap items-center mt-2">
-                {{-- <div class="inline-flex items-center text-gray-500 mb-2 sm:mb-0 ml-6 cursor-default">
-                    <i class="fal fa-graduation-cap mb-2 ml-2"></i>
-                    <span class="text-sm">دکترای روانشناسی بالینی</span>
-                </div> --}}
-                {{-- <div class="inline-flex items-center text-gray-500 mb-2 sm:mb-0 ml-6 cursor-default">
-                    <i class="fal fa-calendar-alt mb-2 ml-2"></i>
-                    <span class="text-sm">۲۰ آذر ۱۳۶۰</span>
-                </div> --}}
-                @if ($user->email)
-                    <div class="inline-flex items-center text-gray-500 mb-2 sm:mb-0 ml-6">
-                        <i class="fal fa-envelope mb-2 ml-2"></i>
-                        <a href="mailto:{{ $user->email }}" class="block dir-ltr text-right text-sm hover:text-blue-500 direct">{{ $user->email }}</a>
-                    </div>
-                @endif
+                @isset ($user->email)
+                    <a href="mailto:{{ $user->email }}" class="inline-flex items-center text-gray-500 hover:text-blue-600 mb-2 sm:mb-0 ml-6 transition direct" title="{{ $user->email }}" aria-label="{{ $user->email }}">
+                        <i class="fal fa-envelope-open ml-2"></i>
+                        <span class="block dir-ltr text-right text-sm pt-1">{{ $user->email }}</span>
+                    </a>
+                @endisset
 
-                @if ($user->mobile)
-                    <div class="inline-flex items-center text-gray-500 mb-2 sm:mb-0 ml-6">
-                        <i class="fal fa-phone mb-2 ml-2"></i>
-                        <a href="tel:+{{ $user->mobile }}" class="block dir-ltr text-right text-sm hover:text-blue-500 direct">+{{ $user->mobile }}</a>
-                    </div>
-                @endif
+                @isset ($user->mobile)
+                    <a href="tel:+{{ $user->mobile }}" class="inline-flex items-center text-gray-500 hover:text-blue-600 mb-2 sm:mb-0 ml-6 transition direct" title="{{ $user->mobile }}" aria-label="{{ $user->mobile }}">
+                        <i class="fal fa-phone ml-2"></i>
+                        <span class="block dir-ltr text-right text-sm pt-1">+{{ $user->mobile }}</span>
+                    </a>
+                @endisset
             </div>
 
             {{-- <div class="flex items-center mt-2">
--- resources/views/davat/layouts/headerUser.blade.php
+++ resources/views/davat/layouts/headerUser.blade.php
@@ -1,6 +1,6 @@
 <div class="flex" data-xhr="user-header-nav">
 
-    <a href="{{ route('dashboard.users.me') }}" class="flex items-center border border-gray-300 rounded px-3 h-12 hover:bg-gray-50 transition dropdown-toggle focus">
+    <a href="{{ route('dashboard.users.me') }}" class="flex items-center border border-gray-300 rounded px-3 h-12 hover:bg-gray-50 transition dropdown-toggle mr-2 sm:mr-0 focus">
         <div class="flex justify-center items-center flex-shrink-0 w-8 h-8 rounded overflow-hidden ml-3 bg-blue-500 text-white border border-gray-200 text-xs" data-xhr="short-avatar">
             @avatarOrName(auth()->user())
         </div>
@@ -15,7 +15,7 @@
         </a>
     @endif
 
-    <a href="{{ route('logout') }}" data-lijax='click' data-method='post' class="flex justify-center items-center w-12 h-12 border border-gray-300 text-gray-400 rounded hover:bg-gray-50 hover:text-red-500 mr-2 group focus-current ring-red-500 direct" title="{{ __('Logout') }}">
+    <a href="{{ route('logout') }}" data-lijax='click' data-method='post' class="flex justify-center items-center flex-shrink-0 w-12 h-12 border border-gray-300 text-gray-400 rounded hover:bg-gray-50 hover:text-red-500 mr-2 group focus-current ring-red-500 direct" title="{{ __('Logout') }}">
         <i class="fal fa-sign-out text-xl text-gray-500 group-hover:text-red-500 transform rotate-180"></i>
     </a>
 
