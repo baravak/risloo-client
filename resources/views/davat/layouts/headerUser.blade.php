c04fe8aec34026eddc19272150f1b2d0b4939e96
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-04-07 19:40:32 +0430

    Update headerUser.blade.php
    Co-Authored-By: Ahmad Karimi <ahmadkarimi1991@gmail.com>

--- resources/views/davat/layouts/headerUser.blade.php
+++ resources/views/davat/layouts/headerUser.blade.php
@@ -12,9 +12,11 @@
         </a>
     @endif
 
-    <a href="{{ route('logout') }}" data-lijax='click' data-method='post' class="flex justify-center items-center flex-shrink-0 w-12 h-12 border border-gray-300 text-gray-400 rounded hover:bg-gray-50 hover:text-red-500 mr-2 group focus-current ring-red-500 direct" title="{{ __('Logout') }}">
-        <i class="fal fa-sign-out text-xl text-gray-500 group-hover:text-red-500 transform rotate-180"></i>
-    </a>
+    @if (!auth()->user()->response('current'))
+        <a href="{{ route('logout') }}" data-lijax='click' data-method='post' class="flex justify-center items-center flex-shrink-0 w-12 h-12 border border-gray-300 text-gray-400 rounded hover:bg-gray-50 hover:text-red-500 mr-2 group focus-current ring-red-500 direct" title="{{ __('Logout') }}">
+            <i class="fal fa-sign-out text-xl text-gray-500 group-hover:text-red-500 transform rotate-180"></i>
+        </a>
+    @endif
 
     @if (false)
         <div class="relative dropdown">
