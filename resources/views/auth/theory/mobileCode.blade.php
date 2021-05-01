19efedcbc8d78917d88469a9b35ad21372552e13
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-05-01 19:34:39 +0430

    Update verify mobile and password

--- resources/views/auth/theory/mobileCode.blade.php
+++ resources/views/auth/theory/mobileCode.blade.php
@@ -4,14 +4,14 @@
         <input type="authorized_key"  disabled value="{{ $theory->response('authorized_key') }}" class="w-full text-sm text-left dir-ltr text-gray-400 border border-gray-300 rounded h-10 px-3" id="authorized_key" name="authorized_key">
     </div>
     <div class="mb-4">
-        <input type="password" class="w-full h-10 text-sm text-left dir-ltr placeholder-right placeholder-gray-400 border border-gray-300 rounded" id="code" name="code" placeholder="{{ __('Sms code')}}" autocomplete="off" autofocus>
+        <input type="password" class="w-full h-10 text-sm text-left dir-ltr placeholder-right placeholder-gray-400 border border-gray-300 rounded" id="code" name="code" placeholder="{{ __('SMS code')}}" autocomplete="off" autofocus>
         <div class="flex text-xs text-gray-400 mt-2 cursor-default leading-relaxed">
             <i class="fal fa-info-circle ml-1 mt-0.5"></i>
             <span>@lang('Mobile verify help :mobile', ['mobile' => $theory->response('authorized_key')])</span>
         </div>
     </div>
 
-    <button class="flex items-center justify-center w-full h-10 text-sm rounded-full bg-brand text-white hover:bg-brand-600 transition mb-8 focus" title="{{ __('Register') }}" aria-label="{{ __('Register') }}" role="button">{{ __('Register') }}</button>
+    <button class="flex items-center justify-center w-full h-10 text-sm rounded-full bg-brand text-white hover:bg-brand-600 transition mb-8 focus" title="{{ __('Verify Mobile') }}" aria-label="{{ __('Verify Mobile') }}" role="button">{{ __('Verify Mobile') }}</button>
 @endsection
 
 @extends('auth.theory')
--- resources/views/auth/theory/password.blade.php
+++ resources/views/auth/theory/password.blade.php
@@ -1,9 +1,14 @@
 @section('auth-form')
+    <h2 class="text-center variable-font-bold text-gray-800 mb-4 cursor-default">کلمه عبور</h2>
     <div class="mb-2">
         <input type="authorized_key" disabled value="{{ $theory->response('authorized_key') }}" class="w-full text-sm text-left dir-ltr text-gray-400 border border-gray-200 rounded h-10 px-3" id="authorized_key" name="authorized_key">
     </div>
     <div class="mb-4">
-        <input type="password" class="w-full h-10 text-sm text-left dir-ltr placeholder-right border border-gray-200 rounded" id="password" name="password" placeholder="{{__('Password')}}">
+        <input type="password" class="w-full h-10 text-sm text-left dir-ltr placeholder-right placeholder-gray-400 border border-gray-300 rounded" id="password" name="password" placeholder="{{__('Password')}}">
+        <div class="flex text-xs text-gray-400 mt-2 cursor-default leading-relaxed">
+            <i class="fal fa-info-circle ml-1 mt-0.5"></i>
+            <span>کلمه عبور خود را وارد کرده و روی دکمه ورود کلیک کنید.</span>
+        </div>
     </div>
 
     <button class="flex items-center justify-center w-full h-10 text-sm rounded-full bg-brand text-white hover:bg-brand-600 transition mb-8 focus" title="{{ __('Login') }}" aria-label="{{ __('Login') }}" role="button">{{ __('Login') }}</button>
