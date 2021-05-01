cf4919120fbfd32ae0b3b99c59cade705263d614
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-05-01 22:33:14 +0430

    Add description to aside menu

--- resources/views/davat/layouts/aside.blade.php
+++ resources/views/davat/layouts/aside.blade.php
@@ -8,18 +8,30 @@
 
         <ul class="px-2">
             <li class="mb-1">
-                <a href="{{ route('dashboard.home') }}" data-metarget-default class="flex justify-between items-center h-12 px-6 rounded text-gray-900 hover:bg-gray-200 transition">
+                {{-- <a href="{{ route('dashboard.home') }}" data-metarget-default class="flex justify-between items-center h-12 px-6 rounded text-gray-900 hover:bg-gray-200 transition">
                     <div class="flex items-center">
                         <i class="fal fa-tachometer-alt-fastest ml-2"></i>
                         <span class="variable-font-light pt-1">{{ __('Dashboard') }}</span>
+                    </div>
+                </a> --}}
+                <a href="{{ route('dashboard.home') }}" data-metarget-default class="flex justify-between items-center h-12 px-6 rounded text-gray-900 hover:bg-gray-200 transition">
+                    <div class="flex items-center">
+                        <i class="fal fa-tachometer-alt-fastest ml-3"></i>
+                        <div>
+                            <div class="variable-font-medium">{{ __('Dashboard') }}</div>
+                            <div class="text-xs variable-font-light">خلاصه‌ای از سرویس</div>
+                        </div>
                     </div>
                 </a>
             </li>
             <li class="mb-1">
                 <a href="{{ route('dashboard.centers.index') }}" data-metarget="centers-index" class="flex justify-between items-center h-12 px-6 rounded text-gray-900 hover:bg-gray-200 transition">
                     <div class="flex items-center">
-                        <i class="fal fa-building ml-2"></i>
-                        <span class="variable-font-light pt-1">{{ __('Therapy centers') }}</span>
+                        <i class="fal fa-building ml-3"></i>
+                        <div>
+                            <div class="variable-font-medium">{{ __('Therapy centers') }}</div>
+                            <div class="text-xs variable-font-light">لیست مراکز درمانی</div>
+                        </div>
                     </div>
                 </a>
 
@@ -51,7 +63,10 @@
                     @else
                     <ul class="pr-8 mt-2">
                         <li>
-                            <a href="{{ route('dashboard.centers.index', ['my' => 'yes']) }}" data-metarget="centers-myclinics" class="flex items-center text-sm text-gray-600 h-12 pr-4 border-r border-gray-300 hover:text-gray-800 transition">{{  __('My therapy centers') }}</a>
+                            <a href="{{ route('dashboard.centers.index', ['my' => 'yes']) }}" data-metarget="centers-myclinics" class="flex flex-col justify-center text-sm text-gray-600 h-12 pr-4 border-r border-gray-300 hover:text-gray-800 transition">
+                                <div class="variable-font-medium">{{  __('My therapy centers') }}</div>
+                                <div class="text-xs variable-font-light">لیست مراکز مرتبط با من</div>
+                            </a>
                         </li>
                     </ul>
                     @endif
@@ -61,16 +76,22 @@
                 <li class="mb-1">
                     <a href="{{ route('dashboard.rooms.index') }}" data-metarget="rooms" data-metarget-pattern="^/dashboard/rooms.*" class="flex justify-between items-center h-12 px-6 rounded text-gray-900 hover:bg-gray-200 transition">
                         <div class="flex items-center">
-                            <i class="fal fa-door-open ml-2"></i>
-                            <span class="variable-font-light pt-1">{{ __('Rooms') }}</span>
+                            <i class="fal fa-door-open ml-3"></i>
+                            <div>
+                                <div class="variable-font-medium">{{ __('Rooms') }}</div>
+                                <div class="text-xs variable-font-light">اتاق‌های درمانی من</div>
+                            </div>
                         </div>
                     </a>
                 </li>
                 <li class="mb-1">
                     <a href="{{ route('dashboard.cases.index') }}" data-metarget="cases" data-metarget-pattern="^/dashboard/cases.*" class="flex justify-between items-center h-12 px-6 rounded text-gray-900 hover:bg-gray-200 transition">
                         <div class="flex items-center">
-                            <i class="fal fa-folders ml-2"></i>
-                            <span class="variable-font-light pt-1">{{ __('Cases') }}</span>
+                            <i class="fal fa-folders ml-3"></i>
+                            <div>
+                                <div class="variable-font-medium">{{ __('Cases') }}</div>
+                                <div class="text-xs variable-font-light">پرونده‌های درمانی من</div>
+                            </div>
                         </div>
                     </a>
                 </li>
@@ -79,7 +100,10 @@
                 <a href="{{ route('dashboard.sessions.index') }}" data-metarget="session" data-metarget-pattern="^/dashboard/session.*" class="flex justify-between items-center h-12 px-6 rounded text-gray-900 hover:bg-gray-200 transition">
                     <div class="flex items-center">
                         <i class="fal fa-user-friends ml-2"></i>
-                        <span class="variable-font-light pt-1">{{ __('Sessions') }}</span>
+                        <div>
+                            <div class="variable-font-medium">{{ __('Sessions') }}</div>
+                            <div class="text-xs variable-font-light">جلسات درمانی من</div>
+                        </div>
                     </div>
                 </a>
             </li>
@@ -96,8 +120,11 @@
             <li class="mb-1">
                 <a href="{{ route('dashboard.assessments.index') }}" data-metarget="assessments" data-metarget-pattern="^/dashboard/assessments.*" class="flex justify-between items-center h-12 px-6 rounded text-gray-900 hover:bg-gray-200 transition">
                     <div class="flex items-center">
-                        <i class="fal fa-balance-scale ml-2"></i>
-                        <span class="variable-font-light pt-1">{{ __('Assessments') }}</span>
+                        <i class="fal fa-balance-scale ml-3"></i>
+                        <div>
+                            <div class="variable-font-medium">{{ __('Assessments') }}</div>
+                            <div class="text-xs variable-font-light">لیست نمونه‌های موجود در سامانه</div>
+                        </div>
                     </div>
                 </a>
             </li>
@@ -105,13 +132,19 @@
                 <a href="{{ route('dashboard.samples.index') }}" data-metarget="samples" data-metarget-pattern="^/dashboard/samples.*" class="flex justify-between items-center h-12 px-6 rounded text-gray-900 hover:bg-gray-200 transition">
                     <div class="flex items-center">
                         <i class="fal fa-vial ml-2"></i>
-                        <span class="variable-font-light pt-1">{{ __('Samples') }}</span>
+                        <div>
+                            <div class="variable-font-medium">{{ __('Samples') }}</div>
+                            <div class="text-xs variable-font-light">نمونه‌های گرفته‌شده</div>
+                        </div>
                     </div>
                 </a>
                 @if (auth()->isAdmin() || (auth()->centers() && auth()->centers()->whereIn('acceptation.position', ['manager', 'operator', 'psychologist'])->first()))
                     <ul class="pr-8 mt-2">
                         <li>
-                            <a href="{{ route('dashboard.bulk-samples.index') }}" data-metarget="bulk-samples" data-metarget-pattern="^/dashboard/bulk-samples.*" class="flex items-center text-sm text-gray-600 h-12 pr-4 border-r border-gray-300 hover:text-gray-800 transition">@lang('Bulk samples')</a>
+                            <a href="{{ route('dashboard.bulk-samples.index') }}" data-metarget="bulk-samples" data-metarget-pattern="^/dashboard/bulk-samples.*" class="flex flex-col justify-center text-sm text-gray-600 h-12 pr-4 border-r border-gray-300 hover:text-gray-800 transition">
+                                <div class="variable-font-medium">@lang('Bulk samples')</div>
+                                <div class="text-xs variable-font-light">نمونه‌های زنجیره‌ای</div>
+                            </a>
                         </li>
                     </ul>
                 @endif
