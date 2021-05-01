0b31658281923e0b79761dc3c511a1044d611d4d
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-05-01 12:56:47 +0430

    Update billings item edit

--- resources/views/dashboard/billings/items/index.blade.php
+++ resources/views/dashboard/billings/items/index.blade.php
@@ -12,38 +12,5 @@
             @include('dashboard.billings.items.list')
         </div>
 
-        {{-- <div class="m-auto w-full md:w-1/2 mt-8">
-            <form method="POST" action="{{ route('dashboard.payments.sotre') }}">
-                <div class="border border-gray-300 rounded p-4">
-                    <div>
-                        <label for="title" class="block mb-2 text-sm text-gray-700 variable-font-medium cursor-default">{{ __('Title') }}</label>
-                        <input type="text" name="title" id="title" autocomplete="off" class="border border-gray-500 h-10 rounded px-4 w-full text-sm focus:border-brand focus">
-                    </div>
-                    <div class="mt-4">
-                        <label for="Debtor" class="block mb-2 text-sm text-gray-700 variable-font-medium">@lang('Debtor')</label>
-                        <select id="Debtor" name="Debtor" class="border border-gray-500 h-10 rounded pl-4 pr-8 w-full text-sm focus:border-brand focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-opacity-60">
-                            <option value="">مرکز مشاوره طلیعه سلامت</option>
-                            <option value="">مرکز مشاوره ریسلو</option>
-                        </select>
-                    </div>
-                    <div class="mt-4">
-                        <label for="Creditor" class="block mb-2 text-sm text-gray-700 variable-font-medium">@lang('Creditor')</label>
-                        <select id="Creditor" name="Creditor" class="border border-gray-500 h-10 rounded pl-4 pr-8 w-full text-sm focus:border-brand focus:outline-none focus:ring-2 focus:ring-offset-1 focus:ring-opacity-60">
-                            <option value="">محمدعلی نخلی</option>
-                            <option value="">محمدحسن صالحی</option>
-                        </select>
-                    </div>
-                    <div class="mt-4">
-                        <label for="amount" class="block mb-2 text-sm text-gray-700 variable-font-medium cursor-default">{{ __('Amount') }} <span class="text-xs text-gray-500 variable-font-normal">({{ __('Toman') }})</span></label>
-                        <input type="number" name="amount" id="amount" autocomplete="off" class="border border-gray-500 h-10 rounded px-4 w-full text-sm text-left dir-ltr focus:border-brand focus">
-                    </div>
-                </div>
-                <div class="mt-4">
-                    <button type="submit" class="inline-flex justify-center items-center h-9 px-8 bg-brand text-white text-sm rounded-full hover:bg-brand-600 transition ml-4 focus" role="button">
-                        {{ __('Edit') }}
-                    </button>
-                </div>
-            </form>
-        </div> --}}
     </div>
 @endsection
--- resources/views/dashboard/billings/items/listRaw.blade.php
+++ resources/views/dashboard/billings/items/listRaw.blade.php
@@ -45,7 +45,53 @@
     </td>
     <td class="px-3 py-2 whitespace-nowrap text-left dir-ltr">
         <div class="inline-block">
-            <a href="#" title="{{ __('Edit') }}" aria-label="{{ __('Edit') }}"><i class="fal fa-edit text-sm leading-relaxed text-gray-600 hover:text-blue-600"></i></a>
+            <button class="w-6 h-w-6 rounded-full focus" title="{{ __('Edit') }}" aria-label="{{ __('Edit') }}"><i class="relative top-0.5 fal fa-edit text-sm text-gray-600 hover:text-blue-600"></i></button>
+        </div>
+    </td>
+</tr>
+
+<tr data-xhr="transaction-list-id" class="transition hover:bg-gray-50">
+    <td class="px-3 py-2 whitespace-nowrap">
+        <div class="flex items-center">
+            <span class="text-xs text-gray-600 block cursor-default">{{ $loop->index + 1 }}</span>
+        </div>
+    </td>
+    <td class="px-3 py-2 whitespace-nowrap">
+        <div class="flex items-center">
+            <input placeholder="{{ $item->title }}" type="text" name="title" id="title" autocomplete="off" class="border border-gray-500 text-xs text-gray-600 h-8 w-full rounded px-2 focus:border-brand focus">
+        </div>
+    </td>
+    <td class="px-3 py-2 whitespace-nowrap">
+        <div class="flex items-center">
+            <span class="text-xs text-gray-600 text-right dir-ltr en cursor-default">{{ $item->action->id }}</span>
+        </div>
+    </td>
+    <td class="p-3 whitespace-nowrap">
+        <div>
+            <select id="Debtor" name="Debtor" class="border border-gray-500 h-8 rounded pl-4 pr-8 w-full text-xs focus:border-brand focus">
+                <option value="">مرکز مشاوره طلیعه سلامت</option>
+                <option value="">مرکز مشاوره ریسلو</option>
+            </select>
+        </div>
+    </td>
+    <td class="px-3 py-2 whitespace-nowrap">
+        <div>
+            <select id="Debtor" name="Debtor" class="border border-gray-500 h-8 rounded pl-4 pr-8 w-full text-xs focus:border-brand focus">
+                <option value="">صندوق نقدی مرکز طلیعه سلامت</option>
+            </select>
+        </div>
+    </td>
+    <td class="p-3 whitespace-nowrap">
+        <div>
+            <input type="number" placeholder="@amount($item->amount)" name="amount" id="amount" autocomplete="off" class="border border-gray-500 h-8 rounded px-2 w-full text-xs focus:border-brand focus">
+        </div>
+    </td>
+    <td class="px-3 py-2 whitespace-nowrap text-left dir-ltr">
+        <div class="inline-block mr-2">
+            <button class="w-6 h-w-6 rounded-full focus-current ring-red-600" title="{{ __('Cancel') }}" aria-label="{{ __('Cancel') }}"><i class="relative top-1 fal fa-times text-gray-600 hover:text-red-600"></i></button>
+        </div>
+        <div class="inline-block">
+            <button type="submit" class="w-6 h-w-6 rounded-full focus-current ring-green-600" title="{{ __('Save changes') }}" aria-label="{{ __('Save changes') }}"><i class="relative top-1 fal fa-check text-gray-600 hover:text-green-600"></i></button>
         </div>
     </td>
 </tr>
