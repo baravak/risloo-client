6be79bac593d5037f8914b4c746fefeeb475f2b0
Ahmad Karimi <ahmadkarimi1991@gmail.com>
2021-05-03 11:56:00 +0430

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,8 +22,8 @@
                     </h1>
 
                     <div class="flex items-center">
-                        <a href="/dashboard" class="text-sm variable-font-medium hover:text-brand transition ml-4">ورود</a>
-                        <a href="/register" class="text-sm text-white variable-font-normal bg-brand py-2 px-8 rounded-full hover:bg-brand-600 transition">ثبت‌نام</a>
+                        <a href="/dashboard" class="text-sm variable-font-medium hover:text-brand transition ml-4">@lang('Login')</a>
+                        <a href="/register" class="text-sm text-white variable-font-normal bg-brand py-2 px-8 rounded-full hover:bg-brand-600 transition">@lang('Register')</a>
                     </div>
                 </div>
             </div>
@@ -33,14 +33,14 @@
             <h2 class="text-center text-2xl sm:text-4xl variable-font-bold">درمان، آموزش و پژوهش</h2>
             <p class="text-center sm:w-1/2 md:w-1/3 mt-4 mx-auto">شما به عنوان روان‌شناس، روی کار اصلی خود تمرکز کنید؛ دغدغه کارهای جانبی <span class="variable-font-bold">درمان، آموزش و پژوهش</span> را نداشته باشید و به خود آن‌ها فقط فکر کنید. ریسلو بستری یک‌پارچه است که کارهای شما را در این سه حوزه تسهیل می‌کند.</p>
             <div class="flex flex-col justify-center items-center text-center mt-8">
-                <a href="/register" class="bg-brand text-white py-2 px-8 rounded-full">در ریسلو ثبت‌نام کنید</a>
+                <a href="/register" class="bg-brand text-white py-2 px-8 rounded-full">عضو ریسلو شوید</a>
                 <a href="https://play.google.com/store/apps/details?id=com.majazeh.risloo" target="_blank" rel="noopener" class="variable-font-normal text-gray-700 hover:text-gray-900 transition mt-4">دانلود نسخه اندروید از <span class="variable-font-bold">Google Play</span></a>
             </div>
         </section>
 
         <section class="mt-8 px-4">
             <div class="w-full lg:w-1/2 mx-auto shadow">
-                <img src="{{ asset('/public/images/main.jpg') }}" alt="">
+                <img src="{{ asset('/public/images/main.jpg') }}" alt="ریسلو">
             </div>
         </section>
     </main>
