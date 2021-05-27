61fb207499e4aa21757d6eec237affba31a2f180
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-05-27 19:13:53 +0430

    Minor fix

--- resources/views/dashboard/samples/index.blade.php
+++ resources/views/dashboard/samples/index.blade.php
@@ -7,7 +7,9 @@
         </div>
 
         <div class="flex justify-between items-center flex-wrap mb-4">
-            @include('layouts.quick_search')
+            @if ($samples->count())
+                @include('layouts.quick_search')
+            @endif
             @can('create', App\Sample::class)
                 <a href="{{ route('dashboard.samples.create') }}" class="flex items-center justify-center flex-shrink-0 w-9 sm:w-auto h-9 sm:px-4 text-sm text-white border bg-green-700 hover:bg-green-900 rounded-full transition mr-4 focus-current ring-green-700" title="{{ __('Create sampel') }}">
                     <i class="fal fa-plus sm:ml-2"></i>
