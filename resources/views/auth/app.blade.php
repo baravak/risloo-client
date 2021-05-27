d17c24c4f86e20ab201e708921944db45a675cdd
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-05-27 09:18:51 +0430

    Remove auth images

--- resources/views/auth/app.blade.php
+++ resources/views/auth/app.blade.php
@@ -4,7 +4,7 @@
 @section('main')
     <div class="flex-1 bg-gray-50">
         <div class="flex justify-center">
-            <div class="rounded w-full sm:w-80 mx-4 sm:mx-auto relative top-10">
+            <div class="rounded w-full sm:w-80 mx-4 sm:mx-auto relative top-20">
                 @if (auth()->check() && auth()->user())
                     <div class="mb-4">
                         <a href="{{ route(auth()->check() ? 'dashboard.home' : 'auth') }}" class="flex justify-center items-center mx-auto w-16 h-16 rounded overflow-hidden border border-gray-200 direct flex-shrink-0 bg-gray-300 text-gray-600 text-sm">
@@ -39,7 +39,7 @@
                     @yield('auth-nav')
                 </div>
             </div>
-            <div class="auth-images fixed bottom-0 w-full hidden">
+            {{-- <div class="auth-images fixed bottom-0 w-full hidden">
                 <img src="/images/graphics/1.png" alt="{{ __('App Title') }}" class="w-auto max-h-60 mx-auto md:hidden">
                 <img src="/images/graphics/2.png" alt="{{ __('App Title') }}" class="w-auto max-h-60 mx-auto hidden">
                 <img src="/images/graphics/3.png" alt="{{ __('App Title') }}" class="w-auto max-h-60 mx-auto hidden">
@@ -50,7 +50,7 @@
                 <img src="/images/graphics/linear-2.png" alt="{{ __('App Title') }}" class="w-full mx-auto hidden md:block xl:hidden">
                 <img src="/images/graphics/linear-3.png" alt="{{ __('App Title') }}" class="w-full mx-auto hidden xl:block 2xl:hidden">
                 <img src="/images/graphics/linear-5.png" alt="{{ __('App Title') }}" class="w-full hidden 2xl:block">
-            </div>
+            </div> --}}
         </div>
     </div>
 @endsection
