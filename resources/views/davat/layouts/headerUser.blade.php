4b4fe6769e2fe653d4acd104949390fb139ca7e5
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-05-29 10:10:46 +0430

    Fix dropdown menu padding

--- resources/views/davat/layouts/headerUser.blade.php
+++ resources/views/davat/layouts/headerUser.blade.php
@@ -21,13 +21,13 @@
                 </div>
             </div>
         </button>
-        <div class="rounded bg-white border border-gray-200 mt-1 shadow-md dropdown-menu px-4 min-w-max absolute left-0">
-            <a href="{{ route('dashboard.users.me') }}" class="block text-sm text-gray-700 text-center py-3 hover:bg-gray-100 border-b border-gray-100 transition">مشاهده پروفایل</a>
-            <a href="{{ route('dashboard.treasuries.index') }}" class="block text-sm text-gray-700 text-center py-3 hover:bg-gray-100 border-b border-gray-100 transition">@lang('Financial treasuries')</a>
-            <a href="{{ route('dashboard.billings.index') }}" class="block text-sm text-gray-700 text-center py-3 hover:bg-gray-100 border-b border-gray-100 transition">@lang('Billings')</a>
+        <div class="rounded bg-white border border-gray-200 mt-1 shadow-md dropdown-menu min-w-max absolute left-0">
+            <a href="{{ route('dashboard.users.me') }}" class="block text-sm text-gray-700 text-center py-3 px-4 hover:bg-gray-100 border-b border-gray-100 transition">مشاهده پروفایل</a>
+            <a href="{{ route('dashboard.treasuries.index') }}" class="block text-sm text-gray-700 text-center py-3 px-4 hover:bg-gray-100 border-b border-gray-100 transition">@lang('Financial treasuries')</a>
+            <a href="{{ route('dashboard.billings.index') }}" class="block text-sm text-gray-700 text-center py-3 px-4 hover:bg-gray-100 border-b border-gray-100 transition">@lang('Billings')</a>
 
-            <a href="{{ route('dashboard.payments.index') }}" class="block text-sm text-gray-700 text-center py-3 hover:bg-gray-100 border-b border-gray-100 transition">@lang('Credit charge')</a>
-            <a href="{{ route('logout') }}" data-lijax='click' data-method='post' title="{{ __('Logout') }}" class="block text-sm text-red-600 text-center py-3 hover:bg-gray-100 transition">@lang('Logout')</a>
+            <a href="{{ route('dashboard.payments.index') }}" class="block text-sm text-gray-700 text-center py-3 px-4 hover:bg-gray-100 border-b border-gray-100 transition">@lang('Credit charge')</a>
+            <a href="{{ route('logout') }}" data-lijax='click' data-method='post' title="{{ __('Logout') }}" class="block text-sm text-red-600 text-center py-3 px-4 hover:bg-gray-100 transition">@lang('Logout')</a>
         </div>
     </div>
     {{-- <a href="{{ route('dashboard.users.me') }}" class="flex items-center border border-gray-300 rounded px-3 h-12 hover:bg-gray-50 transition dropdown-toggle mr-2 sm:mr-0 focus">
