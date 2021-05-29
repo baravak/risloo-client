41f1b7dd81ddf541c5a2ea49a5ad44eeb0753e3c
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-05-29 14:00:59 +0430

    Update join user confirm translate

--- resources/views/auth/theory/joinUser.blade.php
+++ resources/views/auth/theory/joinUser.blade.php
@@ -1,5 +1,5 @@
 @section('auth-form')
-    <button class="flex items-center justify-center w-full h-10 text-sm rounded-full bg-brand text-white hover:bg-brand-600 transition mb-8 focus" title="{{ __('Join user') }}" aria-label="{{ __('Join user') }}" role="button">{{ __('Join user') }}</button>
+    <button class="flex items-center justify-center w-full h-10 text-sm rounded-full bg-brand text-white hover:bg-brand-600 transition mb-8 focus" title="{{ __('Confirm registration') }}" aria-label="{{ __('Confirm registration') }}" role="button">{{ __('Confirm registration') }}</button>
 @endsection
 
 @extends('auth.theory')
