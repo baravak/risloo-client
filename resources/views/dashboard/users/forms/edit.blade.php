b4aaeae2492dfabeab9ebd8df39681ebd27f9d0b
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-05-29 12:38:06 +0430

    Update edit profile tabs icon font size

--- resources/views/dashboard/users/forms/edit.blade.php
+++ resources/views/dashboard/users/forms/edit.blade.php
@@ -9,32 +9,32 @@
             <li>
                 <a href="#basic-tab" class="direct focus" title="{{ __('Basic info') }}" aria-label="{{ __('Basic info') }}">
                     <span class="hidden md:block">{{ __('Basic info') }}</span>
-                    <i class="fal fa-info text-2xl block md:hidden"></i>
+                    <i class="fal fa-info text-xl block md:hidden"></i>
                 </a>
             </li>
             @endif
             <li>
                 <a data-tabby-default href="#personal-tab" class="direct focus" title="{{ __('Personal info') }}" aria-label="{{ __('Personal info') }}">
                     <span class="hidden md:block">{{ __('Personal info') }}</span>
-                    <i class="fal fa-address-card text-2xl block md:hidden"></i>
+                    <i class="fal fa-address-card text-xl block md:hidden"></i>
                 </a>
             </li>
             <li>
                 <a href="#password-tab" class="direct focus" title="{{ __('Password') }}" aria-label="{{ __('Password') }}">
                     <span class="hidden md:block">{{ __('Password') }}</span>
-                    <i class="fal fa-unlock-alt text-2xl block md:hidden"></i>
+                    <i class="fal fa-unlock-alt text-xl block md:hidden"></i>
                 </a>
             </li>
             <li>
                 <a href="#avatar-tab" class="direct focus" title="{{ __('Avatar') }}" aria-label="{{ __('Avatar') }}">
                     <span class="hidden md:block">{{ __('Avatar') }}</span>
-                    <i class="fal fa-user-circle text-2xl block md:hidden"></i>
+                    <i class="fal fa-user-circle text-xl block md:hidden"></i>
                 </a>
             </li>
             <li>
                 <a href="#key-tab" class="direct focus" title="{{ __('Public key') }}" aria-label="{{ __('Public key') }}">
                     <span class="hidden md:block">{{ __('Public key') }}</span>
-                    <i class="fal fa-key text-2xl block md:hidden"></i>
+                    <i class="fal fa-key text-xl block md:hidden"></i>
                 </a>
             </li>
         </ul>
