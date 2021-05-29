864a071c0a75bc15d25801186649a12bdee7cb38
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-05-29 12:10:57 +0430

    Add sample description for page's title

--- resources/views/dashboard/assessments/index.blade.php
+++ resources/views/dashboard/assessments/index.blade.php
@@ -3,6 +3,7 @@
     <div>
         <div class="mb-4 mt-8">
             <h2 class="heading" data-total="({{ $assessments->total() }})" data-xhr="total">{{ __('Assessments') }}</h2>
+            {{-- <p class="text-xs text-gray-500 mt-2 pr-10">آزمون‌های روان‌شناختی موجود در سامانه را در این صفحه مشاهده می‌نمایید. این یک متن تستی می‌باشد جهت ارائه توضیحات یک صفحه که در زیر عنوان هر صفحه درج می‌شود.</p> --}}
         </div>
 
         <div class="mb-4">
