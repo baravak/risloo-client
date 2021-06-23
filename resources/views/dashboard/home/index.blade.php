1e30744ee23491307b7f330995155905f079c925
abouhatef <itb.baravak@gmial.com>
2021-06-24 02:08:43 +0430

    bugfix

--- resources/views/dashboard/home/index.blade.php
+++ resources/views/dashboard/home/index.blade.php
@@ -2,9 +2,11 @@
 @section('content')
 
 <h2 class="text-gray-700 variable-font-bold mb-4">@lang('Welcome to risloo')</h2>
-@if (config('app.env') == 'local')
+@if (config('app.env') == 'localx')
     @include('dashboard.home.alerts')
     @include('dashboard.home.summary')
+@endif
+@if ($user->dalily_schedule_exports)
     @include('dashboard.home.schedules')
 @endif
 @endsection
