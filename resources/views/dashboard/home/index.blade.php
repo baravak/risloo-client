8bea234b889af5838ca9b6f01aee97d1bb39be29
abouhatef <itb.baravak@gmial.com>
2021-06-24 10:22:38 +0430

    bugfix

--- resources/views/dashboard/home/index.blade.php
+++ resources/views/dashboard/home/index.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 
 <h2 class="text-gray-700 variable-font-bold mb-4">@lang('Welcome to risloo')</h2>
-@if (config('app.env') == 'localx')
+@if (config('app.env') == 'local')
     @include('dashboard.home.alerts')
     @include('dashboard.home.summary')
 @endif
