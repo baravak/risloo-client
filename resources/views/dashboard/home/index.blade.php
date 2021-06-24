9891510870b9ef01b3c5d171b297eaa6dbc7c84e
abouhatef <itb.baravak@gmial.com>
2021-06-24 10:29:52 +0430

    change dashboard view

--- resources/views/dashboard/home/index.blade.php
+++ resources/views/dashboard/home/index.blade.php
@@ -2,11 +2,12 @@
 @section('content')
 
 <h2 class="text-gray-700 variable-font-bold mb-4">@lang('Welcome to risloo')</h2>
-@if (config('app.env') == 'local')
-    @include('dashboard.home.alerts')
-    @include('dashboard.home.summary')
-@endif
+@include('dashboard.home.alerts')
 @if ($user->dalily_schedule_exports)
     @include('dashboard.home.schedules')
+@endif
+
+@if (config('app.env') == 'local')
+    {{-- @include('dashboard.home.summary') --}}
 @endif
 @endsection
