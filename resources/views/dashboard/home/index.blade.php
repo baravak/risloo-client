b2f939c80272f1ad60d12c18513ef08d230b38b5
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-07-11 09:57:32 +0430

    Update home schedules

--- resources/views/dashboard/home/index.blade.php
+++ resources/views/dashboard/home/index.blade.php
@@ -1,6 +1,6 @@
 @extends($layouts->dashboard)
 @section('content')
-<h2 class="text-gray-700 variable-font-bold mb-4">@lang('Welcome to risloo')</h2>
+<h2 class="text-gray-500 variable-font-semibold text-sm mb-4">@lang('Welcome to risloo')</h2>
 @include('dashboard.home.alerts')
 @if ($user->rooms && $user->rooms->where('acceptation.position', 'manager')->count())
     @include('dashboard.home.schedules')
--- resources/views/dashboard/home/schedules.blade.php
+++ resources/views/dashboard/home/schedules.blade.php
@@ -1,30 +1,34 @@
-<h3 class="text-gray-700 variable-font-bold mb-4 mt-8">@lang('Therapy Schedules')</h3>
+<div class="flex items-center mb-4 mt-8">
+    <h3 class="text-gray-700 variable-font-bold cursor-default">@lang('Therapy Schedules')</h3>
+    <a href="#" class="flex items-center justify-center bg-green-600 text-white text-xs rounded-full px-2 mr-2 w-5 sm:w-auto h-5 hover:bg-green-700 transition" title="@lang('Define new schedule')">
+        <i class="fal fa-plus text-xs"></i>
+        <span class="hidden sm:inline-flex mr-1">@lang('Define new schedule')</span>
+    </a>
+</div>
+
 <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-6 gap-4">
     @isset($user->dalily_schedule_exports->today)
-        <span class="flex items-center border border-brand rounded py-2 px-4 text-white bg-brand hover:bg-brand-600 transition">
-            <a href="{{ $user->dalily_schedule_exports->today->png }}" target="_blank">
-                <i class="fal fa-calendar-day pb-1 ml-4 text-2xl"></i>
-                <span class="text-sm variable-font-medium">برنامه درمانی امروز</span>
-            </a>
-        </span>
-    @else
-        <span class="flex items-center border border-yellow-500 rounded py-2 px-4 text-yellow-600 transition">
+        <a href="{{ $user->dalily_schedule_exports->today->png }}" target="_blank" class="flex items-center border border-brand rounded py-2 px-4 text-brand hover:text-white hover:bg-brand transition">
             <i class="fal fa-calendar-day pb-1 ml-4 text-2xl"></i>
-            <span class="text-xs variable-font-medium">برنامه‌ای برای امروز ندارید!</span>
-        </span>
-    @endisset
-    @isset($user->dalily_schedule_exports->tomorrow)
-        <span class="flex items-center border border-brand rounded py-2 px-4 text-white bg-brand hover:bg-brand-600 transition">
-            <a href="{{ $user->dalily_schedule_exports->tomorrow->png }}" target="_blank">
-                <i class="fal fa-calendar-alt pb-1 ml-4 text-2xl"></i>
-                <span class="text-sm variable-font-medium">برنامه درمانی فردا</span>
-            </a>
-        </span>
+            <span class="text-sm variable-font-medium">برنامه درمانی امروز</span>
+        </a>
     @else
-    <span class="flex items-center border border-yellow-500 rounded py-2 px-4 text-yellow-600 transition">
-        <i class="fal fa-calendar-alt pb-1 ml-4 text-2xl"></i>
-        <span class="text-xs variable-font-medium">جلسه‌ای برای فردا ثبت نشده است!</span>
-    </span>
+        <div class="flex items-center border border-gray-400 rounded py-2 px-4 text-gray-400 transition cursor-default">
+            <i class="fal fa-calendar-day pb-1 ml-4 text-2xl"></i>
+            <span class="block text-xs variable-font-medium">برنامه‌ای برای امروز ندارید!</span>
+        </div>
+    @endisset
+
+    @isset($user->dalily_schedule_exports->tomorrow)
+        <a href="{{ $user->dalily_schedule_exports->tomorrow->png }}" target="_blank" class="flex items-center border border-brand rounded py-2 px-4 text-brand hover:text-white hover:bg-brand transition">
+            <i class="fal fa-calendar-alt pb-1 ml-4 text-2xl"></i>
+            <span class="text-sm variable-font-medium">برنامه درمانی فردا</span>
+        </a>
+    @else
+        <div class="flex items-center border border-gray-400 rounded py-2 px-4 text-gray-400 transition cursor-default">
+            <i class="fal fa-calendar-alt pb-1 ml-4 text-2xl"></i>
+            <span class="block text-xs variable-font-medium">جلسه‌ای برای فردا ثبت نشده است!</span>
+        </div>
     @endisset
     {{-- <a href="#" target="_blank" class="flex items-center border border-brand rounded py-2 px-4 text-brand hover:bg-brand hover:text-white transition">
         <i class="fal fa-calendar-week pb-1 ml-4 text-2xl"></i>
